001e231ea730c1b7fc7d297739c287b9e949c0b2
Adam Colton <adamcolton@gmail.com>
2020-08-23 11:56:50 -0400

    +ds/bufpool.WriterToString

--- ds/bufpool/bufpool_test.go
+++ ds/bufpool/bufpool_test.go
@@ -1,8 +1,10 @@
 package bufpool
 
 import (
-	"github.com/stretchr/testify/assert"
+	"io"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBufferPool(t *testing.T) {
@@ -17,6 +19,32 @@ func TestPutStr(t *testing.T) {
 	s := PutStr(buf)
 	assert.Equal(t, "Hello", s)
 	// another process gets the same buffer
+	buf.Reset()
 	buf.WriteString("Goodbye")
 	assert.Equal(t, "Hello", s)
+	buf.Reset()
+}
+
+func TestPutAndCopy(t *testing.T) {
+	buf := Get()
+	buf.WriteString("Hello")
+	b := PutAndCopy(buf)
+	assert.Equal(t, []byte("Hello"), b)
+	// another process gets the same buffer
+	buf.Reset()
+	buf.WriteString("Goodbye")
+	assert.Equal(t, []byte("Hello"), b)
+	buf.Reset()
+}
+
+type writerto struct{}
+
+func (writerto) WriteTo(w io.Writer) (int64, error) {
+	w.Write([]byte("testing"))
+	return 7, nil
+}
+
+func TestMustWriterToString(t *testing.T) {
+	s := MustWriterToString(writerto{})
+	assert.Equal(t, "testing", s)
 }
