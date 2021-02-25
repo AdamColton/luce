134c496b896d50dc83885ee5639f625f23ed5b94
Adam Colton <adamcolton@gmail.com>
2021-02-24 21:37:23 -0500

    +lerr.Many

--- lerr/errors.go
+++ lerr/errors.go
@@ -1,5 +1,7 @@
 package lerr
 
+import "strings"
+
 // Str provides a string error that can be set to a const making it good for
 // sentinal errors.
 type Str string
@@ -7,4 +9,41 @@ type Str string
 // Error fulfills error
 func (err Str) Error() string {
 	return string(err)
+}
+
+// Many allows many errors to be collected.
+type Many []error
+
+// NewMany creates a new Many and adds all non-nil errs to it.
+func NewMany(errs ...error) Many {
+	var m Many
+	for _, err := range errs {
+		m = m.Add(err)
+	}
+	return m
+}
+
+// Error fulfills error
+func (m Many) Error() string {
+	out := make([]string, 0, len(m))
+	for _, e := range m {
+		out = append(out, e.Error())
+	}
+	return strings.Join(out, "\n")
+}
+
+// Add an error to the collection. If err is nil, it will not be added
+func (m Many) Add(err error) Many {
+	if err == nil {
+		return m
+	}
+	return append(m, err)
+}
+
+// Cast to error type. If Many contains no errors, it will return nil.
+func (m Many) Cast() error {
+	if len(m) == 0 {
+		return nil
+	}
+	return m
 }
