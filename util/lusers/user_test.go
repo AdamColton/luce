e5bf4c038a3f7b686d2bf743136a185c98de8958
Adam Colton <adamcolton@gmail.com>
2021-05-26 10:41:27 -0400

    +util/lusers

---
+++ util/lusers/user_test.go
@@ -0,0 +1,25 @@
+package lusers
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/ds/idx/byteid/bytebtree"
+	"github.com/adamcolton/luce/store/ephemeral"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUser(t *testing.T) {
+	us, err := NewUserStore(ephemeral.Factory(bytebtree.New, 10))
+	assert.NoError(t, err)
+
+	n, p := "testuser", "password"
+	u, err := us.Create(n, p)
+	assert.NoError(t, err)
+
+	u2, err := us.GetByID(u.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, u, u2)
+	assert.Equal(t, u.HashedPassword, u2.HashedPassword)
+	assert.NoError(t, u2.CheckPassword(p))
+	assert.Equal(t, n, u2.Name)
+}
