fe659625ea2318f4c7942ce84d09497e8e2c5532
Adam Colton <adamcolton@gmail.com>
2021-05-29 15:35:52 -0400

    +tools/server

---
+++ tools/server/web.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/lhttp/midware"
+	"github.com/adamcolton/luce/lhttp/valuedecoder"
+	"github.com/adamcolton/luce/util/lusers/lusess"
+)
+
+type TemplateNames struct {
+	SignIn       string
+	Home         string
+	HomeSignedIn string
+}
+
+func (s *Server) setRoutes(host string) {
+	m := midware.New(
+		s.Users.Midware(),
+		midware.NewDecoder(valuedecoder.Form(), "Form"),
+		midware.NewDecoder(valuedecoder.Query(), "URLData"),
+		midware.NewRedirect("Redirect"),
+	)
+
+	r := s.Router
+	if host != "" {
+		r = r.Host(host).Subrouter()
+	}
+
+	r.HandleFunc("/", m.Handle(s.home))
+	r.HandleFunc("/user/signin", m.Handle(s.getSignIn)).Methods("GET")
+	r.HandleFunc("/user/signin", m.Handle(s.postSignIn)).Methods("POST")
+	r.HandleFunc("/user/signout", m.Handle(s.getSignOut)).Methods("GET")
+	r.HandleFunc("/admin/users", m.Handle(s.adminUsers)).Methods("GET")
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	cookie := &http.Cookie{
+		Name:  "testing",
+		Value: "this is a test",
+	}
+	http.SetCookie(w, cookie)
+}
+
+func (s *Server) home(w http.ResponseWriter, r *http.Request, d *struct {
+	Session *lusess.Session
+}) {
+	u := d.Session.User()
+	n := s.TemplateNames.Home
+	if u != nil {
+		n = s.TemplateNames.HomeSignedIn
+	}
+	err := s.Templates.ExecuteTemplate(w, n, u)
+	lerr.Panic(err)
+}
