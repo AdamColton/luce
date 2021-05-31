73660067faeadbf6ab13ab528944e3309d5a8129
Adam Colton <adamcolton@gmail.com>
2021-05-31 19:04:07 -0400

    use midware, login/logout logic

--- tools/server/user.go
+++ tools/server/user.go
@@ -1,15 +1,12 @@
 package server
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/util/lusers/lusess"
 )
-
-type UserTemplates struct {
-	SignIn string
-}
 
 type signinData struct {
 	Message string
@@ -17,14 +14,31 @@ type signinData struct {
 
 func (signinData) Title() string { return "Sign In" }
 
-func (s *Server) getSignIn(w http.ResponseWriter, r *http.Request) {
-	err := s.Templates.ExecuteTemplate(w, s.UserTemplates.SignIn, signinData{})
+func (s *Server) getSignIn(w http.ResponseWriter, r *http.Request, d *struct {
+	Session *lusess.Session
+}) {
+	if d.Session.User() != nil {
+		redirect(w, r)
+	}
+	err := s.Templates.ExecuteTemplate(w, s.TemplateNames.SignIn, signinData{})
 	lerr.Panic(err)
 }
 
-func (s *Server) postSignIn(w http.ResponseWriter, r *http.Request) {
-	sess, err := s.Users.Login(w, r)
-	lerr.Panic(err)
+func (s *Server) postSignIn(w http.ResponseWriter, r *http.Request, d *struct {
+	Form    *lusess.Login
+	Session *lusess.Session
+}) {
+	_, err := d.Session.Login(d.Form)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	redirect(w, r)
+}
 
-	fmt.Fprint(w, sess.User())
+func (s *Server) getSignOut(w http.ResponseWriter, r *http.Request, d *struct {
+	Session *lusess.Session
+}) {
+	d.Session.Logout()
+	redirect(w, r)
 }
--- tools/server/web.go
+++ tools/server/web.go
@@ -2,16 +2,44 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/lhttp/formdecoder"
+	"github.com/adamcolton/luce/lhttp/midware"
+	"github.com/adamcolton/luce/util/lusers/lusess"
 )
 
-func (s *Server) setRoutes() {
-	r := s.Router
-
-	r.HandleFunc("/", s.sayHi)
-	r.HandleFunc("/user/signin", s.getSignIn).Methods("GET")
-	r.HandleFunc("/user/signin", s.postSignIn).Methods("POST")
+type TemplateNames struct {
+	SignIn       string
+	Home         string
+	HomeSignedIn string
 }
 
-func (s *Server) sayHi(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hi!"))
+func (s *Server) setRoutes() {
+	m := midware.NewMagic(
+		s.Users,
+		midware.NewDecoder(formdecoder.New(), "Form"),
+	)
+	r := s.Router
+
+	r.HandleFunc("/", m.Handle(s.home))
+	r.HandleFunc("/user/signin", m.Handle(s.getSignIn)).Methods("GET")
+	r.HandleFunc("/user/signin", m.Handle(s.postSignIn)).Methods("POST")
+	r.HandleFunc("/user/signout", m.Handle(s.getSignOut)).Methods("GET")
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func (s *Server) home(w http.ResponseWriter, r *http.Request, d *struct {
+	Session *lusess.Session
+}) {
+	u := d.Session.User()
+	n := s.TemplateNames.Home
+	if u != nil {
+		n = s.TemplateNames.HomeSignedIn
+	}
+	err := s.Templates.ExecuteTemplate(w, n, u)
+	lerr.Panic(err)
 }
