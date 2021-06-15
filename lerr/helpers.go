3998243a57e5035f3dadfb21cdff856890f0a380
Adam Colton <adamcolton@gmail.com>
2021-06-15 18:42:05 -0400

    add list of errors to not panic; useful for ignoring EOF

--- lerr/helpers.go
+++ lerr/helpers.go
@@ -1,8 +1,15 @@
 package lerr
 
-// Panic if err is not nil
-func Panic(err error) {
+// Panic if err is not nil. If err is in the exception list, it will return
+// true, but will not panic.
+func Panic(err error, except ...error) bool {
+	for _, ex := range except {
+		if err == ex {
+			return true
+		}
+	}
 	if err != nil {
 		panic(err)
 	}
+	return false
 }
