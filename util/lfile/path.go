0b769490d5f5d2d6c54147abb13a5dd84baa36db
Adam Colton <adamcolton@gmail.com>
2022-01-11 15:10:20 -0500

    +lfile.Name of file or dir

--- util/lfile/path.go
+++ util/lfile/path.go
@@ -27,3 +27,21 @@ func (pln PathLength) Trim(filename string) string {
 
 	return filename[idx+1:]
 }
+
+// Name returns the last portion of a path as it's name.
+//  - "/foo/bar.txt" => "/foo/","bar.txt"
+//  - "/foo/bar/" => "/foo/","bar"
+//  - "foo.txt" => "", "foo.txt"
+//  - foo/ => "", "foo"
+// The second returned value is the name and the first
+// is the preceeding portion.
+func Name(path string) (string, string) {
+	end := len(path) - 1
+	for end > 0 && path[end] == '/' {
+		end--
+	}
+	start := end - 1
+	for ; start >= 0 && path[start] != '/'; start-- {
+	}
+	return path[:start+1], path[start+1 : end+1]
+}
--- util/lfile/path_test.go
+++ util/lfile/path_test.go
@@ -20,3 +20,20 @@ func TestPathLength(t *testing.T) {
 		})
 	}
 }
+
+func TestName(t *testing.T) {
+	tt := map[string][2]string{
+		"/foo/bar.txt": {"/foo/", "bar.txt"},
+		"/foo/bar/":    {"/foo/", "bar"},
+		"foo.txt":      {"", "foo.txt"},
+		"foo/":         {"", "foo"},
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			head, name := Name(n)
+			assert.Equal(t, tc[0], head)
+			assert.Equal(t, tc[1], name)
+		})
+	}
+}
