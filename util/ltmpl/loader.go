6175bd3dabc6e8ee4548fa952e78f0d9f92218a1
Adam Colton <adamcolton@gmail.com>
2022-01-15 18:10:44 -0500

    +ltmpl.HTMLLoader.WriteTo fulfills WriterTo

--- util/ltmpl/loader.go
+++ util/ltmpl/loader.go
@@ -2,9 +2,11 @@ package ltmpl
 
 import (
 	"html/template"
+	"io"
 
 	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/util/lfile"
+	"github.com/adamcolton/luce/util/luceio"
 )
 
 // Trimmer takes a filepath and trims it to a useful portion. The
@@ -31,19 +33,42 @@ func (l *HTMLLoader) Load() (*template.Template, error) {
 		return t
 	}
 
-	var err error
-	i, done := l.Iterator()
-	for ; !done && err == nil; done = i.Next() {
-		tmplname := i.Path()
-		if l.Trimmer != nil {
-			tmplname = l.Trim(tmplname)
-		}
-		_, err = addTemplate(tmplname).Parse(string(i.Data()))
+	fn := func(name string, i lfile.Iterator) error {
+		_, err := addTemplate(name).Parse(string(i.Data()))
+		return err
 	}
-	err = lerr.Any(i.Err(), err)
+
+	err := l.Each(fn)
 	if err != nil {
 		return nil, err
 	}
 
 	return t, nil
+}
+
+func (l *HTMLLoader) WriteTo(w io.Writer) (int64, error) {
+	sw := luceio.NewSumWriter(w)
+	sep := ""
+	fn := func(name string, i lfile.Iterator) error {
+		sw.Fprint("%s{{define \"%s\" -}}\n%s\n{{- end}}", sep, name, string(i.Data()))
+		sep = "\n"
+		return sw.Err
+	}
+
+	return sw.Sum, l.Each(fn)
+}
+
+type Each func(name string, i lfile.Iterator) error
+
+func (l *HTMLLoader) Each(fn Each) error {
+	i, done := l.Iterator()
+	var err error
+	for ; err == nil && !done; done = i.Next() {
+		tmplname := i.Path()
+		if l.Trimmer != nil {
+			tmplname = l.Trim(tmplname)
+		}
+		err = fn(tmplname, i)
+	}
+	return lerr.Any(err, i.Err())
 }
--- util/ltmpl/loader_test.go
+++ util/ltmpl/loader_test.go
@@ -39,3 +39,28 @@ func TestHTMLLoader(t *testing.T) {
 	assert.Nil(t, tmpl)
 	assert.Equal(t, expected, err)
 }
+
+func TestWriteTo(t *testing.T) {
+	restore := lfile.ReadFile
+	defer func() { lfile.ReadFile = restore }()
+	lfile.ReadFile = func(filename string) ([]byte, error) {
+		return []byte(filename + " - TEMPLATE"), nil
+	}
+
+	l := HTMLLoader{
+		Trimmer:        lfile.PathLength(2),
+		IteratorSource: lfile.Paths{"common/base/script/foo.js", "common/base/html/foo.html"},
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	l.WriteTo(buf)
+
+	expected := `{{define "script/foo.js" -}}
+common/base/script/foo.js - TEMPLATE
+{{- end}}
+{{define "html/foo.html" -}}
+common/base/html/foo.html - TEMPLATE
+{{- end}}`
+	assert.Equal(t, expected, buf.String())
+}
