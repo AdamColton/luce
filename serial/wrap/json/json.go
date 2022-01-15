d78581822492fb5c856c7ae6db7962ae1c076beb
Adam Colton <adamcolton@gmail.com>
2022-01-15 18:05:48 -0500

    +json.(Serializer && Deserializer) fulfil serial interfaces

--- serial/wrap/json/json.go
+++ serial/wrap/json/json.go
@@ -1,6 +1,7 @@
 package json
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 )
@@ -14,4 +15,29 @@ func Serialize(i interface{}, w io.Writer) error {
 // type32.DeserializeTypeID32Funcn
 func Deserialize(i interface{}, r io.Reader) error {
 	return json.NewDecoder(r).Decode(i)
+}
+
+type Serializer struct {
+	Prefix, Indent string
+}
+
+func NewSerializer(prefix, indent string) Serializer {
+	return Serializer{
+		Prefix: prefix,
+		Indent: indent,
+	}
+}
+
+func (s Serializer) Serialize(i interface{}, buf []byte) ([]byte, error) {
+	b := bytes.NewBuffer(buf)
+	enc := json.NewEncoder(b)
+	enc.SetIndent(s.Prefix, s.Indent)
+	err := enc.Encode(i)
+	return b.Bytes(), err
+}
+
+type Deserializer struct{}
+
+func (Deserializer) Deserialize(v interface{}, data []byte) error {
+	return json.Unmarshal(data, v)
 }
