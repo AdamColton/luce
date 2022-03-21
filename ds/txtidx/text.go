2d3454f6d75818b5685782e342b8702bf86e8940
Adam Colton <adamcolton@gmail.com>
2022-03-21 16:05:08 -0400

    ds/txtidx.Source:append data

--- ds/txtidx/text.go
+++ ds/txtidx/text.go
@@ -9,18 +9,21 @@ type DocumentIndex struct {
 	UnindexWords []UWordIndex
 }
 
+// IWordIndex holds all reference to a root word in the document. By putting all
+// the Variants in one place it should be easier to index.
 type IWordIndex struct {
 	BaseID   uint32
 	Variants []uint16
 	Links    []Link
 }
 
-// Link to the next word
+// Link the current word to the next word
 type Link struct {
 	VID  uint8
 	Next uint32
 }
 
+// UWordIndex
 type UWordIndex struct {
 	ID   uint32
 	Next []uint32
@@ -28,10 +31,101 @@ type UWordIndex struct {
 
 // Source holds all the words in the "dictionary"
 type Source struct {
-	Indexed        []IndexedWord
-	IndexeLookup   map[string]uint32
-	Unindexed      []string
-	UnindexeLookup map[string]uint32
+	Indexed   []IndexedWord
+	Unindexed []string
+	Lookup    map[string]SourceIDX
+}
+
+func NewSource() *Source {
+	return &Source{
+		Lookup: make(map[string]SourceIDX),
+	}
+}
+
+func (s *Source) UpsertRoot(root string) SourceIDX {
+	idx, found := s.Lookup[root]
+	if found {
+		if idx.Variant() != 0 {
+			panic("cannot add variant as root")
+		}
+		return idx
+	}
+	idx = SourceIDX(len(s.Indexed))
+	s.Indexed = append(s.Indexed, IndexedWord{
+		Variants: []string{root},
+	})
+	s.Lookup[root] = idx
+	return idx
+}
+
+func (s *Source) UpsertIndexed(root, variant string) SourceIDX {
+	idx, found := s.Lookup[variant]
+	if found {
+		return idx
+	}
+	ridx := s.UpsertRoot(root)
+	iw := s.Indexed[ridx]
+	idx = ridx.SetVariant(len(iw.Variants))
+	iw.Variants = append(iw.Variants, variant)
+	s.Indexed[ridx] = iw
+	s.Lookup[variant] = idx
+	return idx
+}
+
+func (s *Source) UpsertUnindexed(str string) SourceIDX {
+	idx, found := s.Lookup[str]
+	if found {
+		return idx
+	}
+	idx = UnindexedIDX(len(s.Unindexed))
+	s.Unindexed = append(s.Unindexed, str)
+	s.Lookup[str] = idx
+	return idx
+}
+
+func (s *Source) Get(idx SourceIDX) string {
+	if idx.Indexed() {
+		return s.Indexed[idx.Root()].Variants[idx.Variant()]
+	}
+	return s.Unindexed[idx.Unindex()]
+}
+
+// SourceIDX
+// 1 bit for Indexed
+// 31 bits for variant
+// 32 bits for root
+type SourceIDX uint64
+
+const (
+	isUnindexed SourceIDX = 1 << 63
+	rootMask              = (1 << 32) - 1
+)
+
+func (s SourceIDX) Indexed() bool {
+	return s < isUnindexed
+}
+
+func (s SourceIDX) Variant() uint64 {
+	return uint64(s) >> 32
+}
+func (s SourceIDX) SetVariant(v int) SourceIDX {
+	return (s & rootMask) | (SourceIDX(v) << 32)
+}
+
+func (s SourceIDX) Root() uint64 {
+	return uint64(s & rootMask)
+}
+
+func (s SourceIDX) Unindex() uint64 {
+	return uint64(s &^ isUnindexed)
+}
+
+func IndexedIDX(root, variant uint64) SourceIDX {
+	return SourceIDX((variant << 32) | root)
+}
+
+func UnindexedIDX(idx int) SourceIDX {
+	return isUnindexed | SourceIDX(idx)
 }
 
 // IndexedWord holds all the varaintes of an index word with the word at 0 being
--- ds/txtidx/text_test.go
+++ ds/txtidx/text_test.go
@@ -64,3 +64,35 @@ func TestDoc(t *testing.T) {
 	assert.Equal(t, expected, got)
 
 }
+
+func TestSourceIDX(t *testing.T) {
+	assert.Equal(t, SourceIDX(0), SourceIDX(1)&isUnindexed)
+	assert.Equal(t, SourceIDX(1), SourceIDX(1)&rootMask)
+
+	var r uint64 = 123
+	var v uint64 = 456
+	i := IndexedIDX(r, v)
+	assert.True(t, i.Indexed())
+	assert.Equal(t, r, i.Root())
+	assert.Equal(t, v, i.Variant())
+	v = 789
+	assert.Equal(t, v, i.SetVariant(int(v)).Variant())
+
+	var u int = 31415
+	i = UnindexedIDX(u)
+	assert.False(t, i.Indexed())
+	assert.Equal(t, uint64(u), i.Unindex())
+}
+
+func TestAppendSource(t *testing.T) {
+	s := NewSource()
+	idx := s.UpsertIndexed("this", "This ")
+	assert.Equal(t, "This ", s.Get(idx))
+	assert.Equal(t, idx, s.Lookup["This "])
+	assert.Equal(t, idx, s.UpsertIndexed("this", "This "))
+	assert.Equal(t, idx.Root(), s.UpsertIndexed("this", "this, ").Root())
+
+	idx = s.UpsertUnindexed("foo")
+	assert.Equal(t, "foo", s.Get(idx))
+	assert.Equal(t, idx, s.UpsertUnindexed("foo"))
+}
