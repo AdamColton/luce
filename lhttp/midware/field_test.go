931e7c8e452f4562c99dd3eef6c0b3bbe3179c9e
Adam Colton <adamcolton@gmail.com>
2022-05-12 13:42:05 -0400

    +lhttp/midware.FieldSetter

---
+++ lhttp/midware/field_test.go
@@ -0,0 +1,59 @@
+package midware
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/adamcolton/luce/lerr"
+	"github.com/stretchr/testify/assert"
+)
+
+type testType struct {
+	TestField *testFieldType
+}
+
+type mockFieldSetterInitilizer struct {
+	isSet bool
+}
+
+func (mfsi *mockFieldSetterInitilizer) Initilize(t reflect.Type) FieldSetter {
+	return mfsi
+}
+
+func (mfsi *mockFieldSetterInitilizer) Set(w http.ResponseWriter, r *http.Request, set reflect.Value) (func(), error) {
+	mfsi.isSet = true
+	set.Set(reflect.ValueOf(&testFieldType{
+		A: "set in Set",
+	}))
+	return nil, nil
+}
+
+func TestFieldBlankErr(t *testing.T) {
+	defer func() {
+		r := recover()
+		assert.Equal(t, lerr.Str("Invalid FieldName: bad field name"), r)
+	}()
+	f := &FieldInitilizer{
+		FieldName: "bad field name",
+	}
+	f.Initilize(reflect.TypeOf(""))
+}
+
+func TestFieldInserterNoField(t *testing.T) {
+	f := &FieldInitilizer{
+		FieldName: "B",
+	}
+	di := f.Initilize(reflect.TypeOf(testFieldType{}))
+	assert.Nil(t, di)
+}
+
+func TestFieldSetter(t *testing.T) {
+	mfsi := &mockFieldSetterInitilizer{}
+	fi := NewFieldInitilizer(mfsi, "TestField")
+	tt := testType{}
+	di := fi.Initilize(reflect.TypeOf(tt))
+	di.Insert(nil, nil, reflect.ValueOf(&tt))
+	assert.True(t, mfsi.isSet)
+	assert.Equal(t, "set in Set", tt.TestField.A)
+}
