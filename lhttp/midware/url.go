d9343a66b980060fce533b0b1442e31699dd508a
Adam Colton <adamcolton@gmail.com>
2022-05-12 13:42:36 -0400

    lhttp/midware.URL refactor to use FieldSetter

--- lhttp/midware/url.go
+++ lhttp/midware/url.go
@@ -7,31 +7,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Url struct {
-	Var, FieldName string
+// URLFieldSetter references a Var in gorilla/mux.Vars. It fulfills
+// FieldSetterInitilizer and FieldSetter. It is used to extract values from a
+// URL and set them in the magic data field.
+type URLFieldSetter struct {
+	Var string
 }
 
-type urlInserter struct {
-	idx []int
-	v   string
+// Initilize fulfills FieldSetterInitilizer.
+func (u URLFieldSetter) Initilize(fieldType reflect.Type) FieldSetter {
+	return u
 }
 
-func (u Url) Initilize(t reflect.Type) DataInserter {
-	if u.FieldName == "" {
-		panic("Url.FieldName cannot be blank")
-	}
-	urlField, hasUrl := t.FieldByName(u.FieldName)
-	if !hasUrl {
-		return nil
-	}
-	return &urlInserter{
-		idx: urlField.Index,
-		v:   u.Var,
-	}
-}
+// Vars references mux.Vars. It is left exposed for testing.
+var Vars = mux.Vars
 
-func (ui *urlInserter) Insert(w http.ResponseWriter, r *http.Request, dst reflect.Value) (func(), error) {
-	v := reflect.ValueOf(mux.Vars(r)[ui.v])
-	dst.Elem().FieldByIndex(ui.idx).Set(v)
+// Set fulfills FieldSetter by setting field to a value from the URL using
+// mux.Vars.
+func (u URLFieldSetter) Set(w http.ResponseWriter, r *http.Request, field reflect.Value) (func(), error) {
+	field.Set(reflect.ValueOf(Vars(r)[u.Var]))
 	return nil, nil
+}
+
+// URL creates a magic Initilizer to extract a segment from the URL by name and
+// set it to a magic data field by fieldName.
+func URL(segment, fieldName string) Initilizer {
+	return NewFieldInitilizer(URLFieldSetter{segment}, fieldName)
 }
