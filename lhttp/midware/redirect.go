d1a664db948c88178b020b32805b196cdefebcb3
Adam Colton <adamcolton@gmail.com>
2022-05-11 20:15:42 -0400

    +midware.Redirect
    This solves an issue that if the redirect is invoked before cookies are saved
    by a returning midware then the cookies are not saved.

---
+++ lhttp/midware/redirect.go
@@ -0,0 +1,39 @@
+package midware
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/adamcolton/luce/util/filter"
+)
+
+// Redirect is solves a side effect of the callbacks. When http.Redirect is
+// invoked it prevents cookies from being written (and possibly other headers).
+// Putting Redirect at the bottom of midware.NewMagic allows redirects to be set
+// and invoked at the end of the call.
+type Redirect struct{}
+
+var checkRedirect = filter.TypeCheck(filter.IsKind(reflect.String), typeErr("Invalid Redirect field: "))
+
+// NewRedirect creates a Redirect Initilizer. It should be the last Initilizer
+// in the Midware.
+func NewRedirect(fieldName string) Initilizer {
+	return NewFieldInitilizer(Redirect{}, fieldName)
+}
+
+// Initilize fulfills FieldSetterInitilizer.
+func (rd Redirect) Initilize(fieldType reflect.Type) FieldSetter {
+	checkRedirect.Panic(fieldType)
+	return rd
+}
+
+// Set fulfills FieldSetter by setting field to a value from the URL using
+// mux.Vars.
+func (rd Redirect) Set(w http.ResponseWriter, r *http.Request, field reflect.Value) (func(), error) {
+	return func() {
+		url := field.Interface().(string)
+		if url != "" {
+			http.Redirect(w, r, url, 302)
+		}
+	}, nil
+}
