7833dac6a160ae13fbc6eaad9c2be7f38d76f777
Adam Colton <adamcolton@gmail.com>
2022-05-12 10:07:24 -0400

    -lhttp/midware.Decoder.Handler
    Handler was created before midware.Magic. It is no longer necessary and it is
    better for there to be as little magical reflection code as possible.

--- lhttp/midware/decoder.go
+++ lhttp/midware/decoder.go
@@ -20,34 +20,6 @@ func NewDecoder(d lhttp.RequestDecoder, fieldName string) Decoder {
 	}
 }
 
-// Handler expects fn to be a function with 3 arguments. The first should be
-// http.ResponseWriter and the second should be *http.Request.  The third should
-// be the type that will be populated by the form.
-func (d Decoder) Handler(fn interface{}) http.HandlerFunc {
-	t := reflect.TypeOf(fn)
-	if t.Kind() != reflect.Func || t.NumIn() != 3 {
-		panic("Decode.Handler requires a func with 3 args")
-	}
-
-	dstType := t.In(2)
-	if dstType.Kind() == reflect.Ptr {
-		dstType = dstType.Elem()
-	}
-	v := reflect.ValueOf(fn)
-	return func(w http.ResponseWriter, r *http.Request) {
-		dst := reflect.New(dstType)
-		err := d.Decode(dst.Interface(), r)
-		if d.Check(w, r, err) {
-			return
-		}
-		v.Call([]reflect.Value{
-			reflect.ValueOf(w),
-			reflect.ValueOf(r),
-			dst,
-		})
-	}
-}
-
 type decoderInserter struct {
 	lhttp.RequestDecoder
 	idx []int
