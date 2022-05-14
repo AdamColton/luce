482cf0a0152f270950066b27244e34c2199262d7
Adam Colton <adamcolton@gmail.com>
2022-05-14 06:43:35 -0400

    -lhttp/midware old sockets

--- lhttp/midware/socket.go
+++ lhttp/midware/socket.go
@@ -29,36 +29,6 @@ func NewWebSocket() WebSocket {
 	}
 }
 
-func (ws WebSocket) Handler(handler lhttp.SocketHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		socket, err := ws.Upgrader.Upgrade(w, r, nil)
-		if !ws.Check(w, r, lerr.Wrap(err, "while_upgrading_socket")) {
-			handler(socket, r)
-		}
-	}
-}
-
-// HandleSocketChans abstracts the websocket as a pair of channels. The handler
-// must close the to channel when it is done.
-func (ws WebSocket) HandleSocketChans(handler lhttp.ChanHandler) http.HandlerFunc {
-	return ws.Handler(func(socket *websocket.Conn, r *http.Request) {
-		to := make(chan []byte, ws.ToBuf)
-		from := make(chan []byte, ws.FromBuf)
-
-		socket.SetCloseHandler(func(code int, text string) error {
-			close(to)
-			return nil
-		})
-
-		sw := lhttp.NewSocket(socket)
-
-		go sw.RunReader(from)
-		go handler(to, from, r)
-		sw.RunSender(to)
-		socket.Close()
-	})
-}
-
 // ChannelInitilizer creates an Initilizer that will abstract the websocket to
 // a pair of []byte channels.
 func (ws WebSocket) Initilizer(to, from, conn string) Initilizer {
