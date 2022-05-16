82786437fc9de02be41540180db25ddafab2bf7c
Adam Colton <adamcolton@gmail.com>
2022-05-16 12:58:04 -0400

    +tools/server/service docs

--- tools/server/service/client.go
+++ tools/server/service/client.go
@@ -6,17 +6,22 @@ import (
 	"github.com/adamcolton/luce/lerr"
 )
 
+// Client is a sub-service running on a luce Server.
 type Client struct {
 	*Mux
 	*Conn
 }
 
+// MustClient creates a client and will panic if there are any errors. The addr
+// value is the unix socket for communicating with the server.
 func MustClient(addr string) *Client {
 	c, err := NewClient(addr)
 	lerr.Panic(err)
 	return c
 }
 
+// NewClient creates a client. The addr value is the unix socket for
+// communicating with the server.
 func NewClient(addr string) (*Client, error) {
 	netConn, err := net.Dial("unix", addr)
 	if err != nil {
@@ -41,11 +46,14 @@ func NewClient(addr string) (*Client, error) {
 	}, nil
 }
 
+// Run sends the routes to the server and runs the listener.
 func (c *Client) Run() {
 	c.Sender.Send(c.Mux.Routes)
 	c.Listener.Run()
 }
 
+// Add a Route, RequestResponder is the handler and RouteConfig describes the
+// route.
 func (c *Client) Add(h RequestResponder, r RouteConfig) {
 	lerr.Panic(r.Validate())
 	fn := func(r Request) {
