7b500c3d70444d139caa387a8b3d1197344117e4
Adam Colton <adamcolton@gmail.com>
2022-05-16 12:47:56 -0400

    +example/luce.testsrv

---
+++ example/luce.testsrv/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adamcolton/luce/tools/server/service"
+)
+
+func main() {
+	conn := service.MustClient("/tmp/luceserver.service")
+
+	g := service.RouteConfigGen{
+		Base: "/testsrv",
+	}
+
+	conn.Add(
+		SayHi,
+		g.Get("/sayHi/{name}").WithUser(),
+	)
+
+	conn.Add(
+		Query,
+		g.GetQuery("/query").WithPrefix(),
+	)
+
+	conn.Run()
+}
+
+func SayHi(req service.Request) service.Response {
+	name := req.PathVars["name"]
+	if req.User != nil && req.User.Name != "" {
+		name = req.User.Name
+	}
+	return req.ResponseString(
+		fmt.Sprintf("Hi, %s!", name),
+	)
+}
+
+func Query(req service.Request) service.Response {
+	return req.ResponseString(fmt.Sprintf("Query: %v Path: %v", req.Query, req.Path))
+}
