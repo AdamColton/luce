4b4469e5f553c72ba3c7c7da2ed8830c72b1578e
Adam Colton <adamcolton@gmail.com>
2022-05-27 10:06:45 -0400

    +ds/huffman.Lookup.All() []T

--- ds/huffman/huffman_test.go
+++ ds/huffman/huffman_test.go
@@ -1,8 +1,10 @@
 package huffman
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/adamcolton/luce/ds/slice"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,13 +60,20 @@ func TestFromMap(t *testing.T) {
 	got := ht.ReadAll(enc)
 	assert.Equal(t, expected, got)
 	assert.True(t, enc.Ln < len(expected)*8)
+
+	var less = slice.LT[rune]()
+	expectedRunes := less.Sort(slice.Keys(letters))
+	gotRunes := less.Sort(l.All())
+	assert.Equal(t, expectedRunes, gotRunes)
 }
 
 func TestTranslate(t *testing.T) {
 	data := make([]Frequency[[]byte], 0, len(letters))
+	var expectedSlices [][]byte
 	for r, c := range letters {
 		b := []byte(string(r))
 		data = append(data, Frequency[[]byte]{Val: b, Count: c})
+		expectedSlices = append(expectedSlices, b)
 	}
 	ht := New(data)
 	l := NewTranslateLookup(ht, func(b []byte) string {
@@ -99,4 +108,34 @@ func TestTranslate(t *testing.T) {
 	got := ht.ReadAll(enc)
 	assert.Equal(t, expected, got)
 	assert.True(t, enc.Ln < len(expected)*8)
+
+	less := slice.Less[[]byte](func(i, j []byte) bool {
+		return string(i) < string(j)
+	})
+	gotSlices := less.Sort(l.All())
+
+	assert.Equal(t, less.Sort(expectedSlices), gotSlices)
+}
+
+func ExampleEncode_roundTrip() {
+	var letterFrequencyMap = map[rune]int{
+		'E': 21912, 'T': 16587, 'A': 14810, 'O': 14003, 'I': 13318, 'N': 12666,
+		'S': 11450, 'R': 10977, 'H': 10795, 'D': 7874, 'L': 7253, 'U': 5246,
+		'C': 4943, 'M': 4761, 'F': 4200, 'Y': 3853, 'W': 3819, 'G': 3693,
+		'P': 3316, 'B': 2715, 'V': 2019, 'K': 1257, 'X': 315, 'Q': 205,
+		'J': 188, 'Z': 128,
+	}
+	tree := MapNew(letterFrequencyMap)
+	l := NewLookup(tree)
+
+	bits := Encode([]rune("THISISATEST"), l)
+	// Encoded length is 5, much less than the 11 characters
+	fmt.Println("Length:", len(bits.Data))
+
+	str := string(tree.ReadAll(bits))
+	fmt.Println(str)
+
+	// Output:
+	// Length: 5
+	// THISISATEST
 }
