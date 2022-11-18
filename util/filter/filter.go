7fe5e568dea87e7e299aee16d2c382bb6bba1abc
Adam Colton <adamcolton@gmail.com>
2022-11-18 17:16:49 -0500

    filter/filter.go: fix comment wrapping

--- util/filter/filter.go
+++ util/filter/filter.go
@@ -14,8 +14,8 @@ func (f Filter[T]) Slice(vals []T) []T {
 	return out
 }
 
-// Chan runs a go routine listening on ch and any int that passes the
-// Int is passed to the channel that is returned.
+// Chan runs a go routine listening on ch and any int that passes the Int is
+// passed to the channel that is returned.
 func (f Filter[T]) Chan(ch <-chan T, buf int) <-chan T {
 	out := make(chan T, buf)
 	go func() {
@@ -29,24 +29,21 @@ func (f Filter[T]) Chan(ch <-chan T, buf int) <-chan T {
 	return out
 }
 
-// Or builds a new Int that will return true if either underlying
-// Int is true.
+// Or builds a new Int that will return true if either underlying Int is true.
 func (f Filter[T]) Or(f2 Filter[T]) Filter[T] {
 	return func(val T) bool {
 		return f(val) || f2(val)
 	}
 }
 
-// And builds a new Int that will return true if both underlying
-// Ints are true.
+// And builds a new Int that will return true if both underlying Ints are true.
 func (f Filter[T]) And(f2 Filter[T]) Filter[T] {
 	return func(val T) bool {
 		return f(val) && f2(val)
 	}
 }
 
-// Not builds a new Int that will return true if the underlying
-// Int is false.
+// Not builds a new Int that will return true if the underlying Int is false.
 func (f Filter[T]) Not() Filter[T] {
 	return func(val T) bool {
 		return !f(val)
