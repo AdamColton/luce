3df74e674e7980901bff27270ecd760ca704a4cd
Adam Colton <adamcolton@gmail.com>
2022-11-20 14:41:52 -0500

    +ds/slice.Clone

---
+++ ds/slice/slice_test.go
@@ -0,0 +1,16 @@
+package slice_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClone(t *testing.T) {
+	data := slice.New([]int{3, 1, 4, 1, 5, 9})
+	cp := data.Clone()
+	assert.Equal(t, data, cp)
+	data[0] = 0
+	assert.Equal(t, 3, cp[0])
+}
