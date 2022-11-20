056d448b94eaad69d3d4622244daacd2f5718131
Adam Colton <adamcolton@gmail.com>
2022-11-20 14:43:25 -0500

    +ds/slice.Swap

--- ds/slice/slice_test.go
+++ ds/slice/slice_test.go
@@ -14,3 +14,11 @@ func TestClone(t *testing.T) {
 	data[0] = 0
 	assert.Equal(t, 3, cp[0])
 }
+
+func TestSwap(t *testing.T) {
+	data := slice.Slice[int]{3, 1, 4, 1, 5, 9}
+	data.Swap(0, 1)
+	assert.Equal(t, 1, data[0])
+	assert.Equal(t, 3, data[1])
+
+}
