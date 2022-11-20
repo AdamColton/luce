8230cec30ed70991a48ce41679fb6a6a29ee3765
Adam Colton <adamcolton@gmail.com>
2022-11-20 14:41:52 -0500

    +ds/slice.Clone

--- ds/slice/slice.go
+++ ds/slice/slice.go
@@ -7,3 +7,10 @@ type Slice[T any] []T
 func New[T any](s []T) Slice[T] {
 	return s
 }
+
+// Clone a slice.
+func (s Slice[T]) Clone() Slice[T] {
+	out := make([]T, len(s))
+	copy(out, s)
+	return out
+}
