1c2d88bcb90909fc26977677dc6f1525e3faefc0
Adam Colton <adamcolton@gmail.com>
2022-11-27 12:56:25 -0500

    +ds/slice.AppendNotZero

--- ds/slice/slice.go
+++ ds/slice/slice.go
@@ -1,6 +1,10 @@
 package slice
 
-import "github.com/adamcolton/luce/util/liter"
+import (
+	"reflect"
+
+	"github.com/adamcolton/luce/util/liter"
+)
 
 // Slice is a wrapper that provides helper methods
 type Slice[T any] []T
@@ -20,6 +24,18 @@ func (s Slice[T]) Clone() Slice[T] {
 // Swaps two values in the slice.
 func (s Slice[T]) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
+}
+
+// AppendNotZero will append any values from ts that are not the zero
+// value for the type. Particularly useful for appending not nil values.
+func (s Slice[T]) AppendNotZero(ts ...T) []T {
+	for _, t := range ts {
+		v := reflect.ValueOf(t)
+		if v.Kind() != reflect.Invalid && !v.IsZero() {
+			s = append(s, t)
+		}
+	}
+	return s
 }
 
 // Iter returns an iter.Wrapper for the slice.
