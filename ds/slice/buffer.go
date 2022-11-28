f8fadd0e673f158507dbfc05b218c5260aaa3904
Adam Colton <adamcolton@gmail.com>
2022-11-28 14:46:28 -0500

    +ds/slice.BufferSlice

--- ds/slice/buffer.go
+++ ds/slice/buffer.go
@@ -11,3 +11,12 @@ func (buf Buffer[T]) Empty(c int) Slice[T] {
 	}
 	return make([]T, 0, c)
 }
+
+// BufferSlice returns a buffer with length c. If the provided buffer has
+// capacity, it will be used otherwise a new one is created.
+func (buf Buffer[T]) Slice(c int) Slice[T] {
+	if cap(buf) >= c {
+		return Slice[T](buf[:c])
+	}
+	return make([]T, c)
+}
