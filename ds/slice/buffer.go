3497bed0a1f2ac366c10b13003027cf05f4e4128
Adam Colton <adamcolton@gmail.com>
2022-11-28 14:39:49 -0500

    +ds/slice.BufferEmpty

--- ds/slice/buffer.go
+++ ds/slice/buffer.go
@@ -1,3 +1,12 @@
 package slice
 
 type Buffer[T any] []T
+
+// BufferEmpty returns a zero length buffer with at least capacity c. If the
+// provided buffer has capacity, it will be used otherwise a new one is created.
+func (buf Buffer[T]) Empty(c int) Slice[T] {
+	if cap(buf) >= c {
+		return Slice[T](buf[:0])
+	}
+	return make([]T, 0, c)
+}
