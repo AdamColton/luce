f903523cb82cc5eca4542eb52567a41e6a8a578f
Adam Colton <adamcolton@gmail.com>
2022-11-29 11:18:05 -0500

    +util/iter.Factory.Concurrent

--- util/iter/factory.go
+++ util/iter/factory.go
@@ -1,5 +1,7 @@
 package iter
 
+import "sync"
+
 // Factory creates an iterator.
 type Factory[T any] func() (iter Iter[T], t T, done bool)
 
@@ -8,4 +10,12 @@ type Factory[T any] func() (iter Iter[T], t T, done bool)
 func (f Factory[T]) Do(fn func(t T) bool) Iter[T] {
 	i, t, done := f()
 	return do(i, t, done, fn)
+}
+
+// Concurrent creates a new Iter from the factory and calls fn in a Go routine
+// for each value Iter returns until Done is true. The returned WaitGroup will
+// reach zero when all Go routines return.
+func (f Factory[T]) Concurrent(fn func(t T, idx int)) *sync.WaitGroup {
+	i, t, done := f()
+	return concurrent(i, t, done, 0, fn)
 }
