b7949a2156f73504db0ed78e3ceba9f1bbcb5fd7
Adam Colton <adamcolton@gmail.com>
2022-11-29 11:12:53 -0500

    +util/iter.Channel

--- util/iter/iter.go
+++ util/iter/iter.go
@@ -45,3 +45,12 @@ func Concurrent[T any](i Iter[T], fn func(t T, idx int)) *sync.WaitGroup {
 	t, done := i.Cur()
 	return concurrent(i, t, done, fn)
 }
+
+// Channel creates a chan with size buf and places each value from Iter on the
+// channel until Done is true. This does not reset the iterator. The channel
+// is filled from a Go routine so all the values need to be consumed or the
+// routine will never close.
+func Channel[T any](i Iter[T], buf int) <-chan T {
+	t, done := i.Cur()
+	return channel(i, t, done, buf)
+}
--- util/iter/shared.go
+++ util/iter/shared.go
@@ -49,3 +49,14 @@ func concurrent[T any](i Iter[T], t T, done bool, fn func(t T, idx int)) *sync.W
 		}
 	})
 }
+
+func channel[T any](i Iter[T], t T, done bool, buf int) <-chan T {
+	ch := make(chan T, buf)
+	go func() {
+		for ; !done; t, done = i.Next() {
+			ch <- t
+		}
+		close(ch)
+	}()
+	return ch
+}
