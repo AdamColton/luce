a13296e0f18ff98cde0feda2db42934af0a9c59b
Adam Colton <adamcolton@gmail.com>
2022-11-29 11:09:47 -0500

    +util/iter.Iter

---
+++ util/iter/iter.go
@@ -0,0 +1,11 @@
+package iter
+
+// Iter interface allows for a standard set of tools for iterating over a
+// collection.
+type Iter[T any] interface {
+	Start() (t T, done bool)
+	Next() (t T, done bool)
+	Cur() (t T, done bool)
+	Done() bool
+	Idx() int
+}
