60b3e1803dc8f12e5ccc70ae16fcb9e935e6ca49
Adam Colton <adamcolton@gmail.com>
2022-11-29 11:15:49 -0500

    +util/iter.Factory.Do

--- util/iter/factory.go
+++ util/iter/factory.go
@@ -2,3 +2,10 @@ package iter
 
 // Factory creates an iterator.
 type Factory[T any] func() (iter Iter[T], t T, done bool)
+
+// Do creates a new Iter from the factory and calls fn sequentially for each
+// value Iter returns until Done is true.
+func (f Factory[T]) Do(fn func(t T) bool) Iter[T] {
+	i, t, done := f()
+	return do(i, t, done, fn)
+}
