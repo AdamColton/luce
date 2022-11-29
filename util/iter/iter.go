a576ad1309b93a31de1e79b7fab8a77bcbc17c59
Adam Colton <adamcolton@gmail.com>
2022-11-29 11:11:56 -0500

    +util/iter.Concurrent

--- util/iter/iter.go
+++ util/iter/iter.go
@@ -1,5 +1,7 @@
 package iter
 
+import "sync"
+
 // Iter interface allows for a standard set of tools for iterating over a
 // collection.
 type Iter[T any] interface {
@@ -15,4 +17,13 @@ type Iter[T any] interface {
 func Do[T any](i Iter[T], fn func(t T) bool) Iter[T] {
 	t, done := i.Cur()
 	return do(i, t, done, fn)
+}
+
+// Concurrent calls fn in a Go routine for each value Iter returns until Done is
+// true. The returned WaitGroup will reach zero when all Go routines return.
+// This does not reset the iterator.
+func Concurrent[T any](i Iter[T], fn func(t T, idx int)) *sync.WaitGroup {
+	t, done := i.Cur()
+	idx := i.Idx()
+	return concurrent(i, t, done, idx, fn)
 }
--- util/iter/shared.go
+++ util/iter/shared.go
@@ -1,5 +1,7 @@
 package iter
 
+import "sync"
+
 func do[T any](i Iter[T], t T, done bool, fn func(t T) bool) Iter[T] {
 	for ; !done; t, done = i.Next() {
 		if fn(t) {
@@ -7,4 +9,18 @@ func do[T any](i Iter[T], t T, done bool, fn func(t T) bool) Iter[T] {
 		}
 	}
 	return nil
+}
+
+func concurrent[T any](i Iter[T], t T, done bool, idx int, fn func(t T, idx int)) *sync.WaitGroup {
+	var wg sync.WaitGroup
+	wrap := func(t T, idx int) {
+		wg.Add(1)
+		fn(t, idx)
+		wg.Add(-1)
+	}
+	for ; !done; t, done = i.Next() {
+		go wrap(t, idx)
+		idx++
+	}
+	return &wg
 }
