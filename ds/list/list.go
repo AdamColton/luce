6d99c143e5174fe373ffe664f6bc455048a371cc
Adam Colton <adamcolton@gmail.com>
2022-11-30 14:35:20 -0500

    list

---
+++ ds/list/list.go
@@ -0,0 +1,32 @@
+package list
+
+type Generator[T any] struct {
+	Fn     func(int) T
+	Length int
+}
+
+func (g Generator[T]) AtIdx(idx int) T {
+	return g.Fn(idx)
+}
+
+func (g Generator[T]) Len() int {
+	return g.Length
+}
+
+type Reverse[T any] struct {
+	List[T]
+}
+
+func (r Reverse[T]) AtIdx(idx int) T {
+	return r.List.AtIdx(r.Len() - 1 - idx)
+}
+
+func (r Reverse[T]) Slice(buf []T) []T {
+	ln := r.Len()
+	out := AllocBuf(buf, ln)
+	ln--
+	for i := 0; i <= ln; i++ {
+		out = append(out, r.List.AtIdx(ln-i))
+	}
+	return out
+}
