a01cf3d4b750723781ddbd91d111c8109c9c9fb0
Adam Colton <adamcolton@gmail.com>
2023-01-04 22:29:00 -0500

    +ds/prefix.Prefix.Remove

---
+++ ds/prefix/prefixInternal_test.go
@@ -0,0 +1,35 @@
+package prefix
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemove(t *testing.T) {
+	p := New()
+	p.Upsert("abc")
+	p.Upsert("ab")
+	p.Upsert("a")
+
+	p.Upsert("defga")
+	p.Upsert("ef")
+
+	p.Remove("ab")
+	assert.True(t, p.Find("abc").IsWord())
+	assert.False(t, p.Find("ab").IsWord())
+	p.Remove("abc")
+	assert.Nil(t, p.Find("abc"))
+	assert.Nil(t, p.Find("ab"))
+
+	p.Remove("ef")
+	p.Remove("xa")
+
+	before := len(p.starts)
+	got := p.Containing("a").Strings().Slice(nil)
+	slice.LT[string]().Sort(got)
+	assert.Equal(t, []string{"a", "defga"}, got)
+	p.Purge()
+	assert.True(t, len(p.starts) < before)
+}
