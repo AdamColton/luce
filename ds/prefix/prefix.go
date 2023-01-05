a89f04f3b00baccdc098629280979c3e300653b0
Adam Colton <adamcolton@gmail.com>
2023-01-04 22:29:00 -0500

    +ds/prefix

---
+++ ds/prefix/prefix.go
@@ -0,0 +1,46 @@
+// Package prefix holds a prefix tree for strings. The term word is used to
+// refer to values which are present in the tree. Gram is used to refer to any
+// sequence that exists in the tree. For instance is the word 'test' is inserted
+// then the tree contains the gram 'tes' but it will not be a word.
+package prefix
+
+// Prefix is the root of a prefix tree
+type Prefix struct {
+	root *node
+}
+
+// New Prefix tree.
+func New() *Prefix {
+	return &Prefix{
+		root: newNode(),
+	}
+}
+
+func (p *Prefix) seeker(str string) *seeker {
+	s := &seeker{
+		runes: []rune(str),
+		p:     p,
+		n:     p.root,
+	}
+	return s
+}
+
+// Upsert a word into the prefix tree returning the Node for that word and a
+// bool indicating if an insert happened.
+func (p *Prefix) Upsert(word string) (n Node, insert bool) {
+	if len(word) == 0 {
+		return nil, false
+	}
+	s := p.seeker(word)
+	for done := s.moveNext(true); !done; done = s.moveNext(true) {
+	}
+	if !s.n.isWord {
+		insert = true
+		s.n.isWord = true
+		for p := s.n.parent; p != nil; p = p.parent {
+			p.childrenCount++
+		}
+	}
+	n = s.n
+	return
+}
