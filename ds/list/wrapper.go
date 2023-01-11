0108318b271d43cd8750f46b300a30c512136fb2
Adam Colton <adamcolton@gmail.com>
2023-01-10 20:55:36 -0500

    +ds/list.Wrapper

---
+++ ds/list/wrapper.go
@@ -0,0 +1,26 @@
+package list
+
+import (
+	"reflect"
+
+	"github.com/adamcolton/luce/util/upgrade"
+)
+
+// Wrapper provides a number of useful methods that can be applied to any List.
+type Wrapper[T any] struct {
+	List[T]
+}
+
+// Wrap a List. Also checks that the underlying list is not itself a Wrapper.
+func Wrap[T any](l List[T]) Wrapper[T] {
+	if w, ok := l.(Wrapper[T]); ok {
+		return w
+	}
+	return Wrapper[T]{l}
+}
+
+// Upgrade fulfills upgrade.Upgrader. Checks if the underlying List fulfills the
+// given Type.
+func (w Wrapper[T]) Upgrade(t reflect.Type) interface{} {
+	return upgrade.Wrapped(w.List, t)
+}
