2be39ec39fc019f65dfc2fb9b8f46bd2e382511b
Adam Colton <adamcolton@gmail.com>
2023-01-11 11:46:15 -0500

    +ds/slice/IterSlice

--- ds/slice/iter.go
+++ ds/slice/iter.go
@@ -1,11 +1,14 @@
 package slice
 
-import "github.com/adamcolton/luce/util/iter"
+import (
+	"github.com/adamcolton/luce/util/iter"
+	"github.com/adamcolton/luce/util/upgrade"
+)
 
 // Iter wraps a slice to fulfill iter.Iter.
 type Iter[T any] struct {
-	Slice []T
-	I     int
+	S []T
+	I int
 	// DoneFn determines when the iterator is done. If it does not check the
 	// bounds, an out of bounds panic is likely.
 	DoneFn func(*Iter[T]) bool
@@ -15,9 +18,9 @@ type Iter[T any] struct {
 // that compares to the length of the slice.
 func NewIter[T any](s []T) *Iter[T] {
 	return &Iter[T]{
-		Slice: s,
+		S: s,
 		DoneFn: func(i *Iter[T]) bool {
-			return i.I >= len(i.Slice)
+			return i.I >= len(i.S)
 		},
 	}
 }
@@ -27,7 +30,7 @@ func NewIter[T any](s []T) *Iter[T] {
 func (i *Iter[T]) Cur() (t T, done bool) {
 	done = i.Done()
 	if !done {
-		t = i.Slice[i.I]
+		t = i.S[i.I]
 	}
 	return
 }
@@ -56,6 +59,15 @@ func (i *Iter[T]) Idx() int {
 	return i.I
 }
 
+// Slice fulfills Slicer. If a buffer is provided, a copy is made. If no buffer
+// is provided, the underlying slice is returned.
+func (i *Iter[T]) Slice(buf []T) []T {
+	if cap(buf) > 0 {
+		return append(buf[:0], i.S...)
+	}
+	return i.S
+}
+
 // IterFactory fulfills iter.Factory.
 func IterFactory[T any](s []T) iter.Factory[T] {
 	return func() (i iter.Iter[T], t T, done bool) {
@@ -63,4 +75,21 @@ func IterFactory[T any](s []T) iter.Factory[T] {
 		t, done = i.Cur()
 		return
 	}
+}
+
+// IterSlice creates a slice from an iter.Iter. If the Iter fulfills Slicer,
+// then the Slice method will be used. If it fulfill Lener, that will be used
+// for buffer allocation.
+func IterSlice[T any](i iter.Iter[T], buf []T) []T {
+	var s Slicer[T]
+	if upgrade.Upgrade(i, &s) {
+		return s.Slice(buf)
+	}
+
+	out := BufferLener(i, buf)
+	iter.For(i, func(t T, idx int) {
+		out = append(out, t)
+	})
+
+	return out
 }
