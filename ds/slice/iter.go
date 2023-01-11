eac80bb37d7441b53043522bf18f50f97c5bd9f5
Adam Colton <adamcolton@gmail.com>
2023-01-11 11:46:15 -0500

    +ds/slice.(Slicer,FromIter,FromIterFactory)

--- ds/slice/iter.go
+++ ds/slice/iter.go
@@ -1,6 +1,9 @@
 package slice
 
-import "github.com/adamcolton/luce/util/liter"
+import (
+	"github.com/adamcolton/luce/util/liter"
+	"github.com/adamcolton/luce/util/upgrade"
+)
 
 // Iter wraps a slice to fulfillliter.Iter.
 type Iter[T any] struct {
@@ -56,6 +59,15 @@ func (i *Iter[T]) Idx() int {
 	return i.I
 }
 
+// Slice fulfills Slicer. If a buffer is provided, a copy is made. If no buffer
+// is provided, the underlying slice is returned.
+func (i *Iter[T]) Slice(buf []T) []T {
+	if cap(buf) > 0 {
+		return append(buf[:0], i.S...)
+	}
+	return i.S
+}
+
 // IterFactory fulfills liter.Factory.
 func IterFactory[T any](s []T) liter.Factory[T] {
 	return func() (i liter.Iter[T], t T, done bool) {
@@ -63,4 +75,48 @@ func IterFactory[T any](s []T) liter.Factory[T] {
 		t, done = i.Cur()
 		return
 	}
+}
+
+// FromIter creates a slice from a liter.Iter. If the Iter fulfills Slicer,
+// then the Slice method will be used. If it fulfill Lener, that will be used
+// for buffer allocation.
+func FromIter[T any](i liter.Iter[T], buf []T) Slice[T] {
+	if s, ok := upgrade.To[Slicer[T]](i); ok {
+		return s.Slice(buf)
+	}
+
+	if ln, ok := i.(Lener); ok {
+		agg := Buffer[T](buf).Empty(ln.Len())
+		return liter.Appender[T]().Iter(agg, i)
+	}
+
+	t, done := i.Cur()
+	if done {
+		return nil
+	}
+	out := recurFromIter(i, buf, 1)
+	out[0] = t
+	return out
+}
+
+func recurFromIter[T any](i liter.Iter[T], buf []T, size int) (out Slice[T]) {
+	t, done := i.Next()
+	if done {
+		out = Buffer[T](buf).Slice(size)
+	} else {
+		out = recurFromIter(i, buf, size+1)
+		out[size] = t
+	}
+	return
+}
+
+// FromIterFactory creates a slice from a liter.Factory. If the Iter fulfills
+// Slicer, then the Slice method will be used. If it fulfill Lener, that will be
+// used for buffer allocation.
+func FromIterFactory[T any](f liter.Factory[T], buf []T) Slice[T] {
+	i, _, done := f()
+	if done {
+		return nil
+	}
+	return FromIter(i, buf)
 }
