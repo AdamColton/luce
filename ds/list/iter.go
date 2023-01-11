98c28a8dc9a9edc396282a451decb77b8c13ea66
Adam Colton <adamcolton@gmail.com>
2023-01-10 20:58:25 -0500

    +ds/list/Iter

---
+++ ds/list/iter.go
@@ -0,0 +1,72 @@
+package list
+
+import (
+	"github.com/adamcolton/luce/util/iter"
+)
+
+// Iter fulfills iter.Iter by iterating over the list.
+type Iter[T any] struct {
+	List[T]
+	I int
+}
+
+// NewIter creates an Iter using the provided list.
+func NewIter[T any](l List[T]) *Iter[T] {
+	return &Iter[T]{List: l}
+}
+
+// Idx fulfills iter.Iter and provides the current index.
+func (i *Iter[T]) Idx() int {
+	return i.I
+}
+
+// Done fulfills iter.Iter indicating if iteration is done.
+func (i *Iter[T]) Done() bool {
+	return i.I >= i.Len()
+}
+
+// Cur fulfills iter.Iter returning both the current value and if iteration is
+// done. If iteration is done, T will be the default value.
+func (i *Iter[T]) Cur() (t T, done bool) {
+	done = i.Done()
+	if !done {
+		t = i.List.AtIdx(i.I)
+	}
+	return
+}
+
+// Next fulfills iter.Iter and increments the index. It returns the current
+// index and a bool indicating if it's done.
+func (i *Iter[T]) Next() (t T, done bool) {
+	ln := i.Len()
+	done = i.I >= ln
+	if done {
+		return
+	}
+	i.I++
+	done = i.I >= ln
+	if !done {
+		t = i.AtIdx(i.I)
+	}
+	return
+}
+
+// Start sets the index to zero. Returns the first element and a bool indicating
+// if it's done.
+func (i *Iter[T]) Start() (t T, done bool) {
+	i.I = -1
+	return i.Next()
+}
+
+// IterFactory creates an iter.Factory that generates a *list.Iter backed by
+// list L.
+func IterFactory[T any](l List[T]) iter.Factory[T] {
+	return func() (it iter.Iter[T], t T, done bool) {
+		it = &Iter[T]{
+			List: l,
+			I:    -1,
+		}
+		t, done = it.Next()
+		return
+	}
+}
--- ds/list/list_test.go
+++ ds/list/list_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/adamcolton/luce/ds/list"
+	"github.com/adamcolton/luce/util/iter"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -26,9 +27,26 @@ func TestLists(t *testing.T) {
 	for n, tc := range tt {
 		t.Run(n, func(t *testing.T) {
 			assert.Equal(t, len(tc.expected), tc.List.Len())
-			for i, e := range tc.expected {
-				assert.Equal(t, e, tc.List.AtIdx(i))
+			f := list.IterFactory(tc.List)
+			var last int
+			fn := func(i, idx int) {
+				assert.Equal(t, tc.expected[idx], i)
+				last = idx
 			}
+			f.For(fn)
+			assert.Len(t, tc.expected, last+1)
+
+			it := list.NewIter(tc.List)
+			last = 0
+			iter.For[int](it, fn)
+			assert.Len(t, tc.expected, last+1)
+
+			it.Start()
+			assert.Equal(t, 0, it.Idx())
+
+			it.I = it.Len()
+			_, done := it.Next()
+			assert.True(t, done)
 		})
 	}
 }
