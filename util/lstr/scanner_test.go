13c4a44708d437efffee810b587b41cb05357959
Adam Colton <adamcolton@gmail.com>
2023-04-04 19:31:38 -0400

    +util/lstr.Scanner

---
+++ util/lstr/scanner_test.go
@@ -0,0 +1,27 @@
+package lstr_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/util/lstr"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScanner(t *testing.T) {
+	str := "Test"
+	s := lstr.NewScanner(str)
+	c := s.Iter().ForIdx(func(r rune, idx int) {
+		assert.Equal(t, rune(str[idx]), r)
+	})
+	assert.Equal(t, c, len(str))
+
+	s.Reset()
+	assert.False(t, s.Peek(lstr.IsLower))
+	assert.Equal(t, 0, s.I)
+	assert.True(t, s.Match(lstr.IsUpper))
+	assert.Equal(t, 1, s.I)
+	assert.False(t, s.Many(lstr.IsUpper))
+	assert.True(t, s.Many(lstr.IsLower))
+	assert.Equal(t, 4, s.I)
+	assert.True(t, s.Done())
+}
