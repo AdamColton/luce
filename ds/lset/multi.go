e3ccca06b0b5b7a8b81ce44c7e0388bdb87dfd32
Adam Colton <adamcolton@gmail.com>
2023-04-08 18:11:20 -0400

    +ds/lset.Multi.Sort

--- ds/lset/multi.go
+++ ds/lset/multi.go
@@ -1,3 +1,13 @@
 package lset
 
+import "github.com/adamcolton/luce/ds/slice"
+
 type Multi[T comparable] []*Set[T]
+
+// Sort Multi from smallest to largest. This order is assumend for optimization
+// of other methods, but is not necessary.
+func (m Multi[T]) Sort() {
+	slice.Less[*Set[T]](func(i, j *Set[T]) bool {
+		return i.Len() < j.Len()
+	}).Sort(m)
+}
