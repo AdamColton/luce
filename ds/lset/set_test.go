b391e2768965fa98f34d99490bbcc37cd30e7e86
Adam Colton <adamcolton@gmail.com>
2023-04-08 16:42:34 -0400

    +ds/lset

---
+++ ds/lset/set_test.go
@@ -0,0 +1,35 @@
+package lset_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/ds/lset"
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSet(t *testing.T) {
+	lt := slice.LT[int]()
+	s := lset.New[int]()
+	s.Add(3, 1, 4, 1, 5, 9)
+
+	assert.True(t, s.Contains(1))
+	assert.False(t, s.Contains(2))
+
+	s.Remove(1)
+	assert.False(t, s.Contains(1))
+
+	expect := func(expected ...int) {
+		assert.Equal(t, expected, lt.Sort(s.Slice(nil)))
+	}
+	expect(3, 4, 5, 9)
+
+	assert.Equal(t, s.Len(), 4)
+
+	s2 := lset.New(6, 7)
+	s.AddAll(s2)
+	expect(3, 4, 5, 6, 7, 9)
+
+	s2 = s.Copy()
+	assert.Equal(t, s, s2)
+}
