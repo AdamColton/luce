c07a7459ec65ba85f0841a6d07fe82270ac8eb5c
Adam Colton <adamcolton@gmail.com>
2023-04-08 18:11:20 -0400

    +ds/lset.Multi.Sort

--- ds/lset/set_test.go
+++ ds/lset/set_test.go
@@ -33,3 +33,13 @@ func TestSet(t *testing.T) {
 	s2 = s.Copy()
 	assert.Equal(t, s, s2)
 }
+
+func TestMulti(t *testing.T) {
+	s1 := lset.New(1, 2, 3, 4)
+	s2 := lset.New(4, 5)
+	s3 := lset.New(5, 6, 7)
+	m := lset.NewMulti(s1, s2, s3)
+
+	m.Sort()
+	assert.Equal(t, lset.Multi[int]{s2, s3, s1}, m)
+}
