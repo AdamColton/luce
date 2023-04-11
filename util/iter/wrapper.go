c4c8a3b94cfdf3e1e4d3cd4ee239669b6229a74b
Adam Colton <adamcolton@gmail.com>
2023-04-10 22:17:53 -0400

    +util/iter.Wrapper

---
+++ util/iter/wrapper.go
@@ -0,0 +1,14 @@
+package iter
+
+// Wrapper provides useful methods that can be applied to any List.
+type Wrapper[T any] struct {
+	Iter[T]
+}
+
+// Wrap a Iter. Also checks that the underlying Iter is not itself a Wrapper.
+func Wrap[T any](i Iter[T]) Wrapper[T] {
+	if w, ok := i.(Wrapper[T]); ok {
+		return w
+	}
+	return Wrapper[T]{i}
+}
