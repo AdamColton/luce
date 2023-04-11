108711649266259cd948d7f2f5f0d5d13e6a30e8
Adam Colton <adamcolton@gmail.com>
2023-04-10 22:38:24 -0400

    +util/iter.Factory.For

--- util/iter/factory.go
+++ util/iter/factory.go
@@ -9,3 +9,10 @@ func (f Factory[T]) Seek(fn func(t T) bool) Iter[T] {
 	i, t, done := f()
 	return seek(i, t, done, fn)
 }
+
+// ForIdx calls fn sequentially for each value Iter. This does not reset the
+// iterator.
+func (f Factory[T]) For(fn func(t T)) {
+	i, t, done := f()
+	fr(i, t, done, fn)
+}
