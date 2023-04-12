c76a7b3238e208be9e7208cbae243c2c49ac3bfd
Adam Colton <adamcolton@gmail.com>
2023-04-11 20:01:58 -0400

    +util/iter.Wrapper.Upgrade

--- util/iter/wrapper_test.go
+++ util/iter/wrapper_test.go
@@ -1,14 +1,20 @@
 package iter_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/adamcolton/luce/util/iter"
+	"github.com/adamcolton/luce/util/upgrade"
 	"github.com/stretchr/testify/assert"
 )
 
 func (s *sliceIter[T]) Wrap() iter.Wrapper[T] {
 	return iter.Wrapper[T]{s}
+}
+
+func (s *sliceIter[T]) String() string {
+	return "sliceIter"
 }
 
 func TestWrap(t *testing.T) {
@@ -19,4 +25,16 @@ func TestWrap(t *testing.T) {
 	assert.Equal(t, si, w.Iter)
 	w = iter.Wrap[int](w)
 	assert.Equal(t, si, w.Iter)
+}
+
+func TestUpgrade(t *testing.T) {
+	si := &sliceIter[int]{
+		Slice: []int{3, 1, 4, 1, 5, 9},
+	}
+	w := si.Wrap()
+
+	s, ok := upgrade.To[fmt.Stringer](w)
+	assert.True(t, ok)
+	assert.NotNil(t, s)
+	assert.Equal(t, "sliceIter", s.String())
 }
