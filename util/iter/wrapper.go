95a82a0abecdccc198be3744fb8b216029601591
Adam Colton <adamcolton@gmail.com>
2023-04-11 20:02:34 -0400

    +util/iter.Wrapper.Seek

--- util/iter/wrapper.go
+++ util/iter/wrapper.go
@@ -24,3 +24,10 @@ func Wrap[T any](i Iter[T]) Wrapper[T] {
 func (w Wrapper[T]) Upgrade(t reflect.Type) interface{} {
 	return upgrade.Wrapped(w.Iter, t)
 }
+
+// Seek calls fn sequentially for each value Iter returns until Done is true.
+// This does not reset the iterator.
+func (w Wrapper[T]) Seek(fn func(t T) bool) Iter[T] {
+	t, done := w.Cur()
+	return seek(w.Iter, t, done, fn)
+}
