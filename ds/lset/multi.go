f7688ed3429b7f31494969b522a258604362498d
Adam Colton <adamcolton@gmail.com>
2023-04-11 22:08:48 -0400

    +ds/lset.Multi.Intersection

--- ds/lset/multi.go
+++ ds/lset/multi.go
@@ -37,3 +37,28 @@ func (m Multi[T]) AllContain(t T) bool {
 	}
 	return true
 }
+
+func (m Multi[T]) Intersection() *Set[T] {
+	if len(m) == 0 {
+		return nil
+	}
+	if len(m) == 1 {
+		return m[0].Copy()
+	}
+	out := &Set[T]{
+		m: make(map[T]flag, m[0].Len()),
+	}
+	for k := range m[0].m {
+		inAll := true
+		for _, s := range m[1:] {
+			inAll = s.Contains(k)
+			if !inAll {
+				break
+			}
+		}
+		if inAll {
+			out.m[k] = flag{}
+		}
+	}
+	return out
+}
