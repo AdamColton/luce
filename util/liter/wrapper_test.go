c666faa7390b67f5c6410f9cafd627a44949d81d
Adam Colton <adamcolton@gmail.com>
2023-04-11 20:02:34 -0400

    +util/liter.Wrapper.Seek

--- util/liter/wrapper_test.go
+++ util/liter/wrapper_test.go
@@ -38,3 +38,32 @@ func TestUpgrade(t *testing.T) {
 	assert.NotNil(t, s)
 	assert.Equal(t, "sliceIter", s.String())
 }
+
+func TestWrapperSeek(t *testing.T) {
+	si := &sliceIter[int]{
+		Slice: []int{3, 1, 4, 1, 5, 9},
+	}
+	w := si.Wrap()
+	idx := 0
+	fn := func(i int) bool {
+		assert.Equal(t, si.Slice[idx], i)
+		idx++
+		return false
+	}
+	it := w.Seek(fn)
+	assert.Len(t, si.Slice, idx)
+	assert.Nil(t, it)
+
+	w.Iter.(*sliceIter[int]).idx = 0
+	it = w.Seek(func(i int) bool {
+		return i == 4
+	})
+	i, done := it.Cur()
+	assert.Equal(t, 4, i)
+	assert.False(t, done)
+	idx = si.idx
+
+	it = w.Seek(fn)
+	assert.Len(t, si.Slice, idx)
+	assert.Nil(t, it)
+}
