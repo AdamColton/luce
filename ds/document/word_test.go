44c6d20bb8dfd6f2f821b5ced05b082afad06e84
Adam Colton <adamcolton@gmail.com>
2023-05-06 16:31:13 -0400

    +ds/document/Root

---
+++ ds/document/word_test.go
@@ -0,0 +1,22 @@
+package document_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/ds/document"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRoot(t *testing.T) {
+	tt := map[string]string{
+		"test ":      "test",
+		"123test --": "123test",
+		"123Test.--": "123test",
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			assert.Equal(t, tc, document.Root(n))
+		})
+	}
+}
