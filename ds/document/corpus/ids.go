64c13e2924de00022bb308b98e66f70092d9184d
Adam Colton <adamcolton@gmail.com>
2023-05-12 22:49:18 -0400

    +ds/document/corpus.Corpus.Find(word)

--- ds/document/corpus/ids.go
+++ ds/document/corpus/ids.go
@@ -1,11 +1,14 @@
 package corpus
 
+import "github.com/adamcolton/luce/ds/lset"
+
 // RootID represents a root word - a lower case alphanumeric string
 type RootID uint32
 
 type root struct {
 	RootID
-	str string
+	str  string
+	docs *lset.Set[DocID]
 }
 
 // VariantID represents a Variant by ID.
