ac74a7d2b7efca2b30970f3e8117d4d45a273ed5
Adam Colton <adamcolton@gmail.com>
2023-06-15 21:16:01 -0400

    +ds/huffman/huffslice.Slice.Iter

--- ds/huffman/huffslice/huffslice_test.go
+++ ds/huffman/huffslice/huffslice_test.go
@@ -23,7 +23,7 @@ func TestHuffSlice(t *testing.T) {
 			ue.Slice = append(ue.Slice, tc...)
 
 			u := ue.Encode()
-			got := u.Decode()
+			got := slice.FromIter(u.Iter(), nil)
 
 			assert.Equal(t, slice.New(tc), got)
 		})
