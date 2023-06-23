9c6ca85202caf3c01dc6a3bfd215a3f0e257fc6d
Adam Colton <adamcolton@gmail.com>
2023-06-23 14:00:26 -0400

    +ds/document/corpus.Document

--- ds/document/corpus/corpus.go
+++ ds/document/corpus/corpus.go
@@ -2,26 +2,40 @@ package corpus
 
 import "github.com/adamcolton/luce/ds/document"
 
+const (
+	MaxUint32    = ^uint32(0)
+	MaxVariantID = VariantID(MaxUint32)
+	MaxRootID    = RootID(MaxUint32)
+)
+
 // Corpus holds a collection of documents for text indexing. It fulfills
 // document.Encoder and document.Decoder.
 type Corpus struct {
+	Splitter    func(string) (string, []string)
+	RootVariant func(str string) (string, document.Variant)
+
 	cur struct {
 		RootID
 		VariantID
+		DocID
 	}
 	id2root    map[RootID]*root
 	rootByStr  map[string]*root
 	variant2id map[string]VariantID
 	id2variant map[VariantID]document.Variant
+	docs       map[DocID]*Document
 }
 
 // New creates a Corpus.
 func New() *Corpus {
 	return &Corpus{
-		id2root:    make(map[RootID]*root),
-		rootByStr:  make(map[string]*root),
-		variant2id: make(map[string]VariantID),
-		id2variant: make(map[VariantID]document.Variant),
+		Splitter:    document.Parse,
+		RootVariant: document.RootVariant,
+		id2root:     make(map[RootID]*root),
+		rootByStr:   make(map[string]*root),
+		variant2id:  make(map[string]VariantID),
+		id2variant:  make(map[VariantID]document.Variant),
+		docs:        make(map[DocID]*Document),
 	}
 }
 
@@ -65,4 +79,23 @@ func (c *Corpus) IDToWord(rID RootID) string {
 // IDToVariant converts a VariantID to a document.Variant, fulfilling document.Decoder.
 func (c *Corpus) IDToVariant(vID VariantID) document.Variant {
 	return c.id2variant[vID]
+}
+
+// AddDoc to Corpus, returns the string encoded as a Document with a DocID.
+func (c *Corpus) AddDoc(str string) *Document {
+	enc := &document.DocumentEncoder[RootID, VariantID]{
+		Encoder:         c,
+		Splitter:        c.Splitter,
+		RootVariant:     c.RootVariant,
+		WordSingleToken: MaxRootID,
+		VarSingleToken:  MaxVariantID,
+	}
+	d := &Document{
+		DocID:    c.cur.DocID,
+		Document: enc.Build(str),
+		c:        c,
+	}
+	c.cur.DocID++
+	c.docs[d.DocID] = d
+	return d
 }
