2e32b3b600c9d4b860981961e244fb7e3811dd36
Adam Colton <adamcolton@gmail.com>
2023-06-23 13:57:23 -0400

    +ds/document/corpus.Corpus variants

--- ds/document/corpus/corpus_test.go
+++ ds/document/corpus/corpus_test.go
@@ -3,6 +3,7 @@ package corpus_test
 import (
 	"testing"
 
+	"github.com/adamcolton/luce/ds/document"
 	"github.com/adamcolton/luce/ds/document/corpus"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,4 +22,25 @@ func TestRootID(t *testing.T) {
 
 	assert.Equal(t, "", c.IDToWord(123))
 
+}
+
+func TestVariantID(t *testing.T) {
+	c := corpus.New()
+	vars := []document.Variant{
+		{1, 32},
+		{0, 32},
+		{1, 13},
+		{0, 13},
+	}
+	for i, v := range vars {
+		assert.Equal(t, corpus.VariantID(i), c.VariantToID(v))
+	}
+
+	for i, v := range vars {
+		id := corpus.VariantID(i)
+		assert.Equal(t, v, c.IDToVariant(id))
+		assert.Equal(t, id, c.VariantToID(v))
+	}
+
+	assert.Equal(t, "", c.IDToWord(123))
 }
