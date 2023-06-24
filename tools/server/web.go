93133052d3d45613b3cfa4b8bb9bf27fa39f0659
Adam Colton <adamcolton@gmail.com>
2023-06-24 11:27:21 -0400

    -tools/server.Server.setCookie remove test code

--- tools/server/web.go
+++ tools/server/web.go
@@ -22,20 +22,11 @@ func (s *Server) setRoutes() {
 	)
 	r := s.Router
 
-	r.HandleFunc("/setCookie", setCookie)
 	r.HandleFunc("/", m.Handle(s.home))
 	r.HandleFunc("/user/signin", m.Handle(s.getSignIn)).Methods("GET")
 	r.HandleFunc("/user/signin", m.Handle(s.postSignIn)).Methods("POST")
 	r.HandleFunc("/user/signout", m.Handle(s.getSignOut)).Methods("GET")
 	r.HandleFunc("/admin/users", m.Handle(s.adminUsers)).Methods("GET")
-}
-
-func setCookie(w http.ResponseWriter, r *http.Request) {
-	cookie := &http.Cookie{
-		Name:  "testing",
-		Value: "this is a test",
-	}
-	http.SetCookie(w, cookie)
 }
 
 func (s *Server) home(w http.ResponseWriter, r *http.Request, d *struct {
