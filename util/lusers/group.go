097daab826e994293d48f399966654abff0aceef
Adam Colton <adamcolton@gmail.com>
2023-06-24 20:36:18 -0400

    +util/lusers add documentation

--- util/lusers/group.go
+++ util/lusers/group.go
@@ -6,6 +6,7 @@ import (
 	"github.com/adamcolton/luce/store"
 )
 
+// Group has a name and contains a set of users.
 type Group struct {
 	Name string
 	store.Store
@@ -14,6 +15,7 @@ type Group struct {
 // We only care about the key, but we have to store some value
 var hasUser = []byte{1}
 
+// AddUser to Group
 func (g *Group) AddUser(u *User) error {
 	err := g.Store.Put(u.ID, hasUser)
 	if err != nil {
@@ -24,6 +26,7 @@ func (g *Group) AddUser(u *User) error {
 	return nil
 }
 
+// HasUser checks if the user is in the Group
 func (g *Group) HasUser(u *User) bool {
 	return bytes.Equal(g.Store.Get(u.ID).Value, hasUser)
 }
