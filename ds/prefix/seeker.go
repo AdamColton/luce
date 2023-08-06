7e373d15c910ea3ac55d74776e699a5e543bdde9
Adam Colton <adamcolton@gmail.com>
2023-08-06 07:03:21 -0400

    ds/prefix Use Navigator

--- ds/prefix/seeker.go
+++ ds/prefix/seeker.go
@@ -1,39 +1,8 @@
 package prefix
 
+import "github.com/adamcolton/luce/util/navigator"
+
 type seeker struct {
-	runes []rune
-	idx   int
-	p     *Prefix
-	n     *node
-}
-
-func (s *seeker) moveNext(insert bool) (done bool) {
-	done = s.n == nil || s.idx >= len(s.runes)
-	if !done {
-		r := s.runes[s.idx]
-		s.idx++
-		n := s.n.children[r]
-		if n == nil {
-			done = !insert
-			if insert {
-				n = newNode()
-				n.r = r
-				n.parent = s.n
-				s.n.children[r] = n
-				s.p.starts[r] = append(s.p.starts[r], n)
-			}
-		}
-		s.n = n
-	}
-	return
-}
-
-func (s *seeker) movePrev() (n *node, done bool) {
-	n = s.n
-	done = s.idx <= 0
-	if !done {
-		s.idx--
-		s.n = s.n.parent
-	}
-	return
+	p *Prefix
+	*navigator.Navigator[rune, *node, *Prefix]
 }
