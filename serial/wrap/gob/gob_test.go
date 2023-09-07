0b17dfee3dd172c1dbe0642af508a8015776200a
Adam Colton <adamcolton@gmail.com>
2023-09-07 14:29:21 -0400

    +serial/wrap/gob

---
+++ serial/wrap/gob/gob_test.go
@@ -0,0 +1,13 @@
+package gob_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/serial/wrap/gob"
+	"github.com/adamcolton/luce/serial/wrap/testutil"
+)
+
+func TestAll(t *testing.T) {
+	gob.Register((*testutil.Person)(nil))
+	testutil.SerialFuncsRoundTrip(t, gob.Serialize, gob.Deserialize)
+}
