18e2f69482efdabb4d5a919f15e3d0c6373a66db
Adam Colton <adamcolton@gmail.com>
2023-09-07 18:03:09 -0400

    +ds/merkle

---
+++ ds/merkle/builder.go
@@ -0,0 +1,51 @@
+package merkle
+
+import (
+	"hash"
+
+	"github.com/adamcolton/luce/math/ints"
+)
+
+type builder struct {
+	maxLeafSize int
+	h           func() hash.Hash
+}
+
+// NewBuilder creates a Builder whose trees will follow the limits set by
+// maxSize and branch and will use the provided hash.
+func NewBuilder(maxSize uint32, h func() hash.Hash) Builder {
+	return builder{
+		maxLeafSize: int(maxSize),
+		h:           h,
+	}
+}
+
+func (b builder) Build(data []byte) Tree {
+	if len(data) == 0 {
+		return nil
+	}
+
+	t := &tree{
+		h: b.h(),
+	}
+	t.node, t.leaves = makeTree(0, b.maxLeafSize, data, t.h)
+	return t
+}
+
+func makeTree(idx uint32, maxLeafSize int, data []byte, h hash.Hash) (node, uint32) {
+	ln := len(data)
+	if ln <= maxLeafSize {
+		return newDataLeaf(data, idx, h), idx + 1
+	}
+
+	leaves := ints.DivUp(ln, maxLeafSize)
+	split := (leaves / 2) * maxLeafSize
+
+	n := &branch{
+		data: data,
+	}
+	n.children[0], idx = makeTree(idx, maxLeafSize, data[:split], h)
+	n.children[1], idx = makeTree(idx, maxLeafSize, data[split:], h)
+	n.update(h)
+	return n, idx
+}
---
+++ ds/merkle/interfaces.go
@@ -0,0 +1,19 @@
+package merkle
+
+// Builder handles the logic of generating and populating a tree from data.
+type Builder interface {
+	Build(data []byte) Tree
+}
+
+// node that has been populated with data and hashes
+type node interface {
+	// Digest returns the digest of the Tree for validation
+	Digest() []byte
+	// Data returns the entire data of the tree
+	Data() []byte
+	Leaves() int
+}
+
+type Tree interface {
+	node
+}
