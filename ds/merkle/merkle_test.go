73e6cd32df7d28c32a4cb15c8915dc8d0f2062dd
Adam Colton <adamcolton@gmail.com>
2023-09-12 08:47:18 -0400

    +ds/merkle.Assembler

--- ds/merkle/merkle_test.go
+++ ds/merkle/merkle_test.go
@@ -3,6 +3,7 @@ package merkle_test
 import (
 	"crypto/rand"
 	"crypto/sha256"
+	mrand "math/rand"
 	"testing"
 
 	"github.com/adamcolton/luce/ds/merkle"
@@ -35,5 +36,39 @@ func TestLeaf(t *testing.T) {
 		start = end
 	}
 	assert.Nil(t, m.Leaf(leaves))
+}
 
+func TestAssembler(t *testing.T) {
+	var maxLeafSize uint32 = 200
+	var ln uint32 = 5000
+	data := make([]byte, ln)
+	rand.Read(data)
+
+	m := merkle.NewBuilder(maxLeafSize, sha256.New).Build(data)
+	assert.Equal(t, data, m.Data())
+
+	leaves := make([]*merkle.Leaf, m.Leaves())
+	for i := range leaves {
+		leaves[i] = m.Leaf(i)
+	}
+	mrand.New(mrand.NewSource(31415)).Shuffle(len(leaves), func(i, j int) {
+		leaves[i], leaves[j] = leaves[j], leaves[i]
+	})
+
+	a := m.Description().Assembler(sha256.New())
+
+	l := m.Leaf(0)
+	l.Index = uint32(m.Leaves()) + 1
+	assert.False(t, a.Add(l))
+
+	for _, l := range leaves {
+		done, tr := a.Done()
+		assert.False(t, done)
+		assert.Nil(t, tr)
+		assert.True(t, a.Add(l), l.Index)
+		assert.False(t, a.Add(l))
+	}
+	done, tr := a.Done()
+	assert.True(t, done)
+	assert.Equal(t, data, tr.Data())
 }
