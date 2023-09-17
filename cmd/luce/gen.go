acdf9ecb213ee4c43fb4488c449eca8bdbc83aee
Adam Colton <adamcolton@gmail.com>
2023-09-17 16:28:15 -0400

    cmd/luce update

--- cmd/luce/gen.go
+++ cmd/luce/gen.go
@@ -4,7 +4,6 @@ import (
 	cryptorand "crypto/rand"
 	"encoding/base64"
 	"fmt"
-	"math/rand"
 	"os"
 	"strings"
 	"text/template"
@@ -12,30 +11,17 @@ import (
 	"unicode/utf8"
 
 	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/util/lrand"
 	"github.com/urfave/cli"
 )
 
 func randCmd(c *cli.Context) error {
-	seed := make([]byte, 8)
-	cryptorand.Read(seed)
-
-	rand.Seed(
-		(int64(seed[0]) << (8 * 0)) |
-			(int64(seed[1]) << (8 * 1)) |
-			(int64(seed[2]) << (8 * 2)) |
-			(int64(seed[3]) << (8 * 3)) |
-			(int64(seed[4]) << (8 * 4)) |
-			(int64(seed[5]) << (8 * 5)) |
-			(int64(seed[6]) << (8 * 6)) |
-			(int64(seed[7]) << (8 * 7)),
-	)
-
 	max := c.Int64("n")
 	if b := c.Int("b"); b > 0 {
 		fmt.Println(b)
 		max = 1 << uint(b)
 	}
-	fmt.Println(rand.Int63n(max))
+	fmt.Println(lrand.New().Int63n(max))
 	return nil
 }
 
@@ -128,4 +114,8 @@ func filter(c *cli.Context) error {
 	}
 	fd.update()
 	return filterTmpl.Execute(os.Stdout, fd)
+}
+func rand32(c *cli.Context) error {
+	fmt.Println(lrand.New().Uint32())
+	return nil
 }
