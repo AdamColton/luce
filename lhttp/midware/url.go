82a723c77a98d3c8e2901b07f071ea66e505071c
Adam Colton <adamcolton@gmail.com>
2023-09-17 15:47:35 -0400

    +lhttp/midware.URL

---
+++ lhttp/midware/url.go
@@ -0,0 +1,37 @@
+package midware
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/gorilla/mux"
+)
+
+type Url struct {
+	Var, FieldName string
+}
+
+type urlInserter struct {
+	idx []int
+	v   string
+}
+
+func (u Url) Initilize(t reflect.Type) DataInserter {
+	if u.FieldName == "" {
+		panic("Url.FieldName cannot be blank")
+	}
+	urlField, hasUrl := t.FieldByName(u.FieldName)
+	if !hasUrl {
+		return nil
+	}
+	return &urlInserter{
+		idx: urlField.Index,
+		v:   u.Var,
+	}
+}
+
+func (ui *urlInserter) Insert(w http.ResponseWriter, r *http.Request, dst reflect.Value) (func(), error) {
+	v := reflect.ValueOf(mux.Vars(r)[ui.v])
+	dst.Elem().FieldByIndex(ui.idx).Set(v)
+	return nil, nil
+}
