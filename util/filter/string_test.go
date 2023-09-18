f10307bd6d63694131711283195f275c7abeb0f9
Adam Colton <adamcolton@gmail.com>
2023-09-18 17:39:29 -0400

    +util/filter.Prefix

---
+++ util/filter/string_test.go
@@ -0,0 +1,24 @@
+package filter_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/util/filter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrefix(t *testing.T) {
+	tt := map[string]bool{
+		"test":    true,
+		"testing": true,
+		"atest":   false,
+		"abc":     false,
+	}
+
+	p := filter.Prefix("test")
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			assert.Equal(t, tc, p(n))
+		})
+	}
+}
