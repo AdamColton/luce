af58bc1e9d3cad9c688cb93d5d93d5dd818a8fe5
Adam Colton <adamcolton@gmail.com>
2023-09-18 08:12:05 -0400

    +serial.WrapPrefixer(pre ReflectTypePrefixer) TypePrefixer

--- serial/interfaces.go
+++ serial/interfaces.go
@@ -1,5 +1,7 @@
 package serial
 
+import "reflect"
+
 // Serializer takes an interface and returns the serialization as a byte slice.
 type Serializer interface {
 	Serialize(interface{}, []byte) ([]byte, error)
@@ -9,4 +11,22 @@ type Serializer interface {
 // and populates the interface from the data.
 type Deserializer interface {
 	Deserialize(interface{}, []byte) error
+}
+
+// InterfaceTypePrefixer writes the type of the interface to slice. Generally
+// this will end up effectivly prefixing the type.
+type InterfaceTypePrefixer interface {
+	PrefixInterfaceType(interface{}, []byte) ([]byte, error)
+}
+
+// ReflectTypePrefixer writes a reflect.Type to slice. Generally this will end up
+// effectivly prefixing the type.
+type ReflectTypePrefixer interface {
+	PrefixReflectType(reflect.Type, []byte) ([]byte, error)
+}
+
+// TypePrefixer combines both TypePrefixing techniques.
+type TypePrefixer interface {
+	ReflectTypePrefixer
+	InterfaceTypePrefixer
 }
