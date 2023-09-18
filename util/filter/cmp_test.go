478e21cd8893e05560973939f75f2cedc9c7803b
Adam Colton <adamcolton@gmail.com>
2023-09-18 17:35:16 -0400

    +util/filter.(Comparable, LT, LTE, EQ, NEQ, GTE, GT)

---
+++ util/filter/cmp_test.go
@@ -0,0 +1,53 @@
+package filter_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/util/filter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompare(t *testing.T) {
+	tt := map[string]struct {
+		fn    func(a, b int) bool
+		cmpFn func(a int) filter.Filter[int]
+	}{
+		"lt": {
+			fn:    func(a, b int) bool { return b < a },
+			cmpFn: filter.LT[int],
+		},
+		"lte": {
+			fn:    func(a, b int) bool { return b <= a },
+			cmpFn: filter.LTE[int],
+		},
+		"eq": {
+			fn:    func(a, b int) bool { return b == a },
+			cmpFn: filter.EQ[int],
+		},
+		"gt": {
+			fn:    func(a, b int) bool { return b > a },
+			cmpFn: filter.GT[int],
+		},
+		"gte": {
+			fn:    func(a, b int) bool { return b >= a },
+			cmpFn: filter.GTE[int],
+		},
+		"neq": {
+			fn:    func(a, b int) bool { return b != a },
+			cmpFn: filter.NEQ[int],
+		},
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			for a := 0; a < 3; a++ {
+				fn := tc.cmpFn(a)
+				for b := 0; b < 3; b++ {
+					expect := tc.fn(a, b)
+					got := fn(b)
+					assert.Equal(t, expect, got)
+				}
+			}
+		})
+	}
+}
