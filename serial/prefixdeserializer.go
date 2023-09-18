2f5bfaa4491d3334e6b5fe32a892c2cfa179bab9
Adam Colton <adamcolton@gmail.com>
2023-09-18 08:16:06 -0400

    +serial.PrefixDeserializer

---
+++ serial/prefixdeserializer.go
@@ -0,0 +1,40 @@
+package serial
+
+import (
+	"reflect"
+)
+
+// PrefixDeserializer handles both deserializing the type and the data. This
+// allows a byte slice to be passed in an the interface returned as opposed to
+// the normal requirement of passing in both an interface and a slice.
+type PrefixDeserializer struct {
+	Detyper
+	Deserializer
+}
+
+// DeserializeType gets the type from the data, creates an instance and then
+// deserializes the data into that instance.
+func (ds PrefixDeserializer) DeserializeType(data []byte) (any, error) {
+	t, data, err := ds.GetType(data)
+	if err != nil {
+		return nil, err
+	}
+
+	var i any
+	var isPtr = t.Kind() == reflect.Ptr
+	if isPtr {
+		i = reflect.New(t.Elem()).Interface()
+	} else {
+		i = reflect.New(t).Interface()
+	}
+
+	err = ds.Deserialize(i, data)
+	if err != nil {
+		return nil, err
+	}
+
+	if isPtr {
+		return i, nil
+	}
+	return reflect.ValueOf(i).Elem().Interface(), nil
+}
