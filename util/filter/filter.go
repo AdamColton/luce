a90afd253f18460f7ca027e9fd678bb44a9ff570
Adam Colton <adamcolton@gmail.com>
2023-09-18 17:15:22 -0400

    +util/filter.Filter.(And, Or, Not)

--- util/filter/filter.go
+++ util/filter/filter.go
@@ -2,3 +2,27 @@ package filter
 
 // Filter provides tools to filter ints and compose filters
 type Filter[T any] func(T) bool
+
+// Or builds a new Int that will return true if either underlying
+// Int is true.
+func (f Filter[T]) Or(f2 Filter[T]) Filter[T] {
+	return func(val T) bool {
+		return f(val) || f2(val)
+	}
+}
+
+// And builds a new Int that will return true if both underlying
+// Ints are true.
+func (f Filter[T]) And(f2 Filter[T]) Filter[T] {
+	return func(val T) bool {
+		return f(val) && f2(val)
+	}
+}
+
+// Not builds a new Int that will return true if the underlying
+// Int is false.
+func (f Filter[T]) Not() Filter[T] {
+	return func(val T) bool {
+		return !f(val)
+	}
+}
