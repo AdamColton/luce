1f37cee26a8d9f53a40bd5d3c2479145fd674ffd
Adam Colton <adamcolton@gmail.com>
2023-09-19 10:03:32 -0400

    +util/luceio.TemplateTo

--- util/luceio/interfaces.go
+++ util/luceio/interfaces.go
@@ -1,5 +1,7 @@
 package luceio
 
+import "io"
+
 // StringWriter is the interface that wraps the WriteString method
 type StringWriter interface {
 	WriteString(string) (int, error)
@@ -8,4 +10,11 @@ type StringWriter interface {
 // StringWriter is the interface that wraps the WriteString method
 type StringsWriter interface {
 	WriteStrings(...string) (int, error)
+}
+
+// TemplateExecutor is an interface representing the ExecuteTemplate method on
+// a template.
+type TemplateExecutor interface {
+	ExecuteTemplate(io.Writer, string, interface{}) error
+	Execute(io.Writer, interface{}) error
 }
