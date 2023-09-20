8276c8e5584281162236ec2dbc0686a925f5bfb6
Adam Colton <adamcolton@gmail.com>
2023-09-19 20:26:40 -0400

    +serial/rye.Deserialize

---
+++ serial/rye/deserialize.go
@@ -0,0 +1,30 @@
+package rye
+
+type D byte
+
+var Deserialize D
+
+// Uint16 returns a uint16 from the Deserializer and increases the index.
+func (D) Uint16(b []byte) uint16 {
+	return uint16(b[1])<<8 + uint16(b[0])
+}
+
+// Uint32 returns a uint32 from the Deserializer and increases the index.
+func (D) Uint32(b []byte) uint32 {
+	return uint32(b[3])<<24 +
+		uint32(b[2])<<16 +
+		uint32(b[1])<<8 +
+		uint32(b[0])
+}
+
+// Uint64 returns a uint64 from the Deserializer and increases the index.
+func (D) Uint64(b []byte) uint64 {
+	return uint64(b[7])<<56 +
+		uint64(b[6])<<48 +
+		uint64(b[5])<<40 +
+		uint64(b[4])<<32 +
+		uint64(b[3])<<24 +
+		uint64(b[2])<<16 +
+		uint64(b[1])<<8 +
+		uint64(b[0])
+}
