1413e620b09f5d6c2edf6b91d29f66c4413b1b74
Adam Colton <adamcolton@gmail.com>
2023-09-21 17:22:41 -0400

    +util/filter.(CanElem, Type.Elem)

--- util/filter/type.go
+++ util/filter/type.go
@@ -14,6 +14,13 @@ func (t Type) OnInterface(i any) bool {
 	return t.Filter(reflect.TypeOf(i))
 }
 
+// Elem checks the filter type's Elem against the underlying filter.
+func (t Type) Elem() Type {
+	return Type{func(t2 reflect.Type) (out bool) {
+		return CanElem(t2) && t.Filter(t2.Elem())
+	}}
+}
+
 // IsKind filter checks Kind.
 func IsKind(kind reflect.Kind) Type {
 	return Type{func(t reflect.Type) bool {
@@ -27,4 +34,17 @@ func IsType(t1 reflect.Type) Type {
 	return Type{func(t2 reflect.Type) bool {
 		return t1 == t2
 	}}
+}
+
+// CanElem returns true if it is safe to call Elem on t.
+func CanElem(t reflect.Type) bool {
+	if t == nil {
+		return false
+	}
+	k := t.Kind()
+	return k == reflect.Array ||
+		k == reflect.Chan ||
+		k == reflect.Map ||
+		k == reflect.Pointer ||
+		k == reflect.Slice
 }
