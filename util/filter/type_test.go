c59fe74e27101ee9db9e52ab8cc2985563fca14f
Adam Colton <adamcolton@gmail.com>
2023-09-21 17:38:20 -0400

    +util/filter.TypeChecker

--- util/filter/type_test.go
+++ util/filter/type_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/util/filter"
 	"github.com/adamcolton/luce/util/reflector/ltype"
 	"github.com/stretchr/testify/assert"
@@ -102,4 +103,22 @@ func TestType(t *testing.T) {
 			assert.Equal(t, tc.expected, tc.f.OnInterface(tc.v))
 		})
 	}
+}
+
+func TestTypeChecker(t *testing.T) {
+	expectedErr := lerr.Str("expected string, got int")
+
+	errFn := func(t reflect.Type) error {
+		return lerr.Str("expected string, got " + t.String())
+	}
+
+	c := filter.IsType(ltype.String).Check(errFn)
+
+	ct, err := c("test")
+	assert.NoError(t, err)
+	assert.Equal(t, ltype.String, ct)
+
+	ct, err = c(123)
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, ltype.Int, ct)
 }
