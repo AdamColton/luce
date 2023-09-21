7df193155f2cbc7a5aeb5803c2ececc2278a9f23
Adam Colton <adamcolton@gmail.com>
2023-09-21 16:40:41 -0400

    +reflector.ToType

--- util/reflector/reflector_test.go
+++ util/reflector/reflector_test.go
@@ -2,12 +2,33 @@ package reflector_test
 
 import (
 	"fmt"
+	"reflect"
+	"testing"
 
 	"github.com/adamcolton/luce/util/reflector"
+	"github.com/stretchr/testify/assert"
 )
 
 func ExampleType() {
 	t := reflector.Type[string]()
 	fmt.Println("t is reflect.Type on", t.String())
 	// Output: t is reflect.Type on string
+}
+
+func TestToType(t *testing.T) {
+	s := "string"
+	st := reflect.TypeOf(s)
+
+	assert.Equal(t, st, reflector.ToType(s))
+	assert.Equal(t, st, reflector.ToType(st))
+}
+
+func ExampleToType() {
+	t := reflector.ToType("test")
+	fmt.Println("t is reflect.Type on", t.String())
+
+	t2 := reflector.ToType(t)
+	fmt.Println("t2 is reflect.Type on", t2.String())
+	// Output: t is reflect.Type on string
+	// t2 is reflect.Type on string
 }
