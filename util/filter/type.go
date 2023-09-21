0099e25165ea03f7a62326a72f81a33a1b9db6b5
Adam Colton <adamcolton@gmail.com>
2023-09-21 18:37:43 -0400

    +util/filter.NumOut

--- util/filter/type.go
+++ util/filter/type.go
@@ -68,3 +68,10 @@ func NumIn(f Filter[int]) Type {
 		return t != nil && t.Kind() == reflect.Func && f(t.NumIn())
 	}}
 }
+
+// NumOut checks the filter type's NumOut value against the given filter.
+func NumOut(f Filter[int]) Type {
+	return Type{func(t reflect.Type) (out bool) {
+		return t != nil && t.Kind() == reflect.Func && f(t.NumOut())
+	}}
+}
