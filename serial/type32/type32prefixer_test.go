b49488b89e578396da086c15873d5fd17d57f908
Adam Colton <adamcolton@gmail.com>
2023-09-29 16:58:55 -0400

    +serial/type32.Type32Prefixer

---
+++ serial/type32/type32prefixer_test.go
@@ -0,0 +1,31 @@
+package type32_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/serial/rye"
+	"github.com/adamcolton/luce/serial/type32"
+	"github.com/adamcolton/luce/util/reflector"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestType32Prefixer(t *testing.T) {
+	p := &person{
+		Name: "Adam",
+		Age:  39,
+	}
+	t32p := type32.Type32Prefixer{}
+	got, err := t32p.PrefixInterfaceType(p, nil)
+	assert.NoError(t, err)
+
+	expected := make([]byte, 4)
+	rye.Serialize.Uint32(expected, p.TypeID32())
+
+	assert.Equal(t, expected, got)
+
+	_, err = t32p.PrefixInterfaceType(123, nil)
+	assert.Equal(t, type32.ErrTypeNotFound{reflector.Type[int]()}, err)
+
+	s := t32p.Serializer(nil)
+	assert.Equal(t, s.InterfaceTypePrefixer, t32p)
+}
