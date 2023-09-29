f24260d678ffc54c6f2dd9c348f21469f67b037e
Adam Colton <adamcolton@gmail.com>
2023-09-29 17:02:04 -0400

    +serial/type32.TypeMap

--- serial/type32/type32.go
+++ serial/type32/type32.go
@@ -3,6 +3,8 @@ package type32
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/adamcolton/luce/serial/rye"
 )
 
 // ErrTypeNotFound is returned when a type map does not contain the given
@@ -14,4 +16,11 @@ type ErrTypeNotFound struct {
 // Error fulfills error.
 func (err ErrTypeNotFound) Error() string {
 	return fmt.Sprintf("type32: type %s was not found", err.Type)
+}
+
+func sliceToUint32(b []byte) uint32 {
+	if len(b) < 4 {
+		return 0
+	}
+	return rye.Deserialize.Uint32(b)
 }
