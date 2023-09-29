712c8846f7a5cb65fbb5bc2c0e83fda045830e39
Adam Colton <adamcolton@gmail.com>
2023-09-29 16:58:55 -0400

    +serial/type32.Type32Prefixer

---
+++ serial/type32/type32_test.go
@@ -0,0 +1,24 @@
+package type32_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/serial/type32"
+	"github.com/adamcolton/luce/util/reflector"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrTypeNotFound(t *testing.T) {
+	intTp := reflector.Type[int]()
+	assert.Equal(t, "Type int was not found", type32.ErrTypeNotFound{intTp}.Error())
+
+}
+
+type person struct {
+	Name string
+	Age  int
+}
+
+func (*person) TypeID32() uint32 {
+	return 12345
+}
