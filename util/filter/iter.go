b3d76c5b0e71aa502c0ceb7fca5e905b38dadc8e
Adam Colton <adamcolton@gmail.com>
2023-10-02 17:39:09 -0400

    +util/filter.Iter

---
+++ util/filter/iter.go
@@ -0,0 +1,17 @@
+package filter
+
+import "github.com/adamcolton/luce/util/liter"
+
+// Nexter fulfills liter.NextFunc
+func (f Filter[T]) Nexter(i liter.Iter[T]) liter.NextFunc[T] {
+	return func() (t T, done bool) {
+		for t, done = i.Next(); !done && !f(t); t, done = i.Next() {
+		}
+		return
+	}
+}
+
+// Iter created from the Filter.
+func (f Filter[T]) Iter(i liter.Iter[T]) liter.Wrapper[T] {
+	return f.Nexter(i).Indexer()
+}
