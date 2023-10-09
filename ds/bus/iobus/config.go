17623123dcbae1541e17e7ac8b5e8954853d184e
Adam Colton <adamcolton@gmail.com>
2023-10-09 17:27:00 -0400

    +ds/bus/iobus.Config

---
+++ ds/bus/iobus/config.go
@@ -0,0 +1,28 @@
+package iobus
+
+import "time"
+
+// BufSize is the default buffer size that will be used if BufSize is not
+// explicitly set on a Reader.
+var BufSize uint = 512
+
+// Config sets operational details for a Bus.
+type Config struct {
+	// CloseOnEOF causes a reader to close when it receives and EOF
+	CloseOnEOF bool
+	// Buffer size to use
+	BufSize int
+	// MakeErrCh is used by NewReader and NewWriter to set if an error channel
+	// should be created.
+	MakeErrCh bool
+	// Sleep determines how long to wait before reading again after an empty
+	// read.
+	Sleep time.Duration
+}
+
+func (cfg Config) makeErrCh() chan error {
+	if cfg.MakeErrCh {
+		return make(chan error, 1)
+	}
+	return nil
+}
