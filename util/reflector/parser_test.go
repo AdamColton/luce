4445286e02324a0d538a63982ca7b99a5460cc5f
Adam Colton <adamcolton@gmail.com>
2023-10-10 18:55:56 -0400

    +util/reflector.Parser.ParseFieldName

--- util/reflector/parser_test.go
+++ util/reflector/parser_test.go
@@ -10,6 +10,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type person struct {
+	Name string
+	Age  int
+}
+
 func TestParser(t *testing.T) {
 	uintPtr := reflector.Type[*uint]()
 	strPtr := reflector.Type[*string]()
@@ -46,4 +51,33 @@ func TestParser(t *testing.T) {
 	err = pf.Parser(reflect.ValueOf(&u), "should error")
 	expectErr := lerr.TypeMismatch(strPtr, uintPtr)
 	assert.Equal(t, expectErr, err)
+	psn := &person{}
+	err = p.ParseFieldName(psn, "Name", "Adam")
+	assert.NoError(t, err)
+	assert.Equal(t, "Adam", psn.Name)
+
+	err = p.ParseFieldName(psn, "Age", "39")
+	assert.NoError(t, err)
+	assert.Equal(t, 39, psn.Age)
+}
+
+func TestParseValueFieldNameErrs(t *testing.T) {
+	p := reflector.Parser[string]{}
+	reflector.ParserAdd(p, parsers.String)
+	reflector.ParserAdd(p, parsers.Float64)
+	reflector.ParserAdd(p, parsers.Int)
+
+	type Person struct {
+		Name string
+		Age  int
+	}
+	var person person
+	err := p.ParseValueFieldName(reflect.ValueOf(person), "Age", "39")
+	assert.Equal(t, reflector.ErrExpectedPtr, err)
+
+	err = p.ParseValueFieldName(reflect.ValueOf(&(person.Age)), "Age", "39")
+	assert.Equal(t, reflector.ErrExpectedStruct, err)
+
+	err = p.ParseValueFieldName(reflect.ValueOf(&person), "Role", "admin")
+	assert.Equal(t, "field 'Role' not found", err.Error())
 }
