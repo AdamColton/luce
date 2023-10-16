e23e4b3b549cee8d197e43130c46ebf4f663714e
Adam Colton <adamcolton@gmail.com>
2023-10-16 13:10:52 -0400

    +util/reflector.ltype

---
+++ util/reflector/ltype/types.go
@@ -0,0 +1,30 @@
+package ltype
+
+import (
+	"reflect"
+
+	"github.com/adamcolton/luce/util/reflector"
+)
+
+var (
+	Err       = reflector.Type[error]()
+	String    = reflector.Type[string]()
+	Byte      = reflector.Type[byte]()
+	Bool      = reflector.Type[bool]()
+	ByteSlice = reflect.SliceOf(Byte)
+
+	Int   = reflector.Type[int]()
+	Int8  = reflector.Type[int8]()
+	Int16 = reflector.Type[int16]()
+	Int32 = reflector.Type[int32]()
+	Int64 = reflector.Type[int64]()
+
+	Uint   = reflector.Type[uint]()
+	Uint8  = reflector.Type[uint8]()
+	Uint16 = reflector.Type[uint16]()
+	Uint32 = reflector.Type[uint32]()
+	Uint64 = reflector.Type[uint64]()
+
+	Float32 = reflector.Type[float32]()
+	Float64 = reflector.Type[float64]()
+)
