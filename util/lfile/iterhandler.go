cfff56f2c75578088c0f428ab5a23c5883886451
Adam Colton <adamcolton@gmail.com>
2023-10-17 19:47:41 -0400

    +util/lfile.IterHandler

---
+++ util/lfile/iterhandler.go
@@ -0,0 +1,25 @@
+package lfile
+
+// IterHandler represents something that will handle each value in the iterator.
+type IterHandler interface {
+	HandleIter(Iterator)
+}
+
+// RunHandler will create an Iter from Iterator and call the HandleIter method
+// on the IterHandler for each value in the iterator.
+func RunHandlerSource(ii IteratorSource, ih IterHandler) error {
+	i, done := ii.Iterator()
+	for ; !done; _, done = i.Next() {
+		ih.HandleIter(i)
+	}
+	return i.Err()
+}
+
+// RunHandler will create an Iter from Iterator and call the HandleIter method
+// on the IterHandler for each value in the iterator.
+func RunHandler(i Iterator, ih IterHandler) error {
+	for done := i.Reset(); !done; _, done = i.Next() {
+		ih.HandleIter(i)
+	}
+	return i.Err()
+}
