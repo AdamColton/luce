04a5ce01145d82a0b29817a8f5ab82bf40eab6b9
Adam Colton <adamcolton@gmail.com>
2023-10-17 19:41:08 -0400

    +util/lfile.PathLength

--- util/lfile/path_test.go
+++ util/lfile/path_test.go
@@ -6,6 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPathLength(t *testing.T) {
+	tt := map[string]string{
+		"this/is/a/test.txt": "is/a/test.txt",
+		"a/test.txt":         "a/test.txt",
+		"/a/test.txt":        "a/test.txt",
+	}
+
+	pl := PathLength(3)
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			assert.Equal(t, tc, pl.Trim(n))
+		})
+	}
+}
+
 func TestName(t *testing.T) {
 	tt := map[string][2]string{
 		"/foo/bar.txt": {"/foo/", "bar.txt"},
