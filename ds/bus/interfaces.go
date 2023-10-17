47010a90f7e3851bbef64683537d8b8764e12245
Adam Colton <adamcolton@gmail.com>
2023-10-17 14:03:28 -0400

    +ds/bus/listenerswitch

--- ds/bus/interfaces.go
+++ ds/bus/interfaces.go
@@ -1,5 +1,18 @@
 package bus
 
+import (
+	"github.com/adamcolton/luce/util/handler"
+)
+
+// ListenerSwitcher takes values off an interface channel and multiplexes them out
+// to the correct handlers for the given type.
+type ListenerSwitcher interface {
+	handler.Switcher
+	Run()
+	SetIn(<-chan any)
+	SetErrorHandler(any) error
+}
+
 // Receiver receives data from a bus translates it to a value and retransmits
 // the value on an interface bus. For example, it may receive data as a byte
 // slice, deserialize to a value and retransmit the value.
