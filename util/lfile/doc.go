00f8ac74de6a15f2f2d9369f567037c2973fff5b
Adam Colton <adamcolton@gmail.com>
2023-10-17 19:37:51 -0400

    +util.lfile doc

---
+++ util/lfile/doc.go
@@ -0,0 +1,5 @@
+// Package lfile provides utilities around file interactions beyond those in the
+// standard library.
+//
+// For now, the Search functionality of Match is more of placeholder.
+package lfile
