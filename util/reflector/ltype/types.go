f1cde187d4f7467ca5c74ef1924c72cca6857713
Adam Colton <adamcolton@gmail.com>
2023-10-26 14:09:38 -0400

    +util/reflector/ltype.CheckStructPtr

--- util/reflector/ltype/types.go
+++ util/reflector/ltype/types.go
@@ -28,3 +28,16 @@ var (
 	Float32 = reflector.Type[float32]()
 	Float64 = reflector.Type[float64]()
 )
+
+// CheckStructPtr returns nil if t is not a pointer to a struct. If it is, it
+// returns the struct.
+func CheckStructPtr(t reflect.Type) reflect.Type {
+	if t == nil || t.Kind() != reflect.Ptr {
+		return nil
+	}
+	t = t.Elem()
+	if t.Kind() != reflect.Struct {
+		return nil
+	}
+	return t
+}
