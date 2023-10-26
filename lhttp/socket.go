4696b245b923688678a7882ce77b1b677e02c295
Adam Colton <adamcolton@gmail.com>
2023-10-26 14:53:50 -0400

    lhttp.Socket convert to reader/writer

--- lhttp/socket.go
+++ lhttp/socket.go
@@ -1,37 +1,41 @@
 package lhttp
 
+import (
+	"github.com/adamcolton/luce/math/cmpr"
+)
+
 // Socket is intended to be a websocket
 type Socket struct {
 	MessageReaderWriter
+	buf []byte
 }
 
 // NewSocket creates a Socket. It is intended to be used with a websocket.
-func NewSocket(socket MessageReaderWriter) Socket {
-	return Socket{
+func NewSocket(socket MessageReaderWriter) *Socket {
+	return &Socket{
 		MessageReaderWriter: socket,
 	}
 }
 
-// RunReader reads from the socket until it encounters an error and writes each
-// msg to the "from" channel.
-func (socket Socket) RunReader(from chan<- []byte) {
-	for {
-		_, msg, err := socket.ReadMessage()
+func (socket *Socket) Read(p []byte) (n int, err error) {
+	ln := len(socket.buf)
+	if ln == 0 {
+		_, socket.buf, err = socket.ReadMessage()
 		if err != nil {
-			break
+			return 0, err
 		}
-		from <- msg
 	}
-	close(from)
+
+	ln = cmpr.Min(ln, len(p))
+	copy(p, socket.buf[:ln])
+	socket.buf = socket.buf[ln:]
+	return ln, nil
 }
 
-// RunSender pulls messages off the "to" channel and writes them to the
-// websocket.
-func (socket Socket) RunSender(to <-chan []byte) {
-	for msg := range to {
-		err := socket.WriteMessage(1, msg)
-		if err != nil {
-			break
-		}
+func (socket *Socket) Write(p []byte) (n int, err error) {
+	err = socket.WriteMessage(1, p)
+	if err == nil {
+		n = len(p)
 	}
+	return
 }
