6741b9ab2c56d60c39d61e24166242b689dd19ed
Adam Colton <adamcolton@gmail.com>
2023-10-26 14:16:16 -0400

    +util/reflector/ltype/httptype

---
+++ util/reflector/ltype/httptype/httptypes.go
@@ -0,0 +1,17 @@
+package httptype
+
+import (
+	"net/http"
+
+	"github.com/adamcolton/luce/util/reflector"
+)
+
+var (
+	ResponseWriter = reflector.Type[http.ResponseWriter]()
+	Request        = reflector.Type[*http.Request]()
+	HandlerFunc    = reflector.Type[http.HandlerFunc]()
+	Handler        = reflector.Type[http.Handler]()
+	Header         = reflector.Type[http.Header]()
+	Client         = reflector.Type[*http.Client]()
+	Server         = reflector.Type[*http.Server]()
+)
