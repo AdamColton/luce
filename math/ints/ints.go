38090c2057d0cb10ee214a4640261887d57f1131
Adam Colton <adamcolton@gmail.com>
2023-10-30 10:22:43 -0400

    +math/ints.((Max(I,U),MinI)(_,8,16,32,64))

--- math/ints/ints.go
+++ math/ints/ints.go
@@ -2,6 +2,28 @@ package ints
 
 import "golang.org/x/exp/constraints"
 
+// Provide Max value for all integer types and min value for all signed
+// integers.
+const (
+	MaxU   uint   = ^uint(0)
+	MaxU8  uint8  = ^uint8(0)
+	MaxU16 uint16 = ^uint16(0)
+	MaxU32 uint32 = ^uint32(0)
+	MaxU64 uint64 = ^uint64(0)
+
+	MaxI   int   = int(MaxU >> 1)
+	MaxI8  int8  = int8(MaxU8 >> 1)
+	MaxI16 int16 = int16(MaxU16 >> 1)
+	MaxI32 int32 = int32(MaxU32 >> 1)
+	MaxI64 int64 = int64(MaxU64 >> 1)
+
+	MinI   int   = ^MaxI
+	MinI8  int8  = ^MaxI8
+	MinI16 int16 = ^MaxI16
+	MinI32 int32 = ^MaxI32
+	MinI64 int64 = ^MaxI64
+)
+
 // DivUp returns a/b rounding up.
 func DivUp[T constraints.Integer](a, b T) T {
 	out := a / b
