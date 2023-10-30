0a4003ffcab89de9dcb20a4ba74ac17a54ea6a23
Adam Colton <adamcolton@gmail.com>
2023-10-30 13:32:21 -0400

    +math/ints.(GCD, GCDX, LCM)

--- math/ints/ints.go
+++ math/ints/ints.go
@@ -40,3 +40,39 @@ func Mod[T constraints.Integer](a, b T) T {
 	}
 	return a % b
 }
+
+func GCD[T constraints.Integer](a, b T) T {
+	gcd, _, _ := GCDX(a, b)
+	return gcd
+}
+
+func GCDX[T constraints.Integer](a, b T) (gcd, x, y T) {
+	if a == 0 {
+		return b, 0, 1
+	}
+	gcd, u, v := GCDX(b%a, a)
+
+	x = v - (b/a)*u
+	y = u
+
+	return gcd, x, y
+}
+
+func LCM[T constraints.Integer](a, b T) T {
+	return (a / GCD(a, b)) * b
+}
+
+func Int[T constraints.Integer](i T) int     { return int(i) }
+func Int8[T constraints.Integer](i T) int8   { return int8(i) }
+func Int16[T constraints.Integer](i T) int16 { return int16(i) }
+func Int32[T constraints.Integer](i T) int32 { return int32(i) }
+func Int64[T constraints.Integer](i T) int64 { return int64(i) }
+
+func Uint[T constraints.Integer](i T) uint     { return uint(i) }
+func Uint8[T constraints.Integer](i T) uint8   { return uint8(i) }
+func Uint16[T constraints.Integer](i T) uint16 { return uint16(i) }
+func Uint32[T constraints.Integer](i T) uint32 { return uint32(i) }
+func Uint64[T constraints.Integer](i T) uint64 { return uint64(i) }
+
+func Float32[T constraints.Integer](i T) float32 { return float32(i) }
+func Float64[T constraints.Integer](i T) float64 { return float64(i) }
