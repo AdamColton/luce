7583e6a26d7c1b91e8bd43b0c6e6136cc0ba853f
Adam Colton <adamcolton@gmail.com>
2023-11-19 22:33:03 -0500

    +math/cmpr.Tolerance

---
+++ math/cmpr/tolerance.go
@@ -0,0 +1,29 @@
+package cmpr
+
+// Tolerance represents a difference below which two floats can be considered
+// equal.
+type Tolerance float64
+
+// DefaultTolerance adjusts how close values must be to be considered equal
+var DefaultTolerance Tolerance = 1e-5
+
+// Equal returns true if a and b are within Tolerance t of eachother.
+func (t Tolerance) Equal(a, b float64) bool {
+	return t.Zero(a - b)
+}
+
+// Zero returns true if x is within Tolerance t of value of 0.
+func (t Tolerance) Zero(x float64) bool {
+	z := Tolerance(x)
+	return z < t && z > -t
+}
+
+// Equal returns true if a and b are within the DefaultTolerance of eachother.
+func Equal(a, b float64) bool {
+	return DefaultTolerance.Equal(a, b)
+}
+
+// Zero returns true if x is within the DefaultTolerance of 0.
+func Zero(x float64) bool {
+	return DefaultTolerance.Zero(x)
+}
