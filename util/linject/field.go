6034be91e4deefb670e79254c563ef8fb00bfa92
Adam Colton <adamcolton@gmail.com>
2023-11-19 22:30:23 -0500

    +util/linject.NewFieldSetter

--- util/linject/field.go
+++ util/linject/field.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/util/filter"
+	"github.com/adamcolton/luce/util/reflector"
+	"github.com/adamcolton/luce/util/reflector/ltype"
 )
 
 // FieldSetterInitilizer is called by FieldInitilizer to handle any Type
@@ -75,4 +77,62 @@ func (fs FieldInitilizer) Initilize(fn Func) DataInserter {
 
 func (fi *fieldInserter) Insert(args []reflect.Value) (callback func(), err error) {
 	return fi.Set(args, args[fi.argsIdx].Elem().FieldByIndex(fi.idx))
+}
+
+type outFieldSetter struct {
+	set func(args []reflect.Value, field reflect.Value) (callback func(), err error)
+}
+
+func (ofs outFieldSetter) Set(args []reflect.Value, field reflect.Value) (callback func(), err error) {
+	return ofs.set(args, field)
+}
+
+var (
+	fnType              = reflector.Type[func()]()
+	outFieldSetterCheck = filter.NumOutEq(3).
+				And(filter.IsType(fnType).Out(1)).
+				And(filter.IsType(ltype.Err).Out(2)).
+				Check(filter.TypeErr("exptedted func([]reflect.Value)(t T,callback func(), err error), got %s"))
+
+	valueSliceType = reflector.Type[[]reflect.Value]()
+	wrappedArgsFn  = filter.NumInEq(1).
+			And(filter.IsType(valueSliceType).In(0)).Filter
+)
+
+// NewFieldSetter takes a function and converts it to a field setter. The
+// function must have 3 returns. The first is the value the field will be set
+// to. The second is the callback function and the third is an error.
+//
+// The arguments can either have a single argument of []reflect.Value in which
+// case the arguments will be passed along. Or if it expects a specific argument
+// pattern for the function, it can match the leading arguments. For instance, a
+// FieldSetter on an HttpHandler could have arguments of (w http.ResponseWriter,
+// r *http.Request)
+func NewFieldSetter(fn any) FieldSetter {
+	outFieldSetterCheck.Panic(fn)
+	fnv := reflect.ValueOf(fn)
+	var getArgs func([]reflect.Value) []reflect.Value
+	t := fnv.Type()
+	if wrappedArgsFn(t) {
+		getArgs = func(args []reflect.Value) []reflect.Value {
+			return []reflect.Value{reflect.ValueOf(args)}
+		}
+	} else {
+		getArgs = func(args []reflect.Value) []reflect.Value {
+			return args[:t.NumIn()]
+		}
+	}
+	return outFieldSetter{
+		set: func(args []reflect.Value, field reflect.Value) (callback func(), err error) {
+			fnArgs := getArgs(args)
+			out := fnv.Call(fnArgs)
+			reflector.Set(field, out[0])
+			callback = out[1].Interface().(func())
+			i := out[2].Interface()
+			if i != nil {
+				err = i.(error)
+			}
+			return
+		},
+	}
 }
