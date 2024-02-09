d1c5fb84d792c07c5db06af5a8c3bfe9e5502df6
Adam Colton <adamcolton@gmail.com>
2024-02-08 21:35:28 -0500

    +util/lstr.Strings (fulfills Iter)

---
+++ util/lstr/strings.go
@@ -0,0 +1,80 @@
+package lstr
+
+import "strings"
+
+// Strings is helpful when processing a list of strings, often the result of
+// splitting.
+type Strings struct {
+	Strings    []string
+	Err        error
+	Preprocess func(string) (skip bool, cleaned string)
+
+	idx int
+	cur string
+}
+
+var DefaultPreprocess = func(str string) (skip bool, cleaned string) {
+	cleaned = strings.TrimSpace(str)
+	skip = cleaned == ""
+	return
+}
+
+func NewStrings(strs []string) *Strings {
+	return (&Strings{
+		Strings:    strs,
+		Preprocess: DefaultPreprocess,
+	}).init()
+}
+
+func (s *Strings) Len() int {
+	return len(s.Strings)
+}
+
+func (s *Strings) Next() (str string, done bool) {
+	done = s.inc()
+	for ; !done && s.setCur(); done = s.inc() {
+	}
+	if done {
+		s.cur = ""
+	}
+
+	return s.cur, done
+}
+
+func (s *Strings) inc() (done bool) {
+	s.idx++
+	return s.Done()
+}
+
+func (s *Strings) setCur() (skip bool) {
+	s.cur = s.Strings[s.idx]
+	if s.Preprocess != nil {
+		skip, s.cur = s.Preprocess(s.cur)
+	}
+	return skip
+}
+
+func (s *Strings) init() *Strings {
+	if !s.Done() && s.setCur() {
+		s.Next()
+	}
+	return s
+}
+
+func (s *Strings) Cur() (str string, done bool) {
+	return s.cur, s.Done()
+}
+
+func (s *Strings) Done() bool {
+	return s == nil || s.Err != nil || s.idx >= s.Len()
+}
+
+func (s *Strings) Idx() int {
+	return s.idx
+}
+
+func (s *Strings) Start() (str string, done bool) {
+	s.idx = 0
+	s.init()
+	return s.Cur()
+}
