83695bf1dcc0841287d83a80857f33fe7074c0b7
Adam Colton <adamcolton@gmail.com>
2024-02-08 22:36:04 -0500

    +util/lstr.NewRemover

--- util/lstr/lstr.go
+++ util/lstr/lstr.go
@@ -1,5 +1,16 @@
 package lstr
 
+import "strings"
+
 func Len(s string) int {
 	return len(s)
+}
+
+func NewRemover(rm ...string) *strings.Replacer {
+	s := make([]string, len(rm)*2)
+	for i, r := range rm {
+		s[i*2] = r
+		s[i*2+1] = ""
+	}
+	return strings.NewReplacer(s...)
 }
