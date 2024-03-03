789d4a009fe149129f5db165da6e9ccca0f92a3d
Adam Colton <adamcolton@gmail.com>
2024-03-03 09:35:35 -0500

    +util/lstr.Glue

--- util/lstr/lstr_test.go
+++ util/lstr/lstr_test.go
@@ -11,3 +11,8 @@ func TestLen(t *testing.T) {
 	str := "testing"
 	assert.Equal(t, len(str), lstr.Len(str))
 }
+
+func TestGlue(t *testing.T) {
+	got := lstr.Glue("This", "is", "a", "test")
+	assert.Equal(t, "Thisisatest", got)
+}
