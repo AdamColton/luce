a01ed134be2b417eedb1eca7f1577d461fe9dd3f
Adam Colton <adamcolton@gmail.com>
2024-03-03 09:35:35 -0500

    +util/lstr.Glue

--- util/lstr/lstr.go
+++ util/lstr/lstr.go
@@ -1,6 +1,10 @@
 package lstr
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/adamcolton/luce/math/ints"
+)
 
 // Len creates a strongly typed version of builtin len for strings.
 func Len(s string) int {
@@ -16,4 +20,28 @@ func NewRemover(rm ...string) *strings.Replacer {
 		s[i*2+1] = ""
 	}
 	return strings.NewReplacer(s...)
+}
+
+// Glue strings together with no joining string. Equivalent to
+// strings.Join(strs, "")
+func Glue(strs ...string) string {
+	switch len(strs) {
+	case 0:
+		return ""
+	case 1:
+		return strs[0]
+	}
+
+	var ln int
+	for _, s := range strs {
+		if len(s) > ints.MaxI-ln {
+			panic("lstr: Glue output length overflow")
+		}
+		ln += len(s)
+	}
+	out := make([]byte, 0, ln)
+	for _, s := range strs {
+		out = append(out, s...)
+	}
+	return string(out)
 }
