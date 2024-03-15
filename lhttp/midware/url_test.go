4b2c90d40b77361d423d8ecbe0e749848635b093
Adam Colton <adamcolton@gmail.com>
2024-03-15 19:02:05 -0400

    +lhttp/midware.URL

---
+++ lhttp/midware/url_test.go
@@ -0,0 +1,36 @@
+package midware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/adamcolton/luce/lhttp/midware"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestURL(t *testing.T) {
+	u := midware.URL("foo", "TestField")
+	m := midware.New(u)
+
+	restoreVars := midware.Vars
+	defer func() {
+		midware.Vars = restoreVars
+	}()
+	midware.Vars = func(r *http.Request) map[string]string {
+		return map[string]string{
+			"foo": "bar",
+		}
+	}
+
+	r := httptest.NewRequest("Get", "/", nil)
+	w := httptest.NewRecorder()
+	fn := m.Handle(func(w http.ResponseWriter, r *http.Request, data *struct {
+		TestField string
+	}) {
+		w.Write([]byte(data.TestField))
+	})
+	fn(w, r)
+
+	assert.Equal(t, w.Body.String(), "bar")
+}
