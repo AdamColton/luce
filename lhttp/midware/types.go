8ca97ad62a44de774d3fa37713e937b7dd9d298b
Adam Colton <adamcolton@gmail.com>
2024-03-15 18:58:45 -0400

    +lhttp/midware.NewDecoder

--- lhttp/midware/types.go
+++ lhttp/midware/types.go
@@ -2,6 +2,7 @@ package midware
 
 import (
 	"github.com/adamcolton/luce/util/filter"
+	"github.com/adamcolton/luce/util/linject"
 	"github.com/adamcolton/luce/util/reflector/ltype/httptype"
 )
 
@@ -10,3 +11,9 @@ var (
 		And(filter.InType(0, httptype.ResponseWriter)).
 		And(filter.InType(1, httptype.Request))
 )
+
+func NewField(fsi linject.FieldInitilizer, fieldName string) linject.Field {
+	fi := linject.NewField(fsi, fieldName)
+	fi.FuncType = HttpHandlerType
+	return fi
+}
