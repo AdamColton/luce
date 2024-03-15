46191f953ba6c5bbd554f226a2aa020e90423d30
Adam Colton <adamcolton@gmail.com>
2024-03-15 19:01:13 -0400

    +lhttp/midware.NewRedirect

--- lhttp/midware/types.go
+++ lhttp/midware/types.go
@@ -1,6 +1,9 @@
 package midware
 
 import (
+	"net/http"
+	"reflect"
+
 	"github.com/adamcolton/luce/util/filter"
 	"github.com/adamcolton/luce/util/linject"
 	"github.com/adamcolton/luce/util/reflector/ltype/httptype"
@@ -11,6 +14,12 @@ var (
 		And(filter.InType(0, httptype.ResponseWriter)).
 		And(filter.InType(1, httptype.Request))
 )
+
+func GetWR(args []reflect.Value) (w http.ResponseWriter, r *http.Request) {
+	w = args[0].Interface().(http.ResponseWriter)
+	r = args[1].Interface().(*http.Request)
+	return
+}
 
 func NewField(fsi linject.FieldInitilizer, fieldName string) linject.Field {
 	fi := linject.NewField(fsi, fieldName)
