f807f0027e818fb716e91576611a5eec06e2a60d
Adam Colton <adamcolton@gmail.com>
2024-03-20 09:46:01 -0400

    +example/commands

---
+++ example/commands/logic/rwhandler.go
@@ -0,0 +1,50 @@
+package logic
+
+import (
+	"strconv"
+
+	"github.com/adamcolton/luce/util/cli"
+)
+
+func New(ec *cli.ExitClose) *HandlerObject {
+	return &HandlerObject{
+		ExitCloseHandler: ec.Commands(),
+		Timeout:          25,
+		Helper:           "List all commands",
+	}
+}
+
+func (ho *HandlerObject) Runner() *cli.Runner {
+	r := cli.NewRunner(ho, nil)
+	r.Prompt = "> "
+	r.StartMessage = "Welcome to the commands demo\nenter 'help' for more\n"
+	return r
+}
+
+func (ho *HandlerObject) Handlers(rnr *cli.Runner) []any {
+	return []any{
+		func(r *PersonResp) {
+			rnr.WriteStrings("Created Person: ", r.Name)
+		},
+		func(r string) {
+			rnr.WriteString(r)
+		},
+		func(r []string) {
+			for i, c := range r {
+				if i > 0 {
+					rnr.WriteString("\n")
+				}
+				rnr.WriteStrings(" * ", c)
+			}
+		},
+		func(r int) {
+			rnr.WriteStrings(strconv.Itoa(r), " empty requests")
+		},
+		func(r *SetTimeoutResp) {
+			rnr.WriteStrings("Timeout set to ", strconv.Itoa(r.Timeout))
+		},
+		rnr.ExitRespHandler,
+		rnr.CloseRespHandler,
+		rnr.HelpRespHandler,
+	}
+}
