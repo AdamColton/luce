c43ebc97e77d15429d382081c5161e6dbe45f5d9
Adam Colton <adamcolton@gmail.com>
2024-03-21 12:45:54 -0400

    +ds/channel.Slice

--- ds/channel/channel.go
+++ ds/channel/channel.go
@@ -20,3 +20,18 @@ func Timeout[T any](ms int, ch <-chan T) (t T, err error) {
 	}
 	return
 }
+
+// Slice places the contents of a slice on a channel. This is done as a blocking
+// operation, however, if ch is nil a channel is created with a buffer large
+// enough to hold the slice and it will not block, returning the populated
+// channel.
+func Slice[T any](s []T, ch chan<- T) (out chan T) {
+	if ch == nil {
+		out = make(chan T, len(s))
+		ch = out
+	}
+	for _, t := range s {
+		ch <- t
+	}
+	return
+}
