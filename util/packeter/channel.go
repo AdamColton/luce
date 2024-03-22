b84d552fbe7802ab11661c7e33a5a70b8dbffae6
Adam Colton <adamcolton@gmail.com>
2024-03-21 21:22:52 -0400

    +util/packeter.Run

--- util/packeter/channel.go
+++ util/packeter/channel.go
@@ -1,6 +1,8 @@
 package packeter
 
-import "github.com/adamcolton/luce/ds/channel"
+import (
+	"github.com/adamcolton/luce/ds/channel"
+)
 
 func UnpackPipe(u Unpacker, chs channel.Pipe[[]byte], cls bool) {
 	for msg := range chs.Rcv {
@@ -18,4 +20,19 @@ func PackPipe(p Packer, chs channel.Pipe[[]byte], cls bool) {
 	if cls {
 		close(chs.Snd)
 	}
+}
+
+func Run(p any, chs channel.Pipe[[]byte]) (out channel.Pipe[[]byte]) {
+	if packer, ok := p.(Packer); ok && chs.Snd != nil {
+		pp, snd, _ := channel.NewPipe(nil, chs.Snd)
+		out.Snd = snd
+		go PackPipe(packer, pp, true)
+	}
+	if unpacker, ok := p.(Unpacker); ok && chs.Rcv != nil {
+		up, _, rcv := channel.NewPipe(chs.Rcv, nil)
+		out.Rcv = rcv
+		go UnpackPipe(unpacker, up, true)
+	}
+
+	return
 }
