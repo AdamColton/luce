151cb5aac7bd007e65b5e319c15ad6fd7df6fe43
Adam Colton <adamcolton@gmail.com>
2024-03-27 10:06:30 -0400

    +math/ints Conversion funcs

--- math/ints/ints_test.go
+++ math/ints/ints_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/adamcolton/luce/math/ints"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/constraints"
 )
 
 func TestDiv(t *testing.T) {
@@ -84,4 +85,32 @@ func TestLCM(t *testing.T) {
 	a := 2 * 5
 	b := 3 * 5
 	assert.Equal(t, expected, ints.LCM(a, b))
+}
+
+func TestConversions(t *testing.T) {
+	testConversions(t, int(5))
+	testConversions(t, int8(5))
+	testConversions(t, int16(5))
+	testConversions(t, int32(5))
+	testConversions(t, int64(5))
+	testConversions(t, uint(5))
+	testConversions(t, uint8(5))
+	testConversions(t, uint16(5))
+	testConversions(t, uint32(5))
+	testConversions(t, uint64(5))
+}
+
+func testConversions[I constraints.Integer](t *testing.T, five I) {
+	assert.Equal(t, int(5), ints.Int(five))
+	assert.Equal(t, int8(5), ints.Int8(five))
+	assert.Equal(t, int16(5), ints.Int16(five))
+	assert.Equal(t, int32(5), ints.Int32(five))
+	assert.Equal(t, int64(5), ints.Int64(five))
+	assert.Equal(t, uint(5), ints.Uint(five))
+	assert.Equal(t, uint8(5), ints.Uint8(five))
+	assert.Equal(t, uint16(5), ints.Uint16(five))
+	assert.Equal(t, uint32(5), ints.Uint32(five))
+	assert.Equal(t, uint64(5), ints.Uint64(five))
+	assert.Equal(t, float32(5), ints.Float32(five))
+	assert.Equal(t, float64(5), ints.Float64(five))
 }
