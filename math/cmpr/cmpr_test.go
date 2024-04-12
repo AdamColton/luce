94aea556b56443f770d51daeb69c641130a1d503
Adam Colton <adamcolton@gmail.com>
2024-04-12 17:20:14 -0400

    +math/cmpr.Tolerance.Unique

--- math/cmpr/cmpr_test.go
+++ math/cmpr/cmpr_test.go
@@ -27,3 +27,63 @@ func TestMaxN(t *testing.T) {
 	assert.Equal(t, 9, cmpr.MaxN(3, 1, 4, 1, 5, 9))
 	assert.Equal(t, 0, cmpr.MaxN[int]())
 }
+
+func TestUnique(t *testing.T) {
+	tt := map[string]struct {
+		in, expected []float64
+		cmpr.Tolerance
+	}{
+		"no-change": {
+			in:        []float64{1, 2, 3, 4, 5},
+			expected:  []float64{1, 2, 3, 4, 5},
+			Tolerance: 1e-3,
+		},
+		"tail-equal": {
+			in:        []float64{1, 2, 3, 4, 5, 5},
+			expected:  []float64{1, 2, 3, 4, 5},
+			Tolerance: 1e-3,
+		},
+		"head-equal": {
+			in:        []float64{1, 1, 2, 3, 4, 5},
+			expected:  []float64{1, 2, 3, 4, 5},
+			Tolerance: 1e-3,
+		},
+		"double": {
+			in:        []float64{1, 1, 2, 2, 3, 3, 4, 4, 5, 5},
+			expected:  []float64{1, 2, 3, 4, 5},
+			Tolerance: 1e-3,
+		},
+		"close": {
+			in:        []float64{1, 2, 3, 3.0006, 4, 5},
+			expected:  []float64{1, 2, 3, 4, 5},
+			Tolerance: 1e-3,
+		},
+		"close-chain": {
+			in:        []float64{1, 2, 3, 3.0006, 3.0012, 3.0018, 4, 5},
+			expected:  []float64{1, 2, 3, 3.0012, 4, 5},
+			Tolerance: 1e-3,
+		},
+		"zero": {
+			in:        []float64{},
+			expected:  []float64{},
+			Tolerance: 1e-3,
+		},
+		"one": {
+			in:        []float64{1},
+			expected:  []float64{1},
+			Tolerance: 1e-3,
+		},
+		"regression": {
+			in:        []float64{0, 0, 1.0 / 3.0},
+			expected:  []float64{0, 1.0 / 3.0},
+			Tolerance: 1e-6,
+		},
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			got := tc.Unique(tc.in)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
