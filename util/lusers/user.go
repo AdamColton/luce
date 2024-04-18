29eecee3a8f8634c579c2a0244fa862c761f8013
Adam Colton <adamcolton@gmail.com>
2024-04-17 20:58:40 -0400

    +util/lusers.User.OneRequired and more

--- util/lusers/user.go
+++ util/lusers/user.go
@@ -3,6 +3,8 @@ package lusers
 import (
 	"sort"
 
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/adamcolton/luce/util/filter"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,7 +12,7 @@ type User struct {
 	ID             []byte `json:"-"`
 	Name           string
 	HashedPassword []byte
-	Groups         []string
+	Groups         slice.Slice[string]
 }
 
 func (u *User) SetPassword(password string) error {
@@ -27,17 +29,43 @@ func (u *User) CheckPassword(password string) error {
 }
 
 func (u *User) In(group string) bool {
-	idx := sort.Search(len(u.Groups), func(i int) bool {
-		return u.Groups[i] >= group
-	})
-	if idx < 0 || idx >= len(u.Groups) {
+	if u == nil {
 		return false
 	}
-	return u.Groups[idx] == group
+	idx := u.gidx(group)
+	return u.Groups.IdxCheck(idx) && u.Groups[idx] == group
+}
+
+func (u *User) gidx(group string) int {
+	return u.Groups.Search(filter.GTE(group))
 }
 
 func (u *User) sortGroups() {
 	sort.Slice(u.Groups, func(i, j int) bool {
 		return u.Groups[i] < u.Groups[j]
 	})
+}
+
+// OneRequired requires that a user be in one of the listed groups. If no groups
+// are listed, the result is true.
+func (u *User) OneRequired(groups []string) bool {
+	if len(groups) == 0 {
+		return true
+	}
+	if u == nil {
+		return false
+	}
+
+	var group string
+	fn := func(g string) bool {
+		return g >= group
+	}
+
+	for _, group = range groups {
+		idx := u.Groups.Search(fn)
+		if u.Groups.IdxCheck(idx) && u.Groups[idx] == group {
+			return true
+		}
+	}
+	return false
 }
