5690e3a771f2e0d31a7c24cd32b38894a2d3fb0c
Adam Colton <adamcolton@gmail.com>
2024-04-18 16:05:12 -0400

    +ds/lmap.Map.Pop

--- ds/lmap/lmap.go
+++ ds/lmap/lmap.go
@@ -5,3 +5,11 @@ type Map[K comparable, V any] map[K]V
 func (m Map[K, V]) Len() int {
 	return len(m)
 }
+
+func (m Map[K, V]) Pop(key K) (V, bool) {
+	v, found := m[key]
+	if found {
+		delete(m, key)
+	}
+	return v, found
+}
