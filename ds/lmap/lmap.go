1deaad56e3e0c2eba78e47fa7a6bf8498148e61f
Adam Colton <adamcolton@gmail.com>
2024-04-18 16:05:12 -0400

    +ds/lmap.Map.Pop

--- ds/lmap/lmap.go
+++ ds/lmap/lmap.go
@@ -9,3 +9,11 @@ func New[K comparable, V any](m map[K]V) Map[K, V] {
 func (m Map[K, V]) Len() int {
 	return len(m)
 }
+
+func (m Map[K, V]) Pop(key K) (V, bool) {
+	v, found := m[key]
+	if found {
+		delete(m, key)
+	}
+	return v, found
+}
--- ds/lmap/lmap_test.go
+++ ds/lmap/lmap_test.go
@@ -11,3 +11,20 @@ func TestLen(t *testing.T) {
 	m := lmap.Map[int, string]{1: "1", 2: "2", 3: "3"}
 	assert.Equal(t, len(m), m.Len())
 }
+
+func TestPop(t *testing.T) {
+	m := lmap.Map[rune, string]{
+		'a': "apple",
+		'b': "banana",
+		'c': "cantaloupe",
+	}
+	got, ok := m.Pop('b')
+	assert.True(t, ok)
+	assert.Equal(t, "banana", got)
+	_, ok = m['b']
+	assert.False(t, ok)
+
+	got, ok = m.Pop('d')
+	assert.False(t, ok)
+	assert.Equal(t, "", got)
+}
