c5eba0f7f3f8814bc4b361e4f8b2acbf595447a8
Adam Colton <adamcolton@gmail.com>
2024-04-18 14:30:52 -0400

    +util/filter.Map.Slice

---
+++ util/filter/map.go
@@ -0,0 +1,64 @@
+package filter
+
+import (
+	"github.com/adamcolton/luce/ds/lmap"
+	"github.com/adamcolton/luce/ds/slice"
+)
+
+type Map[K comparable, V any] struct {
+	Key Filter[K]
+	Val Filter[V]
+}
+
+type Mapper[K comparable, V any] interface {
+	Each(lmap.IterFunc[K, V])
+}
+
+func NewMap[K comparable, V any](k Filter[K], v Filter[V]) Map[K, V] {
+	return Map[K, V]{
+		Key: k,
+		Val: v,
+	}
+}
+
+func (mf Map[K, V]) Filter(k K, v V) bool {
+	return (mf.Key == nil || mf.Key(k)) && (mf.Val == nil || mf.Val(v))
+}
+
+type MapSliceFlag byte
+
+const (
+	ReturnKeys = 1 << iota
+	InverseKeys
+	ReturnVals
+	InverseVals
+
+	ReturnBoth = ReturnKeys | ReturnVals
+)
+
+func (mf Map[K, V]) Slice(m Mapper[K, V], keyBuf []K, valBuf []V, flags MapSliceFlag) (keys slice.Slice[K], vals slice.Slice[V]) {
+	rk := flags&ReturnKeys == ReturnKeys
+	ik := flags&InverseKeys == InverseKeys
+	rv := flags&ReturnVals == ReturnVals
+	iv := flags&InverseVals == InverseVals
+
+	if !rk && !rv {
+		return
+	}
+	if rk {
+		keys = keyBuf[:0]
+	}
+	if rv {
+		vals = valBuf[:0]
+	}
+	m.Each(func(key K, val V, done *bool) {
+		f := mf.Filter(key, val)
+		if rk && f != ik {
+			keys = append(keys, key)
+		}
+		if rv && f != iv {
+			vals = append(vals, val)
+		}
+	})
+	return
+}
