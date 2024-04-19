ae56f770534409d87f9729791b4ec8aed8c365da
Adam Colton <adamcolton@gmail.com>
2024-04-19 15:17:21 -0400

    +ds/lmap.(Keys,Vals)

--- ds/lmap/lmap.go
+++ ds/lmap/lmap.go
@@ -1,5 +1,7 @@
 package lmap
 
+import "github.com/adamcolton/luce/ds/slice"
+
 type Map[K comparable, V any] map[K]V
 
 func New[K comparable, V any](m map[K]V) Map[K, V] {
@@ -16,4 +18,20 @@ func (m Map[K, V]) Pop(key K) (V, bool) {
 		delete(m, key)
 	}
 	return v, found
+}
+
+func (m Map[K, V]) Vals(buf slice.Slice[V]) slice.Slice[V] {
+	out := slice.NewBuffer(buf).Cap(len(m))
+	for _, v := range m {
+		out = append(out, v)
+	}
+	return out
+}
+
+func (m Map[K, V]) Keys(buf slice.Slice[K]) slice.Slice[K] {
+	out := slice.NewBuffer(buf).Cap(len(m))
+	for k := range m {
+		out = append(out, k)
+	}
+	return out
 }
