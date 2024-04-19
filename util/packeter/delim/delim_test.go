1265a9652419cf729eeba92e939867f52951816e
Adam Colton <adamcolton@gmail.com>
2024-04-18 21:04:19 -0400

    +util/packeter/delim.Unpacker.Unpack

--- util/packeter/delim/delim_test.go
+++ util/packeter/delim/delim_test.go
@@ -33,3 +33,28 @@ func TestPack(t *testing.T) {
 	}
 	assert.Equal(t, expected, got)
 }
+
+func TestUnpack(t *testing.T) {
+	d := delim.Delimiter("\n")
+	u := d.Unpacker()
+	got := u.Unpack([]byte("this"))
+	assert.Len(t, got, 0)
+	got = u.Unpack([]byte(" is"))
+	assert.Len(t, got, 0)
+	got = u.Unpack([]byte(" a"))
+	assert.Len(t, got, 0)
+	got = u.Unpack([]byte(" test\nFOOOOO"))
+	expected := [][]byte{
+		[]byte("this is a test"),
+	}
+	assert.Equal(t, expected, got)
+
+	got = u.Unpack([]byte(" 1\n2\n3\n456\n"))
+	expected = [][]byte{
+		[]byte("FOOOOO 1"),
+		[]byte("2"),
+		[]byte("3"),
+		[]byte("456"),
+	}
+	assert.Equal(t, expected, got)
+}
