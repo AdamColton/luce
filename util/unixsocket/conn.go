a045a798e626801b17229bd7f81105d06922e17e
Adam Colton <adamcolton@gmail.com>
2024-04-19 15:33:24 -0400

    +util/unixsocket.ConnPipe

---
+++ util/unixsocket/conn.go
@@ -0,0 +1,19 @@
+package unixsocket
+
+import (
+	"io"
+	"time"
+
+	"github.com/adamcolton/luce/ds/bus/iobus"
+	"github.com/adamcolton/luce/ds/channel"
+	"github.com/adamcolton/luce/util/packeter"
+	"github.com/adamcolton/luce/util/packeter/prefix"
+)
+
+func ConnPipe(conn io.ReadWriter) channel.Pipe[[]byte] {
+	rw := iobus.Config{
+		CloseOnEOF: true,
+		Sleep:      time.Millisecond,
+	}.NewReadWriter(conn)
+	return packeter.Run(prefix.New[uint32](), rw.Pipe)
+}
