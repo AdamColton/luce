cfdc24e8e91f24cd76687fb306d4f732f7e10858
Adam Colton <adamcolton@gmail.com>
2024-04-22 08:50:12 -0400

    +ds/lmap.SortKeys

--- ds/lmap/lmap.go
+++ ds/lmap/lmap.go
@@ -1,6 +1,9 @@
 package lmap
 
-import "github.com/adamcolton/luce/ds/slice"
+import (
+	"github.com/adamcolton/luce/ds/slice"
+	"golang.org/x/exp/constraints"
+)
 
 type Map[K comparable, V any] map[K]V
 
@@ -34,4 +37,12 @@ func (m Map[K, V]) Keys(buf slice.Slice[K]) slice.Slice[K] {
 		out = append(out, k)
 	}
 	return out
+}
+
+// SortKeys is a convenience function that returns the keys sorted keys.
+// This is equivalent to calling m.Keys(nil).Sort(slice.LT[K]()).
+// It assumes slice.LT for sorting and a nil buffer. If either of those
+// assumtions are not true, use Keys and Sort explicitly.
+func SortKeys[K constraints.Ordered, V any](m map[K]V) slice.Slice[K] {
+	return Map[K, V](m).Keys(nil).Sort(slice.LT[K]())
 }
