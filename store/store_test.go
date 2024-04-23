3f6f6a77a2e17de48cc95ba79628ca819981b954
Adam Colton <adamcolton@gmail.com>
2024-04-23 11:29:16 -0400

    +store.Slice

--- store/store_test.go
+++ store/store_test.go
@@ -1,9 +1,11 @@
 package store_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/adamcolton/luce/ds/idx/byteid/bytebtree"
+	"github.com/adamcolton/luce/ds/slice"
 	"github.com/adamcolton/luce/store"
 	"github.com/adamcolton/luce/store/ephemeral"
 	"github.com/stretchr/testify/assert"
@@ -14,4 +16,21 @@ func TestGetStores(t *testing.T) {
 	s, err := store.GetStoresStr(f, "this", "is", "a", "test")
 	assert.NoError(t, err)
 	assert.Equal(t, 4, s.Len())
+}
+
+func TestSlice(t *testing.T) {
+	f := ephemeral.Factory(bytebtree.New, 10)
+	s, err := f.Store([]byte("test"))
+	assert.NoError(t, err)
+
+	expected := slice.Slice[[]byte]{[]byte("this"), []byte("is"), []byte("a"), []byte("test")}
+	for _, k := range expected {
+		s.Put(k, []byte{0})
+	}
+	expected.Sort(func(i, j []byte) bool {
+		return bytes.Compare(i, j) == -1
+	})
+
+	got := store.Slice(s)
+	assert.Equal(t, expected, got)
 }
