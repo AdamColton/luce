20b4dfa34f8542f45ac1a81614aa69c5bd6140b0
Adam Colton <adamcolton@gmail.com>
2024-04-23 11:13:12 -0400

    +store.(GetStores,GetStoresStr)

---
+++ store/helpers.go
@@ -0,0 +1,24 @@
+package store
+
+import (
+	"github.com/adamcolton/luce/ds/list"
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/util/lstr"
+)
+
+var strToBytes = list.TransformAny(lstr.StringToBytes)
+
+func GetStoresStr(f FlatFactory, names ...string) (list.Wrapper[FlatStore], error) {
+	ns := strToBytes.New(slice.New(names))
+	return GetStores(f, ns)
+}
+
+func GetStores(f FlatFactory, names list.List[[]byte]) (list.Wrapper[FlatStore], error) {
+	var errs lerr.Many
+	return list.NewTransformer(names, func(name []byte, idx int) FlatStore {
+		s, err := f.FlatStore(name)
+		errs = errs.Add(err)
+		return s
+	}), errs.Cast()
+}
