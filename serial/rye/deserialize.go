75fd51445ca30bfb4b1e7e9397f5e18f1c7da8d0
Adam Colton <adamcolton@gmail.com>
2024-04-25 18:03:44 -0400

    +serial/rye.(Deserialize,Serialize).(Int(16,32,64),Float(32,64))Ordered

--- serial/rye/deserialize.go
+++ serial/rye/deserialize.go
@@ -1,5 +1,7 @@
 package rye
 
+import "unsafe"
+
 // D provides a namespace to attach the Deserialize methods.
 type D struct{}
 
@@ -11,12 +13,50 @@ func (D) Uint16(b []byte) uint16 {
 	return uint16(b[1])<<8 + uint16(b[0])
 }
 
+// Int16 decodes b as an int16
+func (d D) Int16(b []byte) int16 {
+	return int16(d.Uint16(b))
+}
+
+func (d D) preOrdered(b []byte) {
+	b[0] ^= 128
+	Reverse(b)
+}
+
+// Int16Ordered decodes b as an ordered int16
+func (d D) Int16Ordered(b []byte) int16 {
+	d.preOrdered(b[:2])
+	return d.Int16(b)
+}
+
 // Uint32 decodes b as a uint32
 func (D) Uint32(b []byte) uint32 {
 	return uint32(b[3])<<24 +
 		uint32(b[2])<<16 +
 		uint32(b[1])<<8 +
 		uint32(b[0])
+}
+
+// Int32 decodes b as an int32
+func (d D) Int32(b []byte) int32 {
+	return int32(d.Uint32(b))
+}
+
+// Int32Ordered decodes b as an ordered int32
+func (d D) Int32Ordered(b []byte) int32 {
+	d.preOrdered(b[:4])
+	return d.Int32(b)
+}
+
+// Int64 decodes b as an int64
+func (d D) Int64(b []byte) int64 {
+	return int64(d.Uint64(b))
+}
+
+// Int64Ordered decodes b as an ordered int64
+func (d D) Int64Ordered(b []byte) int64 {
+	d.preOrdered(b[:8])
+	return d.Int64(b)
 }
 
 // Uint64 decodes b as a uint64
@@ -29,4 +69,20 @@ func (D) Uint64(b []byte) uint64 {
 		uint64(b[2])<<16 +
 		uint64(b[1])<<8 +
 		uint64(b[0])
+}
+
+// Float64 decodes b as a float64
+func (D) Float64(b []byte) float64 {
+	return *(*float64)(unsafe.Pointer(&b[0]))
+}
+
+// Float64Ordered decodes b as an ordered float64
+func (d D) Float64Ordered(b []byte) float64 {
+	if b[0]&128 == 128 {
+		b[0] &= 127
+	} else {
+		Inverse(b)
+	}
+	Reverse(b[:8])
+	return d.Float64(b)
 }
--- serial/rye/serialize.go
+++ serial/rye/serialize.go
@@ -2,6 +2,7 @@ package rye
 
 import (
 	"math"
+	"unsafe"
 
 	"github.com/adamcolton/luce/ds/slice"
 )
@@ -18,12 +19,43 @@ func (S) Uint16(b []byte, x uint16) {
 	b[1] = byte(x >> 8)
 }
 
+// Int32 encodes x and writes it to b.
+func (s S) Int16(b []byte, x int16) {
+	s.Uint16(b, uint16(x))
+}
+
+func (s S) postOrdered(b []byte) {
+	Reverse(b)
+	b[0] ^= 128
+}
+
+// Int16Ordered encodes x and writes it to b so that ordering it with
+// bytes.Compare will produce the same ordering as when ordered by the int16
+// values.
+func (s S) Int16Ordered(b []byte, x int16) {
+	s.Int16(b, x)
+	s.postOrdered(b[:2])
+}
+
 // Uint32 encodes x and writes it to b.
 func (S) Uint32(b []byte, x uint32) {
 	b[0] = byte(x)
 	b[1] = byte(x >> 8)
 	b[2] = byte(x >> 16)
 	b[3] = byte(x >> 24)
+}
+
+// Int32 encodes x and writes it to b.
+func (s S) Int32(b []byte, x int32) {
+	s.Uint32(b, uint32(x))
+}
+
+// Int32Ordered encodes x and writes it to b so that ordering it with
+// bytes.Compare will produce the same ordering as when ordered by the int32
+// values.
+func (s S) Int32Ordered(b []byte, x int32) {
+	s.Int32(b, x)
+	s.postOrdered(b[:4])
 }
 
 // Uint64 encodes x and writes it to b.
@@ -36,6 +68,45 @@ func (S) Uint64(b []byte, x uint64) {
 	b[5] = byte(x >> 40)
 	b[6] = byte(x >> 48)
 	b[7] = byte(x >> 56)
+}
+
+// Int64 encodes x and writes it to b.
+func (s S) Int64(b []byte, x int64) {
+	s.Uint64(b, uint64(x))
+}
+
+// Int64Ordered encodes x and writes it to b so that ordering it with
+// bytes.Compare will produce the same ordering as when ordered by the int64
+// values.
+func (s S) Int64Ordered(b []byte, x int64) {
+	s.Int64(b, x)
+	s.postOrdered(b[:8])
+}
+
+// Float64 encodes f and writes it to b.
+func (s S) Float64(b []byte, f float64) {
+	a := *(*[8]byte)(unsafe.Pointer(&f))
+	b[0] = a[0]
+	b[1] = a[1]
+	b[2] = a[2]
+	b[3] = a[3]
+	b[4] = a[4]
+	b[5] = a[5]
+	b[6] = a[6]
+	b[7] = a[7]
+}
+
+// Float64Ordered encodes f and writes it to b so that ordering it with
+// bytes.Compare will produce the same ordering as when ordered by the float64
+// values.
+func (s S) Float64Ordered(b []byte, f float64) {
+	s.Float64(b, f)
+	Reverse(b)
+	if b[0]&128 == 128 {
+		Inverse(b)
+	} else {
+		b[0] |= 128
+	}
 }
 
 // Any takes any type that rye can serialize (any int, uint, float, string or bool)
