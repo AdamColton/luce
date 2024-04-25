67d28d4fa58feb7e077099be047c3d9473d5dc63
Adam Colton <adamcolton@gmail.com>
2024-04-25 17:59:38 -0400

    +serial/rye.Serialize.Any

--- serial/rye/serialize.go
+++ serial/rye/serialize.go
@@ -1,5 +1,11 @@
 package rye
 
+import (
+	"math"
+
+	"github.com/adamcolton/luce/ds/slice"
+)
+
 // S provides a namespace to attach the Serialize methods.
 type S struct{}
 
@@ -30,4 +36,58 @@ func (S) Uint64(b []byte, x uint64) {
 	b[5] = byte(x >> 40)
 	b[6] = byte(x >> 48)
 	b[7] = byte(x >> 56)
+}
+
+// Any takes any type that rye can serialize (any int, uint, float, string or bool)
+func (s S) Any(i any, buf []byte) []byte {
+	switch t := i.(type) {
+	case uint:
+		buf = slice.NewBuffer(buf).Slice(8)
+		s.Uint64(buf, uint64(t))
+	case uint8:
+		buf = slice.NewBuffer(buf).Slice(1)
+		buf[0] = t
+	case uint16:
+		buf = slice.NewBuffer(buf).Slice(2)
+		s.Uint16(buf, t)
+	case uint32:
+		buf = slice.NewBuffer(buf).Slice(4)
+		s.Uint32(buf, t)
+	case uint64:
+		buf = slice.NewBuffer(buf).Slice(8)
+		s.Uint64(buf, t)
+	case int:
+		buf = slice.NewBuffer(buf).Slice(8)
+		s.Uint64(buf, uint64(t))
+	case int8:
+		buf = slice.NewBuffer(buf).Slice(1)
+		buf[0] = byte(t)
+	case int16:
+		buf = slice.NewBuffer(buf).Slice(2)
+		s.Uint16(buf, uint16(t))
+	case int32:
+		buf = slice.NewBuffer(buf).Slice(4)
+		s.Uint32(buf, uint32(t))
+	case int64:
+		buf = slice.NewBuffer(buf).Slice(8)
+		s.Uint64(buf, uint64(t))
+	case float32:
+		buf = slice.NewBuffer(buf).Slice(4)
+		s.Uint32(buf, math.Float32bits(t))
+	case float64:
+		buf = slice.NewBuffer(buf).Slice(8)
+		s.Uint64(buf, math.Float64bits(t))
+	case string:
+		buf = []byte(t)
+	case bool:
+		buf = slice.NewBuffer(buf).Slice(1)
+		if t {
+			buf[0] = 1
+		} else {
+			buf[0] = 0
+		}
+	default:
+		buf = nil
+	}
+	return buf
 }
