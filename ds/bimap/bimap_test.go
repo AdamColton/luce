5955250cfcd5c47154cf931b4030bf97d3a8bb84
Adam Colton <adamcolton@gmail.com>
2024-04-26 18:42:08 -0400

    +ds/bimap.Each

--- ds/bimap/bimap_test.go
+++ ds/bimap/bimap_test.go
@@ -1,6 +1,7 @@
 package bimap_test
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/adamcolton/luce/ds/bimap"
@@ -40,4 +41,47 @@ func TestBimap(t *testing.T) {
 	assert.True(t, d.A.Deleted)
 	assert.Equal(t, "123", d.A.Value)
 	assert.False(t, d.B.Deleted)
+}
+
+func TestEach(t *testing.T) {
+	bi := bimap.New[string, int](5)
+	bi.Add("apple", 1)
+	bi.Add("banana", 2)
+	bi.Add("cantaloupe", 3)
+	bi.Add("date", 4)
+	bi.Add("elderberry", 5)
+
+	var gotStr []string
+	var gotInt []int
+	bi.Each(func(a string, b int) bool {
+		gotStr = append(gotStr, a)
+		gotInt = append(gotInt, b)
+		return false
+	})
+	slices.Sort(gotStr)
+	slices.Sort(gotInt)
+
+	expectedStr := []string{
+		"apple",
+		"banana",
+		"cantaloupe",
+		"date",
+		"elderberry",
+	}
+	expectedInt := []int{
+		1,
+		2,
+		3,
+		4,
+		5,
+	}
+	assert.Equal(t, expectedStr, gotStr)
+	assert.Equal(t, expectedInt, gotInt)
+
+	gotStr = gotStr[:0]
+	bi.Each(func(a string, b int) (done bool) {
+		gotStr = append(gotStr, a)
+		return true
+	})
+	assert.Len(t, gotStr, 1)
 }
