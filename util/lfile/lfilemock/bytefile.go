5c32c0f98882dd5e9814ff59597ee7c08d863ead
Adam Colton <adamcolton@gmail.com>
2024-06-28 11:43:50 -0400

    lots of lfile changes - needs clean up

---
+++ util/lfile/lfilemock/bytefile.go
@@ -0,0 +1,54 @@
+package lfilemock
+
+import (
+	"bytes"
+	"os"
+
+	"github.com/adamcolton/luce/util/navigator"
+)
+
+// ByteFile is used to create files in mock directory trees. ByteFiles are
+// created when calling ParseDir.
+type ByteFile struct {
+	Name string
+	Data []byte
+	Err  error
+}
+
+// File fulfills Node. It uses a ByteFile to create an instance of File that
+// fulfills lfile.File.
+func (f *ByteFile) File() *File {
+	return &File{
+		FileName: f.Name,
+		Buffer:   bytes.NewBuffer(f.Data),
+		Dir:      false,
+		FileSize: int64(len(f.Data)),
+		FileInfo: &FileInfo{
+			FileName: f.Name,
+			Dir:      false,
+		},
+		Err: f.Err,
+	}
+}
+
+// DirEntry fulfills Node. It creates a DirEntry instance for the ByteFile.
+func (f *ByteFile) DirEntry() os.DirEntry {
+	return &DirEntry{
+		EntryName: f.Name,
+		Dir:       false,
+		FileInfo: &FileInfo{
+			FileName: f.Name,
+			Dir:      false,
+		},
+	}
+}
+
+// Next fulfills Node and navigator.Nexter. It is used internally for navigating
+// the mock directory.
+func (f *ByteFile) Next(key string, create bool, _ navigator.VoidContext) (Node, bool) {
+	return nil, false
+}
+
+func (f *ByteFile) Error() error {
+	return f.Err
+}
