ed39f43d2f8dcdccbae1d6a186ec19f00de60f19
Adam Colton <adamcolton@gmail.com>
2024-06-28 11:43:50 -0400

    lots of lfile changes - needs clean up

--- util/lfile/match_test.go
+++ util/lfile/match_test.go
@@ -1,13 +1,14 @@
-package lfile
+package lfile_test
 
 import (
-	"os"
 	"reflect"
 	"testing"
 
 	"github.com/adamcolton/luce/ds/slice"
 	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/util/filter"
+	"github.com/adamcolton/luce/util/lfile"
+	"github.com/adamcolton/luce/util/lfile/lfilemock"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,12 +16,12 @@ func TestNewMatch(t *testing.T) {
 	f := filter.EQ("foo.txt")
 	d := filter.EQ("/dir/")
 	h := filter.EQ("/.hidden/")
-	m := NewMatch(f, d, h)
+	m := lfile.NewMatch(f, d, h)
 	assert.Equal(t, reflect.ValueOf(m.Find.File).Pointer(), reflect.ValueOf(f).Pointer())
 	assert.Equal(t, reflect.ValueOf(m.Find.Dir).Pointer(), reflect.ValueOf(d).Pointer())
 	assert.Equal(t, reflect.ValueOf(m.SkipDir).Pointer(), reflect.ValueOf(h).Pointer())
 
-	m = MustRegexMatch(Exts(false, "txt"), `\/.*dir\/`, `\/\..*/`)
+	m = lfile.MustRegexMatch(lfile.Exts(false, "txt"), `\/.*dir\/`, `\/\..*/`)
 	assert.True(t, m.Find.File("foo.txt"))
 	assert.False(t, m.Find.File("/foo.txt/bar"))
 	assert.True(t, m.Find.Dir("/mydir/"))
@@ -29,48 +30,53 @@ func TestNewMatch(t *testing.T) {
 	assert.False(t, m.SkipDir("/not.hidden/"))
 }
 
-// TODO:tests where match has nil values
-
 type idxCheck struct {
 	shouldBe int
 	t        *testing.T
 }
 
-func (i *idxCheck) HandleIter(itr Iterator) {
+func (i *idxCheck) HandleIter(itr lfile.Iterator) {
 	assert.Equal(i.t, i.shouldBe, itr.Idx())
 	i.shouldBe++
 }
 
-func TestMRI(t *testing.T) {
-	restore := setupForTestMRI()
-	defer restore()
+func TestMRILMock(t *testing.T) {
+	repo := lfilemock.Parse(map[string]any{
+		".": []string{"a", "aa", "b", "c"},
+		"dir": map[string]any{
+			".":    []string{"d", "e", "f"},
+			"dir1": []string{"g", "h", "hh", "i"},
+			"dir2": []string{"j", "k"},
+		},
+		".hidden1": []string{"x", "y", "z"},
+	}).Repository()
 
 	tt := map[string]struct {
-		Match
-		expectedFiles []string
-		expectedDirs  []string
+		lfile.Match
+		expectedFiles slice.Slice[string]
+		expectedDirs  slice.Slice[string]
 	}{
 		"basic": {
-			Match: MustRegexMatch(`\/[a-z]$`, `[0-9]$`, FilterHidden),
-			expectedFiles: []string{
+			Match: lfile.MustRegexMatch(`\/[a-z]$`, `[0-9]$`, lfile.FilterHidden),
+			expectedFiles: slice.Slice[string]{
 				"/dir/f", "/dir/dir2/k", "/dir/dir2/j", "/dir/e", "/dir/dir1/i",
 				"/dir/dir1/h", "/dir/dir1/g", "/dir/d", "/c", "/b", "/a",
 			},
-			expectedDirs: []string{"/dir/dir2", "/dir/dir1"},
+			expectedDirs: slice.Slice[string]{"/dir/dir2", "/dir/dir1"},
 		},
 		"no-dirs": {
-			Match: MustRegexMatch(`\/[a-z]$`, ``, FilterHidden),
+			Match: lfile.MustRegexMatch(`\/[a-z]$`, ``, lfile.FilterHidden),
 			expectedFiles: []string{
 				"/dir/f", "/dir/dir2/k", "/dir/dir2/j", "/dir/e", "/dir/dir1/i",
 				"/dir/dir1/h", "/dir/dir1/g", "/dir/d", "/c", "/b", "/a",
 			},
 		},
 		"no-files": {
-			Match:        MustRegexMatch(``, `[0-9]$`, FilterHidden),
+			Match:        lfile.MustRegexMatch(``, `[0-9]$`, lfile.FilterHidden),
 			expectedDirs: []string{"/dir/dir2", "/dir/dir1"},
 		},
 		"no-filter": {
-			Match: MustRegexMatch(`\/[a-z]$`, `[0-9]$`, ``),
+			Match: lfile.MustRegexMatch(`\/[a-z]$`, `[0-9]$`, ``),
 			expectedFiles: []string{
 				"/.hidden1/z", "/.hidden1/y", "/.hidden1/x", "/dir/f",
 				"/dir/dir2/k", "/dir/dir2/j", "/dir/e", "/dir/dir1/i",
@@ -83,86 +89,52 @@ func TestMRI(t *testing.T) {
 	lt := slice.LT[string]()
 	for n, tc := range tt {
 		t.Run(n, func(t *testing.T) {
-			mri := tc.Match.Root("/")
-			got := slice.FromIterFactory(mri.Factory, nil)
+			r := tc.Match.Root("/")
+			r.Repository = repo
+			got := slice.FromIterFactory(r.Factory, nil)
 			got.Sort(lt)
 			expected := slice.New(append(slice.New(tc.expectedDirs).Clone(), tc.expectedFiles...))
 			expected.Sort(lt)
 			assert.Equal(t, expected, got)
 
-			i, _ := mri.Iterator()
-			byType := &GetByTypeHandler{}
-			contents := GetContentsHandler{}
+			i, _ := r.Iterator()
+			byType := &lfile.GetByTypeHandler{}
+			contents := lfile.GetContentsHandler{}
+
 			idxChecker := &idxCheck{t: t}
-			err := RunHandler(i, MultiHandler{byType, contents, idxChecker})
+			err := lfile.RunHandler(i, lfile.MultiHandler{byType, contents, idxChecker})
 			assert.NoError(t, err)
 			assert.True(t, i.Done())
 			assert.Equal(t, "", i.Path())
-			assert.Equal(t, tc.expectedFiles, byType.Files)
-			assert.Equal(t, tc.expectedDirs, byType.Dirs)
-			expectedContents := make(GetContentsHandler, len(tc.expectedFiles))
+
+			assert.Equal(t, tc.expectedFiles.Sort(lt), slice.New(byType.Files).Sort(lt))
+			assert.Equal(t, tc.expectedDirs.Sort(lt), slice.New(byType.Dirs).Sort(lt))
+			expectedContents := make(lfile.GetContentsHandler, len(tc.expectedFiles))
 			for _, f := range tc.expectedFiles {
-				expectedContents[f] = []byte(f)
+				_, n := lfile.Name(f)
+				expectedContents[f] = []byte(n)
 			}
 			assert.Equal(t, expectedContents, contents)
 		})
 	}
 }
 
-func setupForTestMRI() func() {
-	restoreReadDirNames := readDirNames
-	restoreStat := Stat
-	restoreReadFile := ReadFile
-
-	mockDir := map[string][]string{
-		"/":         {"a", "aa", "b", "c", "dir", ".hidden1"},
-		"/dir":      {"d", "dir1", "e", "dir2", "f"},
-		"/dir/dir1": {"g", "h", "hh", "i"},
-		"/dir/dir2": {"j", "k"},
-		"/.hidden1": {"x", "y", "z"},
-	}
-	readDirNames = func(r Repository, dirname string) ([]string, error) {
-		return mockDir[dirname], nil
-	}
-	Stat = func(name string) (os.FileInfo, error) {
-		_, isDir := mockDir[name]
-		return mockFileInfo{
-			isDir: isDir,
-		}, nil
-	}
-	ReadFile = mockReadFileAsName
-
-	return func() {
-		readDirNames, Stat, ReadFile = restoreReadDirNames, restoreStat, restoreReadFile
-	}
-}
-
-func TestReadDirNames(t *testing.T) {
-	// TODO: Use lfilemock
-	t.Skip()
-	names, err := readDirNames(nil, ".")
-	assert.NoError(t, err)
-	expected := []string{"dir.go", "dir_test.go", "doc.go", "exts.go",
-		"exts_test.go", "handlers.go", "handlers_test.go", "iterator.go",
-		"lfilemock", "match.go", "match_test.go", "multiglob.go",
-		"multiglob_test.go", "path.go", "path_test.go", "paths.go",
-		"paths_test.go", "repository.go", "search.go", "search_test.go"}
-	assert.Equal(t, expected, names)
-}
-
 func TestMRIErr(t *testing.T) {
-	restoreReadDirNames := readDirNames
-	defer func() {
-		readDirNames = restoreReadDirNames
-	}()
+	dir := lfilemock.Parse(map[string]any{
+		".": []string{"a", "aa", "b", "c"},
+		"dir": map[string]any{
+			".":    []string{"d", "e", "f"},
+			"dir1": []string{"g", "h", "hh", "i"},
+			"dir2": []string{"j", "k"},
+		},
+		".hidden1": []string{"x", "y", "z"},
+	})
+	dir.Err = lerr.Str("test error")
 
-	readDirNames = func(r Repository, dirname string) ([]string, error) {
-		return nil, lerr.Str("test error")
-	}
-
-	i, _ := MustRegexMatch(`\/[a-z]$`, `[0-9]$`, FilterHidden).
-		Root("/").
-		Iterator()
+	mr := lfile.MustRegexMatch(`\/[a-z]$`, `[0-9]$`, lfile.FilterHidden).
+		Root("/")
+	mr.Repository = dir.Repository()
+	i, _ := mr.Iterator()
 
 	assert.Equal(t, "test error", i.Err().Error())
 	_, done := i.Next()
