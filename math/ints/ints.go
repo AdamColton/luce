699db15770042b6f87b41f56d6205554db978e1a
Adam Colton <adamcolton@gmail.com>
2024-09-14 09:21:21 -0400

    +math/ints.Range(start,x,end)

--- math/ints/ints.go
+++ math/ints/ints.go
@@ -104,6 +104,27 @@ func Prod[T Number](ns ...T) T {
 	return Reduce(ProdFn, ns)
 }
 
+// Idx provides a relative index to the given length. So a value of -1 will
+// return the ln-1. The bool indicates if the index is in range.
+func Idx(idx, ln int) (int, bool) {
+	if idx < 0 {
+		idx = ln + idx
+	}
+	return idx, idx >= 0 && idx < ln
+}
+
+// Range checks that x is within the defined range. If it is less than start,
+// start is returned. If it is greater than end, end is returned.
+func Range[T Number](start, x, end T) T {
+	if x < start {
+		return start
+	}
+	if x > end {
+		return end
+	}
+	return x
+}
+
 // Int converts any integer type to an int
 func Int[T constraints.Integer](i T) int { return int(i) }
 
