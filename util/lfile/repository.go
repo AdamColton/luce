a1be969c4d3e805405f78cfe53472d52ca347344
Adam Colton <adamcolton@gmail.com>
2024-09-22 10:51:41 -0400

    lfile savepoint

--- util/lfile/repository.go
+++ util/lfile/repository.go
@@ -9,17 +9,17 @@ import (
 	"sync/atomic"
 
 	"github.com/adamcolton/luce/ds/slice"
+	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/util/upgrade"
 )
 
 // File provides an interface fulfilled by *os.File. This allows for testing
 // without relying on the actual file system.
 type File interface {
+	fs.File
 	Dir
-	io.Reader
 	io.WriteCloser
-	Stat() (os.FileInfo, error)
 	Readdirnames(n int) (names []string, err error)
-	Close() error
 }
 
 // Repository is an interface to the file system.
@@ -121,4 +121,147 @@ func Size(repo Repository, path string) (int64, error) {
 	}
 
 	return size.Load(), nil
+}
+
+type EmbedFS interface {
+	Open(name string) (fs.File, error)
+	ReadFile(name string) ([]byte, error)
+	ReadDir(name string) ([]fs.DirEntry, error)
+}
+
+type embedWrapper struct {
+	EmbedFS
+}
+
+func (embedWrapper) Create(name string) (File, error) {
+	return nil, lerr.Str("embed.FS cannot Create")
+}
+
+func (embedWrapper) Lstat(name string) (os.FileInfo, error) {
+	return nil, lerr.Str("embed.FS cannot Lstat")
+}
+
+func (embedWrapper) Stat(name string) (os.FileInfo, error) {
+	return nil, lerr.Str("embed.FS cannot Stat")
+}
+
+func (embedWrapper) Remove(name string) error {
+	return lerr.Str("embed.FS cannot Remove")
+}
+
+type fsFile struct {
+	fs.File
+	name string
+	ew   embedWrapper
+}
+
+func (f *fsFile) Name() string {
+	return f.name
+}
+
+func (f *fsFile) ReadDir(n int) ([]os.DirEntry, error) {
+	return f.ew.ReadDir(f.name)
+}
+
+func (f *fsFile) Readdirnames(n int) (names []string, err error) {
+	des, err := f.ReadDir(n)
+	if err != nil {
+		return nil, err
+	}
+	out := make([]string, len(des))
+	for i, de := range des {
+		out[i] = de.Name()
+	}
+	return out, nil
+}
+
+func (f *fsFile) Write(p []byte) (n int, err error) {
+	return 0, lerr.Str("embed.FS cannot write")
+}
+
+func (ew embedWrapper) Open(name string) (File, error) {
+	f, err := ew.EmbedFS.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	out := &fsFile{
+		File: f,
+		name: name,
+	}
+	return out, err
+}
+
+func WrapEmbed(fs EmbedFS) Repository {
+	return embedWrapper{fs}
+}
+
+type FSReader interface {
+	FileStater
+	FSOpener
+	FileReader
+}
+
+type FileStater interface {
+	Stat(name string) (os.FileInfo, error)
+}
+
+type FileReader interface {
+	ReadFile(name string) ([]byte, error)
+}
+
+type FSOpener interface {
+	Open(name string) (File, error)
+}
+
+func WrapReadFile(fsr FSOpener) func(name string) ([]byte, error) {
+	if fr, ok := upgrade.To[FileReader](fsr); ok {
+		return fr.ReadFile
+	}
+
+	return func(name string) ([]byte, error) {
+		f, err := fsr.Open(name)
+		if err != nil {
+			return nil, err
+		}
+		return io.ReadAll(f)
+	}
+}
+
+func WrapStat(fsr FSOpener) func(name string) (os.FileInfo, error) {
+	if fr, ok := upgrade.To[FileStater](fsr); ok {
+		return fr.Stat
+	}
+
+	return func(name string) (os.FileInfo, error) {
+		f, err := fsr.Open(name)
+		if err != nil {
+			return nil, err
+		}
+		return f.Stat()
+	}
+}
+
+func WrappedFSReader(o FSOpener) FSReader {
+	if r, ok := upgrade.To[FSReader](o); ok {
+		return r
+	}
+	return wrappedFSReader{
+		FSOpener: o,
+		stat:     WrapStat(o),
+		readFile: WrapReadFile(o),
+	}
+}
+
+type wrappedFSReader struct {
+	FSOpener
+	readFile func(name string) ([]byte, error)
+	stat     func(name string) (os.FileInfo, error)
+}
+
+func (wr wrappedFSReader) ReadFile(name string) ([]byte, error) {
+	return wr.readFile(name)
+}
+
+func (wr wrappedFSReader) Stat(name string) (os.FileInfo, error) {
+	return wr.stat(name)
 }
