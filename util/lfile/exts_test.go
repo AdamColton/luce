75eeaec849ad1d68f4601c91899f4122920b5582
Adam Colton <adamcolton@gmail.com>
2024-10-24 18:14:39 -0400

    fixup util/lfile exts

--- util/lfile/exts_test.go
+++ util/lfile/exts_test.go
@@ -1,42 +1,89 @@
 package lfile
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExts(t *testing.T) {
+	ext := Exts(false, "js")
+	re := regexp.MustCompile(ext)
+	assert.True(t, re.MatchString("../../foo/test.js"))
+	assert.False(t, re.MatchString("../.hidden/foo/test.js"))
+	assert.False(t, re.MatchString(".hidden/nothidden/foo/test.js"))
+
 	tt := map[string]struct {
-		hidden   bool
-		exts     []string
-		expected string
+		exts string
+		pass []string
+		fail []string
 	}{
 		"js": {
-			hidden:   false,
-			exts:     []string{"js"},
-			expected: "^([^\\/]|(\\/[^\\.]))*\\.((js))$",
+			exts: Exts(false, "js"),
+			pass: []string{
+				"foo.js",
+				"foo/bar.js",
+				"/a/b/cde.js",
+				"./hello.js",
+				"../hello/goodbye.js",
+			},
+			fail: []string{
+				"foo.txt",
+				"foo.js.txt",
+				".hidden/foo.js",
+				"/.hidden/foo.js",
+				"/nothidden/.hidden/foo.js",
+			},
 		},
 		"js-withHidden": {
-			hidden:   true,
-			exts:     []string{"js"},
-			expected: "^.*\\.((js))$",
+			exts: Exts(true, "js"),
+			pass: []string{
+				"foo.js",
+				"foo/bar.js",
+				"/a/b/cde.js",
+				"./hello.js",
+				"../hello/goodbye.js",
+				"/.hidden/foo.js",
+				"/nothidden/.hidden/foo.js",
+			},
+			fail: []string{
+				"foo.txt",
+				"foo.js.txt",
+			},
 		},
 		"js&txt": {
-			hidden:   false,
-			exts:     []string{"js", "txt"},
-			expected: "^([^\\/]|(\\/[^\\.]))*\\.((js)|(txt))$",
-		},
-		"js&txt-withHidden": {
-			hidden:   true,
-			exts:     []string{"js", "txt"},
-			expected: "^.*\\.((js)|(txt))$",
+			exts: Exts(false, "js", "txt"),
+			pass: []string{
+				"foo.js",
+				"foo/bar.js",
+				"/a/b/cde.js",
+				"./hello.js",
+				"../hello/goodbye.js",
+				"foo.txt",
+				"foo.js.txt",
+			},
+			fail: []string{
+				"/.hidden/foo.js",
+				"/nothidden/.hidden/foo.js",
+				"foo.js.txt.other",
+				"foo.css",
+			},
 		},
 	}
-
 	for n, tc := range tt {
 		t.Run(n, func(t *testing.T) {
-			assert.Equal(t, tc.expected, Exts(tc.hidden, tc.exts...))
+			re := regexp.MustCompile(tc.exts)
+			for _, s := range tc.pass {
+				t.Run(s, func(t *testing.T) {
+					assert.True(t, re.MatchString(s))
+				})
+			}
+			for _, s := range tc.fail {
+				t.Run(s, func(t *testing.T) {
+					assert.False(t, re.MatchString(s))
+				})
+			}
 		})
 	}
 }
