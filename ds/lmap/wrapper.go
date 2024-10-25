ace050c38d7097c33971ded01ac3aa168ce7ff4b
Adam Colton <adamcolton@gmail.com>
2024-10-25 09:25:49 -0400

    +ds/lmap.Wrapper

---
+++ ds/lmap/wrapper.go
@@ -0,0 +1,20 @@
+package lmap
+
+// Wrapper provides helpers around a Mapper.
+type Wrapper[K comparable, V any] struct {
+	Mapper[K, V]
+}
+
+// Wrap a Mapper. If it is already a Wrapper, that will be returned.
+func Wrap[K comparable, V any](m Mapper[K, V]) Wrapper[K, V] {
+	w, ok := m.(Wrapper[K, V])
+	if ok {
+		return w
+	}
+	return Wrapper[K, V]{m}
+}
+
+// Wrapped fulfills upgrade.Wrapper.
+func (w Wrapper[K, V]) Wrapped() any {
+	return w.Mapper
+}
