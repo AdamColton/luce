ae469285e06f877e5a63c3dc9d8351d36c54e161
Adam Colton <adamcolton@gmail.com>
2024-10-25 09:35:02 -0400

    +ds/lmap/doc.go

---
+++ ds/lmap/doc.go
@@ -0,0 +1,5 @@
+// Package lmap provides helpers for the built in map type.
+//
+// Note that there is also util/filter.Map which provides tools for performing
+// filtering operations on a map.
+package lmap
