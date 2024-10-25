770964a43c48096973b80fba8e865f9d973e2d0c
Adam Colton <adamcolton@gmail.com>
2024-10-25 09:25:49 -0400

    +ds/lmap.Wrapper

---
+++ ds/lmap/wrapper.go
@@ -0,0 +1,17 @@
+package lmap
+
+type Wrapper[K comparable, V any] struct {
+	Mapper[K, V]
+}
+
+func Wrap[K comparable, V any](m Mapper[K, V]) Wrapper[K, V] {
+	w, ok := m.(Wrapper[K, V])
+	if ok {
+		return w
+	}
+	return Wrapper[K, V]{m}
+}
+
+func (w Wrapper[K, V]) Wrapped() any {
+	return w.Mapper
+}
