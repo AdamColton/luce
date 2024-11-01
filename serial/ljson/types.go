6ed5c5be277fc4cb33887c3728a2ca3ee3323b72
Adam Colton <adamcolton@gmail.com>
2024-11-01 13:08:52 -0400

    +serial/ljson circularGuard on types

--- serial/ljson/types.go
+++ serial/ljson/types.go
@@ -3,6 +3,9 @@ package ljson
 import (
 	"reflect"
 	"unsafe"
+
+	"github.com/adamcolton/luce/ds/lset"
+	"github.com/adamcolton/luce/lerr"
 )
 
 // TypesContext is used to create marshalers. Once a marshaler for a type is
@@ -10,15 +13,17 @@ import (
 // marshaled. Ctx defines the type for the Context field during the marshaling
 // phase.
 type TypesContext[Ctx any] struct {
-	marshalers   map[uintptr]unsafeMarshal[Ctx]
-	fieldMarshal map[FieldKey]unsafeFieldMarshaler[Ctx]
+	marshalers    map[uintptr]unsafeMarshal[Ctx]
+	fieldMarshal  map[FieldKey]unsafeFieldMarshaler[Ctx]
+	circularGuard *lset.Set[reflect.Type]
 }
 
 // NewTypesContext creates a TypesContext
 func NewTypesContext[Ctx any]() *TypesContext[Ctx] {
 	return &TypesContext[Ctx]{
-		marshalers:   make(map[uintptr]unsafeMarshal[Ctx]),
-		fieldMarshal: make(map[FieldKey]unsafeFieldMarshaler[Ctx]),
+		marshalers:    make(map[uintptr]unsafeMarshal[Ctx]),
+		fieldMarshal:  make(map[FieldKey]unsafeFieldMarshaler[Ctx]),
+		circularGuard: lset.New[reflect.Type](),
 	}
 }
 
@@ -32,12 +37,17 @@ func (tctx *TypesContext[Ctx]) lazyGetter(t reflect.Type, self *unsafeMarshal[Ct
 
 	*self = func(ptr unsafe.Pointer, ctx *MarshalContext[Ctx]) WriteNode {
 		tctx := ctx.TypesContext
+		if tctx.circularGuard.Contains(t) {
+			panic(lerr.Str("circular type reference"))
+		}
+		tctx.circularGuard.Add(t)
 		m, found := tctx.marshalers[tab]
 		if !found {
 			m = tctx.buildUnsafeMarshaler(t)
 			tctx.marshalers[tab] = m
 		}
 		*self = m
+		tctx.circularGuard.Remove(t)
 
 		return (*self)(ptr, ctx)
 	}
