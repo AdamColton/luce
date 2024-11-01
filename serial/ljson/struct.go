9f9afc2821a1aa244dbde7e66f4b6270c8af0c29
Adam Colton <adamcolton@gmail.com>
2024-11-01 13:21:21 -0400

    +serial/ljson.(AddFieldMarshal,OmitFields)

--- serial/ljson/struct.go
+++ serial/ljson/struct.go
@@ -7,12 +7,18 @@ import (
 	"github.com/adamcolton/luce/ds/slice"
 	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/util/luceio"
+	"github.com/adamcolton/luce/util/reflector"
 )
 
 // FieldKey is used to identify a Field by name on a Type.
 type FieldKey struct {
 	reflect.Type
 	Name string
+}
+
+// Field returns the StructField the FieldKey represents.
+func (fk FieldKey) Field() (reflect.StructField, bool) {
+	return fk.Type.FieldByName(fk.Name)
 }
 
 type unsafeFieldMarshaler[Ctx any] func(name string, ptr unsafe.Pointer, ctx *MarshalContext[Ctx]) (string, WriteNode)
@@ -137,5 +143,63 @@ func defaultFieldMarshaler[Ctx any](t reflect.Type, ctx *TypesContext[Ctx]) unsa
 	ctx.lazyGetter(t, &m)
 	return func(name string, ptr unsafe.Pointer, ctx *MarshalContext[Ctx]) (string, WriteNode) {
 		return name, m(ptr, ctx)
+	}
+}
+
+// StructKeys holds FieldKeys for all exported fields.
+type StructKeys map[string]FieldKey
+
+// GetFieldKeys returns the StructKeys for a struct.
+func GetFieldKeys[T any]() StructKeys {
+	t := reflector.Type[T]()
+	if t.Kind() != reflect.Struct {
+		return nil
+	}
+	n := t.NumField()
+	out := make(map[string]FieldKey, n)
+	for i := 0; i < n; i++ {
+		f := t.Field(i)
+		if f.IsExported() {
+			out[f.Name] = FieldKey{
+				Type: t,
+				Name: f.Name,
+			}
+		}
+	}
+	return out
+}
+
+// FieldMarshal is used to marshal a field on a struct. If either the WriteNode
+// is nil or the string is empty, the field will not be written to the json
+// document.
+type FieldMarshal[T, Ctx any] func(name string, v T, ctx *MarshalContext[Ctx]) (string, WriteNode, error)
+
+// AddFieldMarshal for the given key.
+func AddFieldMarshal[T, Ctx any](key FieldKey, fm FieldMarshal[T, Ctx], ctx *TypesContext[Ctx]) {
+	sf, found := key.Field()
+	if !found {
+		panic("key does not exist")
+	}
+	if sf.Type != reflector.Type[T]() {
+		panic("types do not matcch")
+	}
+	ctx.fieldMarshal[key] = func(name string, ptr unsafe.Pointer, ctx *MarshalContext[Ctx]) (string, WriteNode) {
+		t := *(*T)(ptr)
+		if fm == nil {
+			return "", nil
+		}
+		name, wn, err := fm(name, t, ctx)
+		lerr.Panic(err)
+		return name, wn
+	}
+}
+
+// OmitFields from a struct.
+func (tctx *TypesContext[Ctx]) OmitFields(structKeys StructKeys, fieldNames ...string) {
+	for _, n := range fieldNames {
+		k, found := structKeys[n]
+		if found {
+			tctx.fieldMarshal[k] = nil
+		}
 	}
 }
