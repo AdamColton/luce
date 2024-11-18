64f04de41e2844598d3684a629cbc1888ff7be92
Adam Colton <adamcolton@gmail.com>
2024-11-18 15:34:50 -0500

    +serial/ljson circular reference guard

--- serial/ljson/marshal.go
+++ serial/ljson/marshal.go
@@ -3,6 +3,7 @@ package ljson
 import (
 	"reflect"
 
+	"github.com/adamcolton/luce/ds/lset"
 	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/util/reflector"
 )
@@ -37,8 +38,9 @@ type Marshaler[T, Ctx any] func(v T, ctx *MarshalContext[Ctx]) (WriteNode, error
 // including the underlying TypesContext. The Context field holds an arbitrary
 // data type that will be available during the marshaling phase.
 type MarshalContext[Ctx any] struct {
-	Context      Ctx
-	TypesContext *TypesContext[Ctx]
+	Context       Ctx
+	TypesContext  *TypesContext[Ctx]
+	circularGuard *lset.Set[reflect.Value]
 
 	// Setting Sort to true will sort the keys on structs and maps.
 	// This is useful for testing because it produces consistent output
@@ -49,8 +51,9 @@ type MarshalContext[Ctx any] struct {
 // NewMarshalContext creates a MarshalContext using the TypesContext.
 func (tctx *TypesContext[Ctx]) NewMarshalContext(ctx Ctx) *MarshalContext[Ctx] {
 	return &MarshalContext[Ctx]{
-		Context:      ctx,
-		TypesContext: tctx,
+		Context:       ctx,
+		TypesContext:  tctx,
+		circularGuard: lset.New[reflect.Value](),
 	}
 }
 
@@ -64,4 +67,25 @@ func NewMarshalContext[Ctx any](ctx Ctx) *MarshalContext[Ctx] {
 func AddMarshaler[T, Ctx any](m Marshaler[T, Ctx], ctx *TypesContext[Ctx]) {
 	t := reflector.Type[T]()
 	ctx.marshalers[t] = valMarshal(m)
+}
+
+func (ctx *MarshalContext[Ctx]) initGuard(v reflect.Value) {
+	if ctx.circularGuard.Contains(v) {
+		panic(lerr.Str("marshaled object contains circular reference"))
+	}
+	ctx.circularGuard.Add(v)
+}
+
+func (ctx *MarshalContext[Ctx]) guardMarshal(v reflect.Value, fn valMarshaler[Ctx]) WriteNode {
+	ctx.initGuard(v)
+	wn := fn(v, ctx)
+	ctx.circularGuard.Remove(v)
+	return wn
+}
+
+func (ctx *MarshalContext[Ctx]) guardFieldMarshaler(name string, v reflect.Value, fm valFieldMarshaler[Ctx]) (string, WriteNode) {
+	ctx.initGuard(v)
+	name, wn := fm(name, v, ctx)
+	ctx.circularGuard.Remove(v)
+	return name, wn
 }
