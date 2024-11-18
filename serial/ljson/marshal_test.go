9ced76be176afe3cb421c0a90af888b7b1580966
Adam Colton <adamcolton@gmail.com>
2024-11-18 14:53:16 -0500

    +serial/ljson.MarshalContext.Context

--- serial/ljson/marshal_test.go
+++ serial/ljson/marshal_test.go
@@ -8,9 +8,36 @@ import (
 )
 
 func TestMarshalString(t *testing.T) {
-	ctx := ljson.NewMarshalContext()
+	ctx := ljson.NewMarshalContext("hello")
 
 	str, err := ljson.Stringify("this is a test", ctx)
 	assert.NoError(t, err)
 	assert.Equal(t, `"this is a test"`, str)
+}
+
+type ctxStr string
+
+func contextString(cs ctxStr, ctx *ljson.MarshalContext[bool]) (ljson.WriteNode, error) {
+	str := ":foo"
+	if ctx.Context {
+		str = ":bar"
+	}
+	str = string(cs) + str
+	return func(ctx *ljson.WriteContext) {
+		ctx.WriteString(str)
+	}, nil
+}
+
+func TestMarshalContext(t *testing.T) {
+	ctx := ljson.NewMarshalContext(false)
+	ljson.AddMarshaler(contextString, ctx.TypesContext)
+
+	str, err := ljson.Stringify(ctxStr("test"), ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, "test:foo", str)
+
+	ctx.Context = true
+	str, err = ljson.Stringify(ctxStr("baz"), ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, "baz:bar", str)
 }
