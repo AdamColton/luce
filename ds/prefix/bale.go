2725fdfb6ef80e9871612cf0361508c88fd55c4b
Adam Colton <adamcolton@gmail.com>
2025-02-07 15:27:17 -0500

    prefix entity

---
+++ ds/prefix/bale.go
@@ -0,0 +1,96 @@
+package prefix
+
+import (
+	"github.com/adamcolton/luce/ds/lmap"
+	"github.com/adamcolton/luce/ds/morph"
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/adamcolton/luce/entity"
+)
+
+type NodeBale struct {
+	IsWord        bool
+	R             rune
+	ChildrenCount int
+	Children      map[rune]*NodeBale
+}
+
+func (bale *NodeBale) TypeID32() uint32 {
+	return 4117018735
+}
+
+var (
+	// set in init to avoid initilization cycle error
+	baleChildren   morph.KeyValAll[rune, *node, lmap.KeyVal[rune, *NodeBale]]
+	unbaleChildren morph.KeyValAll[rune, *NodeBale, lmap.KeyVal[rune, *node]]
+)
+
+func (n *node) bale() *NodeBale {
+	return &NodeBale{
+		IsWord:        n.isWord,
+		R:             n.r,
+		ChildrenCount: n.childrenCount,
+		Children:      lmap.FromEacher(baleChildren.Eacher(n.children)).Map(),
+	}
+
+}
+
+func (bale *NodeBale) unbale() *node {
+	n := &node{}
+	bale.unbaleTo(n)
+	n.children.Each(func(r rune, child *node, done *bool) {
+		child.parent = n
+	})
+	return n
+}
+
+func (bale *NodeBale) unbaleTo(n *node) {
+	n.isWord = bale.IsWord
+	n.r = bale.R
+	n.childrenCount = bale.ChildrenCount
+	n.children = lmap.FromEacher(unbaleChildren.Eacher(lmap.New(bale.Children)))
+
+}
+
+type PrefixBale struct {
+	Root *NodeBale
+}
+
+func (bale *PrefixBale) TypeID32() uint32 {
+	return 1399013115
+}
+
+func (p *Prefix) saveIf() {
+	if p.save {
+		p.Save()
+	}
+}
+
+func (p *Prefix) Bale() *PrefixBale {
+	gp := &PrefixBale{
+		Root: p.root.bale(),
+	}
+	return gp
+}
+
+func (bale *PrefixBale) Unbale() *Prefix {
+	p := &Prefix{}
+	bale.UnbaleTo(p)
+	return p
+}
+
+func (bale *PrefixBale) EntRefs() []entity.Key {
+	return nil
+}
+
+func (bale *PrefixBale) UnbaleTo(p *Prefix) {
+	p.root = bale.Root.unbale()
+	p.starts = make(map[rune]slice.Slice[*node])
+	p.root.populateStarts(p)
+}
+
+func (n *node) populateStarts(p *Prefix) {
+	n.children.Each(func(r rune, child *node, done *bool) {
+		p.starts[r] = append(p.starts[r], child)
+		child.populateStarts(p)
+	})
+}
---
+++ ds/prefix/entity_test.go
@@ -0,0 +1,47 @@
+package prefix_test
+
+import (
+	"testing"
+
+	"github.com/adamcolton/luce/ds/prefix"
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/adamcolton/luce/entity"
+	"github.com/adamcolton/luce/entity/enttest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrefixEntity(t *testing.T) {
+	enttest.Setup()
+	p := prefix.New()
+
+	words := []string{"test", "testing", "tea", "adam"}
+
+	for _, w := range words {
+		p.Upsert(w)
+	}
+
+	n := p.Find("te")
+	assert.NotNil(t, n)
+
+	ref, err := p.Save()
+	assert.NoError(t, err)
+	k := ref.EntKey()
+	ref.Clear(true)
+	entity.ClearCache()
+
+	r2 := entity.KeyRef[prefix.Prefix](k)
+	p = r2.GetPtr()
+	if !assert.NotNil(t, p) {
+		return
+	}
+	n = p.Find("te")
+	assert.NotNil(t, n)
+	s := n.Suggest(2)
+	assert.Len(t, s, 2)
+	expected := slice.New([]string{"sting", "st"})
+	assert.Equal(t, expected, s[0].Words(""))
+
+	ls := p.Find("").AllWords().Strings().Slice(nil)
+	lt.Sort(ls)
+	lt.Sort(words)
+}
