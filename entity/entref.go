bb4748e52bc27bd130bb5817029f4989b93cc0ce
Adam Colton <adamcolton@gmail.com>
2025-02-08 23:30:22 -0500

    +entity caching

--- entity/entref.go
+++ entity/entref.go
@@ -32,6 +32,30 @@ func (er *Ref[T, E]) EntKey() Key {
 	return er.key
 }
 
+func (er *Ref[T, E]) isEntRef() {}
+
+func (er *Ref[T, E]) setIdx(idx int) {
+	er.idx = idx
+}
+
+func (er *Ref[T, E]) allRefsRm() {
+	if er.idx > 0 {
+		last := len(allRefs) - 1
+		swap := allRefs[last]
+		swap.setIdx(er.idx)
+		allRefs[er.idx] = swap
+		allRefs = allRefs[:last]
+		er.idx = 0
+	}
+}
+
+func (er *Ref[T, E]) addToAllRefs() {
+	if er.idx == 0 {
+		er.idx = len(allRefs)
+		allRefs = append(allRefs, er)
+	}
+}
+
 // WeakGet will return the current value of the reference, but will not load the
 // entity if the reference is nil.
 func (er *Ref[T, E]) WeakGet() (e E, ok bool) {
@@ -40,4 +64,14 @@ func (er *Ref[T, E]) WeakGet() (e E, ok bool) {
 		ok = true
 	}
 	return
+}
+
+// Clear sets the underlying pointer to nil allowing the Entity to be
+// garbage collected. This should only be temorary for testing.
+func (er *Ref[T, E]) Clear(cacheRm bool) {
+	er.ent.p = nil
+	er.allRefsRm()
+	if cacheRm {
+		cache.Delete(er.key.Hash64())
+	}
 }
