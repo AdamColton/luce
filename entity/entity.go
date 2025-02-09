c28baa79000454cff7e06af53048b6f9177f0b4b
Adam Colton <adamcolton@gmail.com>
2025-02-08 23:19:23 -0500

    +entity.Entity

---
+++ entity/entity.go
@@ -0,0 +1,13 @@
+package entity
+
+type Entity interface {
+	EntKey() Key
+	EntVal(buf []byte) ([]byte, error)
+	EntLoad(k Key, data []byte) error
+}
+
+// EntIniter allows a type to perform initilization when a new instance is
+// created, before EntLoad is called
+type EntIniter interface {
+	EntInit()
+}
