6606b2dc93e93305c01a2107a4ac5c3ab00bd0c1
Adam Colton <adamcolton@gmail.com>
2025-02-10 15:42:45 -0500

    +tools/server/service.Response

---
+++ tools/server/service/response.go
@@ -0,0 +1,19 @@
+package service
+
+import "net/http"
+
+// Response to a request. The ID is the same as the ID is taken from the
+// request.
+type Response struct {
+	ID     uint32
+	Body   []byte
+	Status int
+	Header http.Header
+}
+
+const ResponseTypeID32 uint32 = 370114636
+
+// TypeID32 fulfill TypeIDer32. The ID was choosen at random.
+func (*Response) TypeID32() uint32 {
+	return ResponseTypeID32
+}
