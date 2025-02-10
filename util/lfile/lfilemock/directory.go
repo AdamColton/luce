1c90d21c7510eefd9205e137fdeca7e7fbd40fce
Adam Colton <adamcolton@gmail.com>
2025-02-09 20:56:27 -0500

    +util/lfile/lfilemock

---
+++ util/lfile/lfilemock/directory.go
@@ -0,0 +1,154 @@
+package lfilemock
+
+import (
+	"os"
+	"strings"
+
+	"github.com/adamcolton/luce/ds/lbuf"
+	"github.com/adamcolton/luce/util/lfile"
+	"github.com/adamcolton/luce/util/navigator"
+)
+
+// Directory is used to create mock directory trees. Directories are
+// created when calling ParseDir.
+type Directory struct {
+	Name     string
+	Children map[string]Node
+	Err      error
+}
+
+// ParseDir allows for mock directory trees to be setup easily. To create a File
+// the value can be either a string or a []byte. To create a sub directory the
+// value should be another map[string]any which will be recursivly parsed.
+// Adding a []string will create a directory where each file's name and contents
+// are the same.
+func Parse(root map[string]any) *Directory {
+	// TODO: handle bad characters like /
+	out := &Directory{
+		Children: make(map[string]Node),
+	}
+	for name, f := range root {
+		switch x := f.(type) {
+		case map[string]any:
+			s := Parse(x)
+			s.Name = name
+			out.Children[name] = s
+		case string:
+			out.AddFile(name, []byte(x))
+		case []string:
+			var s *Directory
+			if name == "." {
+				s = out
+			} else {
+				s = &Directory{
+					Name:     name,
+					Children: make(map[string]Node, len(x)),
+				}
+				out.Children[name] = s
+			}
+			for _, file := range x {
+				s.AddFile(file, []byte(file))
+			}
+		case []byte:
+			out.AddFile(name, x)
+		default:
+			panic(ErrParseType)
+		}
+	}
+	return out
+}
+func (d *Directory) Error() error {
+	return d.Err
+}
+
+func (d *Directory) Get(path string) (n Node, found bool) {
+	vc := navigator.VoidContext{}
+	n = d
+	for _, key := range strings.Split(path, "/") {
+		n, found = n.Next(key, false, vc)
+		if !found {
+			break
+		}
+	}
+	if found {
+		n = pathNode{
+			name: path,
+			Node: n,
+		}
+	}
+	return
+}
+
+// AddDir adds a sub directory. This can be used when modifying the mock
+// directory tree.
+func (d *Directory) AddDir(name string) *Directory {
+	c := &Directory{
+		Name:     name,
+		Children: make(map[string]Node),
+	}
+	d.Children[name] = c
+	return d
+}
+
+// AddFile adds a ByteFile. This can be used when modifying the mock directory
+// tree.
+func (d *Directory) AddFile(name string, contents []byte) *ByteFile {
+	f := &ByteFile{
+		Name: name,
+		Data: lbuf.New(contents),
+	}
+	d.Children[name] = f
+	return f
+}
+
+// Parse allows for mock directory trees to be setup easily. To create a
+// File the value can be either a string or a []byte. To create a sub directory
+// the value should be another map[string]any which will be recursivly parsed.
+// The returned Repository fulfills lfile.Repository.
+func (d *Directory) Repository() lfile.Repository {
+	return &Repository{
+		Node: d,
+	}
+}
+
+// Next fulfills Node and navigator.Nexter. It is used internally for navigating
+// the mock directory.
+func (d *Directory) Next(key string, create bool, _ navigator.VoidContext) (Node, bool) {
+	n, found := d.Children[key]
+	if !found && create {
+		n = d.AddDir(key)
+		found = true
+	}
+	return n, found
+}
+
+// File fulfills Node. It creates an instance of *File that fulfills lfile.File.
+func (d *Directory) File() *File {
+	des := make([]os.DirEntry, 0, len(d.Children))
+	for _, c := range d.Children {
+		des = append(des, c.DirEntry())
+	}
+	return &File{
+		FileName:   d.Name,
+		Dir:        true,
+		DirEntries: des,
+		FileInfo: &FileInfo{
+			FileName: d.Name,
+			Dir:      true,
+		},
+		FileMode: 0,
+		Err:      d.Err,
+	}
+}
+
+// DirEntry fulfills Node. It creates a DirEntry instance for the Directory.
+func (d *Directory) DirEntry() os.DirEntry {
+	return &DirEntry{
+		EntryName: d.Name,
+		Dir:       true,
+		FileInfo: &FileInfo{
+			FileName: d.Name,
+			Dir:      true,
+		},
+	}
+}
---
+++ util/lfile/lfilemock/mock_test.go
@@ -0,0 +1,214 @@
+package lfilemock_test
+
+import (
+	"io"
+	"io/fs"
+	"io/ioutil"
+	"sort"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/adamcolton/luce/ds/lbuf"
+	"github.com/adamcolton/luce/ds/slice"
+	"github.com/adamcolton/luce/lerr"
+	"github.com/adamcolton/luce/util/lfile"
+	"github.com/adamcolton/luce/util/lfile/lfilemock"
+	"github.com/adamcolton/luce/util/navigator"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepository(t *testing.T) {
+	file1 := "this is test file 1"
+	file2 := []byte{3, 1, 4, 1, 5, 9, 2, 6, 5}
+	r := lfilemock.Parse(map[string]any{
+		"file1.txt": file1,
+		"dir1": map[string]any{
+			"file2.bin": file2,
+		},
+		"dir2": map[string]any{
+			"dir3":      map[string]any{},
+			"file4.txt": "this is file 4",
+		},
+	}).Repository()
+
+	f := lerr.Must(r.Open("file1.txt")).(lfile.File)
+	b, err := io.ReadAll(f)
+	assert.NoError(t, err)
+	assert.Equal(t, file1, string(b))
+
+	f = lerr.Must(r.Open("dir2")).(lfile.File)
+	des, err := f.ReadDir(0)
+	assert.NoError(t, err)
+	slice.Less[fs.DirEntry](func(i, j fs.DirEntry) bool {
+		return i.Name() < j.Name()
+	}).Sort(des)
+
+	assert.Equal(t, "dir3", des[0].Name())
+	assert.True(t, des[0].IsDir())
+	assert.Equal(t, fs.FileMode(0), des[0].Type())
+	fi, err := des[0].Info()
+	assert.NoError(t, err)
+	assert.Equal(t, "dir3", fi.Name())
+	assert.True(t, fi.IsDir())
+	assert.Equal(t, int64(0), fi.Size())
+	assert.Equal(t, fs.FileMode(0), fi.Mode())
+	assert.Equal(t, time.Time{}, fi.ModTime())
+	assert.Nil(t, fi.Sys())
+
+	f = lerr.Must(r.Open("dir2/dir3")).(lfile.File)
+	fi2, err := f.Stat()
+	assert.NoError(t, err)
+	assert.Equal(t, fi, fi2)
+	assert.NoError(t, f.Close())
+
+	assert.Equal(t, "file4.txt", des[1].Name())
+	assert.False(t, des[1].IsDir())
+
+	f = lerr.Must(r.Open("dir1/file2.bin")).(lfile.File)
+	b, err = io.ReadAll(f)
+	assert.NoError(t, err)
+	assert.Equal(t, file2, b)
+
+	f = lerr.Must(r.Create("dir1/dir4/file3.txt")).(lfile.File)
+	assert.Equal(t, "file3.txt", f.Name())
+
+	err = r.Remove("dir1/file2.bin")
+	assert.NoError(t, err)
+	nilFile, err := r.Open("dir1/file2.bin")
+	assert.Nil(t, nilFile)
+	assert.NoError(t, err)
+
+	lf := lfilemock.New("test.txt", "this is a test")
+	got, err := io.ReadAll(lf)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("this is a test"), got)
+	te := lerr.Str("test_error")
+	lf.Err = te
+	_, err = lf.Read(nil)
+	assert.Equal(t, te, err)
+
+	data := []byte{3, 1, 4, 1, 5, 9, 2, 6, 5, 3}
+	lf = lfilemock.New("test2.txt", data)
+	got, err = ioutil.ReadAll(lf)
+	assert.NoError(t, err)
+	assert.Equal(t, data, got)
+}
+
+func TestNewPanic(t *testing.T) {
+	defer func() {
+		assert.Equal(t, lfilemock.ErrNewType, recover())
+	}()
+	lfilemock.New("thisShouldPanic", 123)
+}
+
+func TestParseDirPanic(t *testing.T) {
+	defer func() {
+		assert.Equal(t, lfilemock.ErrParseType, recover())
+	}()
+	lfilemock.Parse(map[string]any{
+		"file1.txt": 123,
+	})
+}
+
+func TestByteFile(t *testing.T) {
+	bf := &lfilemock.ByteFile{
+		Name: "Test",
+		Data: lbuf.New([]byte{1, 2, 3, 4, 5}),
+	}
+	tree, ok := bf.Next("foo", true, navigator.Void)
+	assert.Nil(t, tree)
+	assert.False(t, ok)
+}
+
+func TestReaddirnames(t *testing.T) {
+	file1 := "this is test file 1"
+	file2 := []byte{3, 1, 4, 1, 5, 9, 2, 6, 5}
+	r := lfilemock.Parse(map[string]any{
+		"file1.txt": file1,
+		"dir1": map[string]any{
+			"file2.bin": file2,
+		},
+		"file2.bin": file2,
+		"file3.txt": "file3.txt",
+		"file4.txt": "file4.txt",
+	}).Repository()
+
+	f := lerr.Must(r.Open("file1.txt")).(lfile.File)
+
+	expectErr := &fs.PathError{Op: "readdirent", Path: "file1.txt", Err: syscall.ENOTDIR}
+	_, err := f.Readdirnames(-1)
+	assert.Equal(t, expectErr, err)
+
+	f = lerr.Must(r.Open("/")).(lfile.File)
+	names, err := f.Readdirnames(-1)
+	assert.NoError(t, err)
+	slice.LT[string]().Sort(names)
+	expected := []string{"dir1", "file1.txt", "file2.bin", "file3.txt", "file4.txt"}
+	assert.Equal(t, expected, names)
+	_, err = f.Readdirnames(-1)
+	assert.Equal(t, io.EOF, err)
+
+	f = lerr.Must(r.Open("/")).(lfile.File)
+	names, err = f.Readdirnames(2)
+	assert.NoError(t, err)
+	assert.Len(t, names, 2)
+	names, err = f.Readdirnames(-1)
+	assert.NoError(t, err)
+	assert.Len(t, names, 3)
+	_, err = f.Readdirnames(-1)
+	assert.Equal(t, io.EOF, err)
+}
+
+func TestWriteByteFile(t *testing.T) {
+	file1 := "this is test file 1"
+	file2 := []byte{3, 1, 4, 1, 5, 9, 2, 6, 5}
+	r := lfilemock.Parse(map[string]any{
+		"file1.txt": file1,
+		"dir1": map[string]any{
+			"file2.bin": file2,
+		},
+		"dir2": map[string]any{
+			"dir3":      map[string]any{},
+			"file4.txt": "this is file 4",
+		},
+	}).Repository()
+
+	f := lerr.Must(r.Open("/dir1/file2.bin")).(lfile.File)
+	f.Write([]byte("test"))
+	f.Close()
+
+	f2, err := r.Open("/dir1/file2.bin")
+	assert.NoError(t, err)
+	expected := append(file2, []byte("test")...)
+	got, err := io.ReadAll(f2)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, got)
+}
+
+func TestReadTwice(t *testing.T) {
+	repo := lfilemock.Parse(map[string]any{
+		".": []string{"a", "aa", "b", "c"},
+		"dir": map[string]any{
+			".":    []string{"d", "e", "f"},
+			"dir1": []string{"g", "h", "hh", "i"},
+			"dir2": []string{"j", "k"},
+		},
+		".hidden1": []string{"x", "y", "z"},
+	}).Repository()
+
+	f := lerr.Must(repo.Open("/dir/dir1")).(lfile.File)
+	names := lerr.Must(f.Readdirnames(-1))
+	sort.Strings(names)
+	expected := []string{"g", "h", "hh", "i"}
+	assert.Equal(t, expected, names)
+
+	f = lerr.Must(repo.Open("/dir/dir1/g")).(lfile.File)
+	got := string(lerr.Must(io.ReadAll(f)))
+	assert.Equal(t, "g", got)
+	f.Close()
+
+	f = lerr.Must(repo.Open("/dir/dir1/g")).(lfile.File)
+	got = string(lerr.Must(io.ReadAll(f)))
+	assert.Equal(t, "g", got)
+}
