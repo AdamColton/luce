559482fbee0bb9545e3871cc6d6dfaabfc0d10ae
Adam Colton <adamcolton@gmail.com>
2025-02-10 15:49:03 -0500

    +tools/server/service.Request.ResponseErr

--- tools/server/service/response_test.go
+++ tools/server/service/response_test.go
@@ -3,6 +3,7 @@ package service_test
 import (
 	"testing"
 
+	"github.com/adamcolton/luce/lerr"
 	"github.com/adamcolton/luce/tools/server/service"
 	"github.com/stretchr/testify/assert"
 )
@@ -27,4 +28,15 @@ func TestResponseString(t *testing.T) {
 	assert.Equal(t, []byte(body), resp.Body)
 	assert.Equal(t, req.ID, resp.ID)
 	assert.Equal(t, service.ResponseTypeID32, resp.TypeID32())
+}
+
+func TestResponseErr(t *testing.T) {
+	req := &service.Request{
+		ID: 31415,
+	}
+	body := "test error"
+	err := lerr.Str(body)
+	resp := req.ResponseErr(err, 500)
+	assert.Equal(t, []byte(body), resp.Body)
+	assert.Equal(t, req.ID, resp.ID)
 }
