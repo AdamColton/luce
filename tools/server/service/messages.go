d31f4028ff00c9956ef39040a32ac312de992686
Adam Colton <adamcolton@gmail.com>
2025-02-10 16:17:47 -0500

    +tools/server/service.SocketOpened

---
+++ tools/server/service/messages.go
@@ -0,0 +1,14 @@
+package service
+
+type SocketOpened struct {
+	ID uint32
+}
+
+// TypeID32 fulfill TypeIDer32. The ID was choosen at random.
+func (SocketOpened) TypeID32() uint32 {
+	return 1046109042
+}
+
+type SocketClose struct {
+	ID uint32
+}
