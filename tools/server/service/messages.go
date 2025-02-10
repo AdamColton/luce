4ef91393a56146071b4f18171961eed0a25dd22c
Adam Colton <adamcolton@gmail.com>
2025-02-10 16:19:46 -0500

    +tools/server/service.SocketClose

--- tools/server/service/messages.go
+++ tools/server/service/messages.go
@@ -12,3 +12,8 @@ func (SocketOpened) TypeID32() uint32 {
 type SocketClose struct {
 	ID uint32
 }
+
+// TypeID32 fulfill TypeIDer32. The ID was choosen at random.
+func (SocketClose) TypeID32() uint32 {
+	return 3196974518
+}
