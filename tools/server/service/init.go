572a551ecd0cf1464d682b9b2c0c0dad3f2f10a9
Adam Colton <adamcolton@gmail.com>
2025-02-10 15:41:16 -0500

    +tools/server/service.Request

---
+++ tools/server/service/init.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"encoding/gob"
+
+	"github.com/adamcolton/luce/serial/type32"
+	"github.com/adamcolton/luce/util/lfile"
+)
+
+var tm = type32.NewTypeMap()
+var OS lfile.FSFileReader = lfile.OSRepository{}
+
+// Register types with both gob and the typemap.
+func Register(zeroValues ...type32.TypeIDer32) {
+	for _, z := range zeroValues {
+		gob.Register(z)
+	}
+	tm.RegisterType32s(zeroValues...)
+}
+
+func init() {
+	Register(
+		(*Request)(nil),
+	)
+}
