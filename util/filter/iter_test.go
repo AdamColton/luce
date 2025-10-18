1d36baeee0f9d07988f2af51cfaf80c1ca1cc6ff
Adam Colton <adamcolton@gmail.com>
2025-10-18 18:00:51 -0400

    util/filter.Filter.Nexter BUGFIX

--- util/filter/iter_test.go
+++ util/filter/iter_test.go
@@ -10,15 +10,22 @@ import (
 )
 
 func TestIter(t *testing.T) {
-	var iterIn, iterOut liter.Iter[int]
-	iterIn = (slice.Slice[int]{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8}).Iter()
+	var iterOut liter.Iter[int]
+	s := slice.Slice[int]{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8}
 
-	iterOut = filter.GTE(5).Iter(iterIn)
+	iterOut = filter.GTE(5).Iter(s.Iter())
 
 	expected := []int{5, 9, 6, 5, 5, 8}
-	c := liter.Wrap(iterOut).ForIdx(func(i, idx int) {
+	checkFn := func(i, idx int) {
 		assert.Equal(t, expected[idx], i, idx)
-	})
+	}
+	c := liter.Wrap(iterOut).ForIdx(checkFn)
+	assert.Len(t, expected, c)
+	assert.True(t, iterOut.Done())
+
+	iterOut = filter.LT(5).Iter(s.Iter())
+	expected = []int{3, 1, 4, 1, 2, 3}
+	c = liter.Wrap(iterOut).ForIdx(checkFn)
 	assert.Len(t, expected, c)
 	assert.True(t, iterOut.Done())
 }
